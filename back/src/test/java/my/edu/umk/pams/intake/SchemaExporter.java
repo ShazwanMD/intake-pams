0cedb342d1b4309e6e83ed16322920566ac5d8cb
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-03-10 08:32:10 +0800

    Add exporter Add ignore

---
+++ back/src/test/java/my/edu/umk/pams/intake/SchemaExporter.java
@@ -0,0 +1,18 @@
+package my.edu.umk.pams.intake;
+
+import my.edu.umk.pams.intake.util.HibernateExporterUtil;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * @author PAMS
+ */
+public class SchemaExporter {
+    public static void main(String[] args) throws FileNotFoundException {
+        HibernateExporterUtil exporter = new HibernateExporterUtil(
+                "org.hibernate.dialect.PostgreSQL82Dialect",
+                "my.edu.umk.pams");
+        exporter.setGenerateDropQueries(true);
+        exporter.export(new File("create.sql"));
+    }}
