42ed08ee5e19c63dffab7fbd1f9b3adbc75d371e
Msyahrul <msyahrul@umk.edu.my>
2017-03-15 16:35:21 +0800

    AMS_1004 Created by Syah n Azah

---
+++ back/src/test/java/my/edu/umk/pams/intake/admission/stage/ThenICanInformTheSelectedApplicants.java
@@ -0,0 +1,22 @@
+package my.edu.umk.pams.intake.admission.stage;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.intake.admission.model.InCandidate;
+
+@JGivenStage
+public class ThenICanInformTheSelectedApplicants extends Stage <ThenICanInformTheSelectedApplicants> {
+	
+	@ExpectedScenarioState
+	private InCandidate candidate;
+
+	public ThenICanInformTheSelectedApplicants I_can_inform_the_selected_applicants() {
+		
+		//Kena buat method kat sini
+		return self();
+		
+	}
+
+}
