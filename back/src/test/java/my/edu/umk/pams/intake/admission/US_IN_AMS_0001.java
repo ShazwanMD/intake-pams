79d02079490b309041c933e1cbd82db411d880dc
Msyahrul <msyahrul@umk.edu.my>
2017-03-15 10:22:20 +0800

    PAMI-6: US_IN_AMS_0001
    Task-Url: http://jira.umk.edu.my:8080/browse/PAMI-6

--- back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_0001.java
+++ back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_0001.java
@@ -2,6 +2,7 @@ package my.edu.umk.pams.intake.admission;
 
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmPPSAdministrator;
+import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.intake.admission.stage.ThenCandidateCanProceedToNextSelectionPhase;
 import my.edu.umk.pams.intake.admission.stage.WhenIPreapproveCandidate;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
@@ -37,6 +38,7 @@ public class US_IN_AMS_0001 extends
 
     @Test
     @Rollback(true)
+    @Issue("PAMI-6")
     public void scenario1(){
         given().I_am_a_PPS_administrator_in_current_intake_session();
         when().I_preapprove_candidate();
