0c081c713ee04c55c5560aa7f3c545d061e5fe2f
Msyahrul <msyahrul@umk.edu.my>
2017-03-15 16:35:35 +0800

    AMS_1004 Created by Syah n Azah

---
+++ back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1004.java
@@ -0,0 +1,51 @@
+package my.edu.umk.pams.intake.admission;
+
+import javax.swing.Spring;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.bdd.stage.GivenIAmPPSAdministrator;
+import my.edu.umk.pams.bdd.tags.Issue;
+import my.edu.umk.pams.intake.admission.stage.ThenICanInformTheSelectedApplicants;
+import my.edu.umk.pams.intake.admission.stage.WhenIWantToSelectSuitableAppealedApplicants;
+import my.edu.umk.pams.intake.config.TestAppConfiguration;
+
+/**
+ * As academic administrator,
+ *      I want to select suitable appealed applicants, 
+ *           so that I can inform the selected applicants.
+ *
+ * @author PAMS
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@Issue("PAMS-6")
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class US_IN_AMS_1004 extends 
+	SpringScenarioTest<GivenIAmPPSAdministrator, WhenIWantToSelectSuitableAppealedApplicants, ThenICanInformTheSelectedApplicants> {
+	
+	@Before
+	private void before(){}
+	
+	@After
+	private void after(){}
+	
+	@Test
+	@Rollback(true)
+	@Issue("PAMS-6")
+	public void scanario1(){
+		given().I_am_a_PPS_administrator_in_current_intake_session();
+		when().I_want_to_select_suitable_appealed_applicants();
+		then().I_can_inform_the_selected_applicants();
+	}
+
+}
