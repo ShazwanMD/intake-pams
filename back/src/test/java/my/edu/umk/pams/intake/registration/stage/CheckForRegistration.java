45903147ff8a46b9be01e21f06a8f7dbd19844e2
Ashraf <Ashraf@10.3.2.67>
2017-03-19 10:28:55 +0800

    added method "when" in US_IN_RGN_2001

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/CheckForRegistration.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/CheckForRegistration.java
@@ -2,6 +2,7 @@
 package my.edu.umk.pams.intake.registration.stage;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 import my.edu.umk.pams.intake.identity.model.InApplicant;
@@ -9,7 +10,10 @@ import my.edu.umk.pams.intake.identity.model.InApplicantImpl;
 import my.edu.umk.pams.intake.identity.model.InUser;
 import my.edu.umk.pams.intake.identity.model.InUserImpl;
 import my.edu.umk.pams.intake.registration.service.RegistrationService;
+import my.edu.umk.pams.intake.security.service.SecurityService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 
 /**
  * @author PAMS
@@ -20,16 +24,20 @@ public class CheckForRegistration extends Stage<CheckForRegistration> {
     @Autowired
     private RegistrationService registrationService;
 
-    @ProvidedScenarioState
-    private InUser user;
+    @Autowired
+    private SecurityService securityService;
+
+    @ExpectedScenarioState
+    private InApplicant applicant;
 
     @ProvidedScenarioState
     private boolean exists;
 
     public CheckForRegistration I_check_for_registeration() {
 		
-		//exists = registrationService.isUserExists(username);	
-		
+    	//uda and max, tolong tgk jap betul dak buat mcm ni untuk check for registered user?
+    	exists = registrationService.isExists("applicant1");	
+    	Assert.isTrue(exists, "registered user doesnt exists");
 		
 		return self();
 	}
