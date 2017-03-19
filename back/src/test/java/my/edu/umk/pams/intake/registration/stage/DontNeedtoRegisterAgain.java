df150b6f6f4c6c2072240168dc4a49e51a2d4a90
Ashraf <Ashraf@10.3.2.67>
2017-03-19 14:29:45 +0800

    added when method for US_IN_RGN_2001 to complete the user story.

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/DontNeedtoRegisterAgain.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/DontNeedtoRegisterAgain.java
@@ -5,7 +5,7 @@ import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 import my.edu.umk.pams.intake.identity.model.InUser;
 import my.edu.umk.pams.intake.registration.service.RegistrationService;
-import org.junit.Assert;
+import org.springframework.util.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,27 +19,22 @@ import org.springframework.security.core.Authentication;
 @JGivenStage
 public class DontNeedtoRegisterAgain extends Stage<DontNeedtoRegisterAgain> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DontNeedtoRegisterAgain.class);
+	private static final Logger LOG = LoggerFactory.getLogger(DontNeedtoRegisterAgain.class);
 
-    @Autowired
-    private RegistrationService registrationService;
+	@Autowired
+	private RegistrationService registrationService;
 
-    @ExpectedScenarioState
-    InUser user;
+	@ExpectedScenarioState
+	InUser user;
 
-    @ExpectedScenarioState
-    boolean exists;
+	@ExpectedScenarioState
+	boolean exists;
 
-    public DontNeedtoRegisterAgain dont_need_to_register_again() {
-		
-		
-		// TODO Auto-generated method stub
-		
-    	
-    	//Assert.True(exists,"yes i have registered before");
-		
+	public DontNeedtoRegisterAgain dont_need_to_register_again() {
+
+		Assert.isTrue(exists, "yes i have registered before");
+
 		return self();
 	}
 
-   
 }
