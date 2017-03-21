c2407b7bb73576f95a6631fb5387a62901e3119f
max.zain <max.zain@canang.com.my>
2017-03-21 18:17:07 +0800

    fix file vs class naming conflicts

--- back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_2000.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_2000.java
@@ -1,17 +1,11 @@
 package my.edu.umk.pams.intake.policy;
 
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
-
-import my.edu.umk.pams.bdd.stage.GivenIAmMGSEBAdministrator;
 import my.edu.umk.pams.bdd.stage.GivenIAmPPSAdministrator;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
-import my.edu.umk.pams.intake.policy.stage.ThenICanChooseFromASelectionOfChoices;
-import my.edu.umk.pams.intake.policy.stage.ThenIntakeIsReadyForSetup;
-import my.edu.umk.pams.intake.policy.stage.ToBeOfferedInAnIntake;
-import my.edu.umk.pams.intake.policy.stage.WhenIAddIntake;
-import my.edu.umk.pams.intake.policy.stage.WhenISelectMultipleProgramCode;
-import my.edu.umk.pams.intake.registration.stage.GivenIHaveIncompleteApplication;
-
+import my.edu.umk.pams.intake.policy.stage.ThenICanChooseFromASelectionOfChoicesPPS;
+import my.edu.umk.pams.intake.policy.stage.ToBeOfferedInAnIntakePPS;
+import my.edu.umk.pams.intake.policy.stage.WhenIAddMultipleProgramCodePPS;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,16 +19,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author PAMS
-As a PPS academic administrator, 
-I want to select multiple program codes 
-to be offered in an intake 
-so that I can choose from a selection of choices
- 
+ *         As a PPS academic administrator,
+ *         I want to select multiple program codes
+ *         to be offered in an intake
+ *         so that I can choose from a selection of choices
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
-public class US_IN_PLC_2000 extends SpringScenarioTest<GivenIAmPPSAdministrator, WhenISelectMultipleProgramCode, ThenICanChooseFromASelectionOfChoices> {
+public class US_IN_PLC_2000 extends SpringScenarioTest<GivenIAmPPSAdministrator, WhenIAddMultipleProgramCodePPS, ThenICanChooseFromASelectionOfChoicesPPS> {
 
     private static final Logger LOG = LoggerFactory.getLogger(US_IN_PLC_2000.class);
 
@@ -53,7 +46,7 @@ public class US_IN_PLC_2000 extends SpringScenarioTest<GivenIAmPPSAdministrator,
     public void testScenario1() {
         given().I_am_a_PPS_administrator_in_current_intake_session();
         when().i_select_multiple_program_code();
-        addStage(ToBeOfferedInAnIntake.class).and().i_offer_an_intake();
+        addStage(ToBeOfferedInAnIntakePPS.class).and().i_offer_an_intake();
         then().i_can_choose_from_selection_of_choices();
     }
 }
