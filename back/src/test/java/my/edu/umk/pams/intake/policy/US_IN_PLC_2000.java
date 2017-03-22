d68d387d3496891564cbe365d5fa7f9451a08694
'hanifnoordin' <hanifnoordin@umk.edu.my>
2017-03-22 13:08:01 +0800

    PAMI-4: System Integrator
    Task-Url: http://jira.umk.edu.my:8080/browse/PAMI-4

--- back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_2000.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_2000.java
@@ -4,6 +4,7 @@ import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 
 import my.edu.umk.pams.bdd.stage.GivenIAmMGSEBAdministrator;
 import my.edu.umk.pams.bdd.stage.GivenIAmPPSAdministrator;
+import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import my.edu.umk.pams.intake.policy.stage.ThenICanChooseFromASelectionOfChoicesMGSEB;
 import my.edu.umk.pams.intake.policy.stage.ThenICanChooseFromASelectionOfChoicesPPS;
@@ -14,6 +15,7 @@ import my.edu.umk.pams.intake.policy.stage.WhenIAddIntake;
 import my.edu.umk.pams.intake.policy.stage.WhenIAddMultipleProgramCodeMGSEB;
 import my.edu.umk.pams.intake.policy.stage.WhenIAddMultipleProgramCodePPS;
 import my.edu.umk.pams.intake.registration.stage.GivenIHaveIncompleteApplication;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -50,6 +52,7 @@ public class US_IN_PLC_2000 extends SpringScenarioTest<GivenIAmPPSAdministrator,
 
         @Test
         @Rollback(true)
+        @Issue("PAMI-5")
         public void testScenario1() {
             given().I_am_a_PPS_administrator_in_current_intake_session();
             when().i_add_multiple_program_code_PPS();
