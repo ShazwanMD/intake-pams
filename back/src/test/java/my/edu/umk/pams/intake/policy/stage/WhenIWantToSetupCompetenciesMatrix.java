e2e04af13d60f896296c1c36e014e4c47390d511
samiya <samiya@10.3.32.157>
2017-03-22 11:43:05 +0800

    commit method for US_IN_PLC_3002

--- back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenIWantToSetupCompetenciesMatrix.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenIWantToSetupCompetenciesMatrix.java
@@ -1,22 +1,29 @@
 package my.edu.umk.pams.intake.policy.stage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.intake.common.service.CommonService;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.model.InProgramOffering;
 import my.edu.umk.pams.intake.policy.model.InProgramOfferingImpl;
 import my.edu.umk.pams.intake.policy.service.PolicyService;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.util.Assert;
 
 @JGivenStage
 public class WhenIWantToSetupCompetenciesMatrix extends Stage <WhenIWantToSetupCompetenciesMatrix> {
+	private static final Logger LOG = LoggerFactory.getLogger(WhenIWantToSetupCompetenciesMatrix.class);
 	
 	@Autowired
 	private PolicyService policyService;
+	
+	@Autowired
+	private CommonService commonService;
 	
 	@ProvidedScenarioState
 	private InProgramOffering programOffering;
@@ -33,12 +40,13 @@ public class WhenIWantToSetupCompetenciesMatrix extends Stage <WhenIWantToSetupC
        
        InProgramOffering programOffering = new InProgramOfferingImpl();
        
+       programOffering.setProgramCode(commonService.findProgramCodeByCode("MEM"));
        programOffering.setGeneralCriteria(generalCriteria);
        programOffering.setSpecificCriteria(specificCriteria);
    //    policyService.
        policyService.addProgramOffering(intake, programOffering);
        
-    //   Assert.
+       Assert.notNull(programOffering, "Program Offering is null");
        
 		return self();
 	}
