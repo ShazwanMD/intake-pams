b6e1a24d8e8bbbeb3c28686d8cf35cff3220ef7d
Ashraf <Ashraf@10.3.32.44>
2017-03-23 16:05:41 +0800

    added US_IN_PLC_3001 with the when method.

---
+++ back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_3001.java
@@ -0,0 +1,54 @@
+package my.edu.umk.pams.intake.policy;
+
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.bdd.stage.GivenIAmMGSEBAdministrator;
+import my.edu.umk.pams.bdd.stage.GivenIAmPPSAdministrator;
+import my.edu.umk.pams.intake.config.TestAppConfiguration;
+import my.edu.umk.pams.intake.policy.stage.ThenICanViewtheStudyCenter;
+import my.edu.umk.pams.intake.policy.stage.ThenIntakeIsReadyForSetup;
+import my.edu.umk.pams.intake.policy.stage.WhenIAddIntake;
+import my.edu.umk.pams.intake.policy.stage.WhenIAddStudyCenterInIntake;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author PAMS
+ * As a PPS Academic Administrator
+ * I want to initiate an intake by selecting a session, campus/site and offered program 
+ * so that the applicant can notice the intake session
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class US_IN_PLC_3001 extends SpringScenarioTest<GivenIAmMGSEBAdministrator, WhenIAddStudyCenterInIntake, ThenICanViewtheStudyCenter> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(US_IN_PLC_3001.class);
+
+    private String referenceNo;
+
+    @Before
+    public void before() {
+    }
+
+    @After
+    public void after() {
+    }
+
+    @Test
+    @Rollback(false)
+    public void scenario1() {
+        given().I_am_a_MGSEB_administrator_in_current_intake_session();
+        when().I_add_StudyCenter_in_intake();
+        then().i_can_view_study_center();
+    }
+}
---
+++ back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenIAddStudyCenterInIntake.java
@@ -0,0 +1,60 @@
+package my.edu.umk.pams.intake.policy.stage;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+import my.edu.umk.pams.intake.common.service.CommonService;
+import my.edu.umk.pams.intake.common.model.InStudyCenterCode;
+import my.edu.umk.pams.intake.policy.model.InIntake;
+import my.edu.umk.pams.intake.policy.model.InIntakeSession;
+import my.edu.umk.pams.intake.policy.model.InProgramLevel;
+import my.edu.umk.pams.intake.policy.model.InProgramOffering;
+import my.edu.umk.pams.intake.policy.model.InProgramOfferingImpl;
+import my.edu.umk.pams.intake.policy.service.PolicyService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+/**
+ * @author PAMS
+ */
+@JGivenStage
+public class WhenIAddStudyCenterInIntake extends Stage<WhenIAddStudyCenterInIntake> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WhenIAddIntake.class);
+
+    @Autowired
+    private CommonService commonService;
+    
+    @Autowired
+    private PolicyService policyService;
+
+    @ExpectedScenarioState
+    private InIntakeSession session;
+
+    @ProvidedScenarioState
+    private InProgramLevel level;
+
+    @ProvidedScenarioState
+    private InStudyCenterCode studyCenter;
+    
+    @ProvidedScenarioState
+    private InProgramOffering programOffering;
+
+    public WhenIAddStudyCenterInIntake I_add_StudyCenter_in_intake() {
+    	
+    	InIntake intake = policyService.findIntakeByReferenceNo("201720181/MASTER");
+
+        InProgramOffering programOffering = new InProgramOfferingImpl();
+
+        programOffering.setStudyCenterCode(commonService.findStudyCenterCodeByCode("SC-003"));
+        Assert.notNull(programOffering.getStudyCenterCode(), "studycenter is null");
+
+        policyService.addProgramOffering(intake, programOffering);
+
+        return self();
+    }
+}
