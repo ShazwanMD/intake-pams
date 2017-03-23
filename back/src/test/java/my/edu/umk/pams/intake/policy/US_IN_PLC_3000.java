bd286101073718d4314ab46c48c688855cac7386
Ashraf <Ashraf@10.3.32.44>
2017-03-23 11:03:29 +0800

    added  @Issue("PAMI-13") annotation

--- back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_3000.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_3000.java
@@ -2,6 +2,7 @@ package my.edu.umk.pams.intake.policy;
 
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmMGSEBAdministrator;
+import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import my.edu.umk.pams.intake.policy.stage.ThenICanChooseFromASelectionOfChoicesMGSEB;
 import my.edu.umk.pams.intake.policy.stage.ToBeOfferedInAnIntakeMGSEB;
@@ -43,6 +44,7 @@ public class US_IN_PLC_3000 extends SpringScenarioTest<GivenIAmMGSEBAdministrato
 
     @Test
     @Rollback(true)
+    @Issue("PAMI-13")
     public void testScenario1() {
         given().I_am_a_MGSEB_administrator_in_current_intake_session();
         when().i_add_multiple_program_code_MGSEB();
