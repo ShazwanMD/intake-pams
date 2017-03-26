9a36d1fd7dba11ce3de29090744621122390663f
Msyahrul <msyahrul@umk.edu.my>
2017-03-26 16:13:47 +0800

    PAMI17 - Done

---
+++ back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_3004.java
@@ -0,0 +1,43 @@
+package my.edu.umk.pams.intake.policy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
+import my.edu.umk.pams.bdd.tags.Issue;
+import my.edu.umk.pams.intake.config.TestAppConfiguration;
+import my.edu.umk.pams.intake.policy.stage.ThenICanDetermineTheLimitOfSupervisor;
+import my.edu.umk.pams.intake.policy.stage.WhenIWantToSetMaximumNumberOfSupervisor;
+
+/**
+ * @author PAMS
+ *  As a CPS academic Administrator, 
+ * I want to set the maximum number of supervisors for applicant, 
+ * so that I can determine the limit for applicant's supervisor
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class US_IN_PLC_3004 extends SpringScenarioTest<GivenIAmCPSAdministrator,
+WhenIWantToSetMaximumNumberOfSupervisor,
+ThenICanDetermineTheLimitOfSupervisor> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(US_IN_PLC_3004.class);
+
+    @Test
+    @Rollback(false)
+    @Issue("PAMI-17")
+    public void testScenario1() {
+        given().I_am_a_CPS_administrator_in_current_intake_session();
+        when().I_Want_To_Set_Maximum_Number_Of_Supervisor();
+        then().I_Can_Determine_The_Limit_Of_Supervisor();
+    }
+
+}
+
--- back/src/test/java/my/edu/umk/pams/intake/policy/stage/ThenICanDetermineTheLimitOfSupervisor.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/stage/ThenICanDetermineTheLimitOfSupervisor.java
@@ -1,19 +1,46 @@
 package my.edu.umk.pams.intake.policy.stage;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.intake.IntakeConstants;
+import my.edu.umk.pams.intake.policy.model.InIntake;
+import my.edu.umk.pams.intake.policy.model.InIntakeSession;
+import my.edu.umk.pams.intake.policy.service.PolicyService;
+import my.edu.umk.pams.intake.system.model.InConfiguration;
+import my.edu.umk.pams.intake.system.service.SystemService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @JGivenStage
 public class ThenICanDetermineTheLimitOfSupervisor extends Stage<ThenICanDetermineTheLimitOfSupervisor> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ThenICanDetermineTheLimitOfSupervisor.class);
+   private static final Logger LOG = LoggerFactory.getLogger(ThenICanDetermineTheLimitOfSupervisor.class);
 
-    
+   @Autowired
+   private PolicyService policyService;
+   
+   @Autowired
+   private SystemService systemService;
+
+   @ExpectedScenarioState
+   private InIntake intake;
+
+   @ExpectedScenarioState
+   private InIntakeSession Session;	
+   
     public ThenICanDetermineTheLimitOfSupervisor I_Can_Determine_The_Limit_Of_Supervisor() {
-
-	 return self();
+    	
+    		InIntake intake = policyService.findIntakeByReferenceNo("201720181/MASTER");
+    	
+    		InConfiguration configuration = systemService.findConfigurationByKey
+    										(IntakeConstants.ADVISING_SUPERVISOR_LIMIT);
+    	 
+    		systemService.saveConfiguration(configuration);
+    	 
+     return self();
     }
 }
-
