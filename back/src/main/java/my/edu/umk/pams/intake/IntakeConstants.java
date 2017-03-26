0ccee35f8001ace4eba0f28432dc6086782bb3e8
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-03-26 10:27:42 +0800

    Add config key

--- back/src/main/java/my/edu/umk/pams/intake/IntakeConstants.java
+++ back/src/main/java/my/edu/umk/pams/intake/IntakeConstants.java
@@ -14,4 +14,7 @@ public interface IntakeConstants {
     public static final String INTAKE_PROCESS_KEY = "intake_workflow";
     public static final String INTAKE_RESOURCE_PATH = "intake.bpmn20.xml";
     public static final String INTAKE_PROCESS_NAME = "intake";
+
+    // configuration
+    public static final String ADVISING_SUPERVISOR_LIMIT = "advising.supervisor.limit";
 }
---
+++ data/src/test/resources/data/IN_CNFG.sql
@@ -0,0 +1,2 @@
+INSERT INTO IN_CNFG (ID, CONFIG_KEY, CONFIG_VALUE, DESCRIPTION, C_ID, C_TS, M_ST)
+    VALUES (nextval('SQ_IN_CNFG'), 'advising.supervisor.limit', '2', 'Supervisor limit configuration', 1, CURRENT_TIMESTAMP, 1);
