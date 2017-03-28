9c65a632d6df6530907ebc8eeed68be45829956b
farah <farah@10.3.32.200>
2017-03-28 15:26:58 +0800

    commit

--- back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenIWantToListOutProgrammes.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenIWantToListOutProgrammes.java
@@ -35,7 +35,6 @@ public class WhenIWantToListOutProgrammes extends Stage<WhenIWantToListOutProgra
 
     public WhenIWantToListOutProgrammes i_want_to_list_out_programmes() {
 
-
         List<InProgramCode> programs = commonService.findProgramCodes();
 
         for (InProgramCode programcode : programs) {
