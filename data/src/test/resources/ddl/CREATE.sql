8722ec72cd0e251e67da76bab6231711de12040a
'hanifnoordin' <hanifnoordin@umk.edu.my>
2017-03-28 11:04:14 +0800

    push lookup seed and add column DESCRIPTION_MS,DESCRIPTION_EN

--- data/src/test/resources/ddl/CREATE.sql
+++ data/src/test/resources/ddl/CREATE.sql
@@ -245,7 +245,8 @@ create table IN_CNTY_CODE (
 create table IN_DBLY_CODE (
   ID int8 not null,
   CODE varchar(255),
-  DESCRIPTION varchar(255),
+  DESCRIPTION_MS varchar(255),
+  DESCRIPTION_EN varchar(255),
   C_TS timestamp,
   C_ID int8,
   D_TS timestamp,
@@ -458,7 +459,8 @@ create table IN_EMPT_SCTR_CODE (
 create table IN_ETNY_CODE (
   ID int8 not null,
   CODE varchar(255),
-  DESCRIPTION varchar(255),
+  DESCRIPTION_MS varchar(255),
+  DESCRIPTION_EN varchar(255),
   C_TS timestamp,
   C_ID int8,
   D_TS timestamp,
@@ -858,7 +860,8 @@ create table IN_PLMT_CODE (
 create table IN_PRGM_CODE (
   ID int8 not null,
   CODE varchar(255) not null,
-  DESCRIPTION varchar(255) not null,
+  DESCRIPTION_MS varchar(255) not null,
+  DESCRIPTION_EN varchar(255) not null,
   C_TS timestamp,
   C_ID int8,
   D_TS timestamp,
@@ -1076,7 +1079,8 @@ create table IN_STDY_CNTR_CODE (
 create table IN_STDY_MODE (
   ID int8 not null,
   CODE varchar(255) not null,
-  DESCRIPTION varchar(255) not null,
+  DESCRIPTION_MS varchar(255) not null,
+  DESCRIPTION_EN varchar(255) not null,
   C_TS timestamp,
   C_ID int8,
   D_TS timestamp,
