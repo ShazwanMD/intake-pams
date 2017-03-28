65b3e6035f1d957b8fa7a7454ad7450ecb50016d
Msyahrul <msyahrul@umk.edu.my>
2017-03-28 10:52:17 +0800

    InBidStatus.java

--- back/src/main/java/my/edu/umk/pams/intake/application/model/InBidStatus.java
+++ back/src/main/java/my/edu/umk/pams/intake/application/model/InBidStatus.java
@@ -1,14 +1,9 @@
 package my.edu.umk.pams.intake.application.model;
 
 public enum InBidStatus {
-    NEW,  //0
-    PROCESSING, //1
-    PRE_SELECTED,  //2
-    SELECTED,  //3
-    REJECTED,  //4
-    WITHDRAWN;  //5
-
+    NEW,
+    PROCESSING,
+    PRE_SELECTED,
+    SELECTED,
+    REJECTED;
 }
-
-	
-
