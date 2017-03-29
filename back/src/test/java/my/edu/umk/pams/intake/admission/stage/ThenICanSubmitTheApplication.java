dbce7b0e5dfd7f83505200f06d6e71abe8894ebe
Syah <Syah@10.3.2.176>
2017-03-29 16:31:28 +0800

    commit

--- back/src/test/java/my/edu/umk/pams/intake/admission/stage/ThenICanSubmitTheApplication.java
+++ back/src/test/java/my/edu/umk/pams/intake/admission/stage/ThenICanSubmitTheApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 
