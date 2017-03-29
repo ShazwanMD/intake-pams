143a0eca9e5c18c1237fe4f69aadc4c475d39fe3
samiya <samiya@10.3.32.157>
2017-03-29 13:12:06 +0800

    commit update additional stage when

--- back/src/test/java/my/edu/umk/pams/intake/application/stage/WhenISubmitApplication.java
+++ back/src/test/java/my/edu/umk/pams/intake/application/stage/WhenISubmitApplication.java
@@ -1,16 +1,18 @@
 package my.edu.umk.pams.intake.application.stage;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
 import my.edu.umk.pams.intake.application.model.InIntakeApplication;
-import my.edu.umk.pams.intake.application.model.InIntakeApplicationImpl;
 import my.edu.umk.pams.intake.application.service.ApplicationService;
 import my.edu.umk.pams.intake.identity.model.InApplicant;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.model.InIntakeSession;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class WhenISubmitApplication extends Stage<WhenISubmitApplication>{
+@JGivenStage
+public class WhenISubmitApplication extends Stage<WhenISubmitApplication> {
+
     @Autowired
     private ApplicationService applicationService;
 
@@ -27,11 +29,8 @@ public class WhenISubmitApplication extends Stage<WhenISubmitApplication>{
     private InIntakeApplication intakeApplication;
 
     public WhenISubmitApplication I_submit_application() {
-    	InIntakeApplication application = new InIntakeApplicationImpl();
-    	
         applicationService.submitIntakeApplication(intake, intakeApplication);
         return self();
     }
-
 
 }
