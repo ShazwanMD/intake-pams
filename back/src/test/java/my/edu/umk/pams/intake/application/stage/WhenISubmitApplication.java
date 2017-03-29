6074402982fc282fff17ea76bf867186c76eca3b
samiya <samiya@10.3.32.157>
2017-03-29 11:25:01 +0800

    update when for APN_1014

--- back/src/test/java/my/edu/umk/pams/intake/application/stage/WhenISubmitApplication.java
+++ back/src/test/java/my/edu/umk/pams/intake/application/stage/WhenISubmitApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import my.edu.umk.pams.intake.application.model.InIntakeApplication;
+import my.edu.umk.pams.intake.application.model.InIntakeApplicationImpl;
 import my.edu.umk.pams.intake.application.service.ApplicationService;
 import my.edu.umk.pams.intake.identity.model.InApplicant;
 import my.edu.umk.pams.intake.policy.model.InIntake;
@@ -26,6 +27,8 @@ public class WhenISubmitApplication extends Stage<WhenISubmitApplication>{
     private InIntakeApplication intakeApplication;
 
     public WhenISubmitApplication I_submit_application() {
+    	InIntakeApplication application = new InIntakeApplicationImpl();
+    	
         applicationService.submitIntakeApplication(intake, intakeApplication);
         return self();
     }
