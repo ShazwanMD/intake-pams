997d5da0a23cd3ae738b5a69cbd73da547fc94df
Ashraf <Ashraf@10.3.2.67>
2017-03-29 10:17:01 +0800

    updated ApplicationModuleTestSuite

--- back/src/test/java/my/edu/umk/pams/intake/application/ApplicationModuleTestSuite.java
+++ back/src/test/java/my/edu/umk/pams/intake/application/ApplicationModuleTestSuite.java
@@ -10,9 +10,17 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         US_IN_APN_1000.class,
         US_IN_APN_1001.class,
-        //  US_IN_APN_1002.class,
+        US_IN_APN_1002.class,
         US_IN_APN_1003.class,
         US_IN_APN_1004.class,
+        US_IN_APN_1005.class,
+        US_IN_APN_1006.class,
+        US_IN_APN_1007.class,
+        US_IN_APN_1008.class,
+        US_IN_APN_1013.class,
+        US_IN_APN_5001.class,
+        US_IN_APN_5002.class,
+        US_IN_APN_5005.class,
 })
 public class ApplicationModuleTestSuite {
 }
