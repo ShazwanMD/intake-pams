d7e7772a9ab60d1f16d2261461beaad9dd405b0a
Msyahrul <msyahrul@umk.edu.my>
2017-03-30 11:29:02 +0800

    PLC_3004

--- back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_3004.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/US_IN_PLC_3004.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
-import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
+import my.edu.umk.pams.bdd.stage.GivenIAmMGSEBAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import my.edu.umk.pams.intake.policy.stage.ThenICanDetermineTheLimitOfSupervisor;
@@ -17,16 +17,16 @@ import my.edu.umk.pams.intake.policy.stage.WhenISetMaximumNumberOfSupervisor;
 
 /**
  * @author PAMS
- *  As a CPS academic Administrator, 
+ *  As a MGSEB academic Administrator, 
  * I want to set the maximum number of supervisors for applicant, 
  * so that I can determine the limit for applicant's supervisor
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
-public class US_IN_PLC_3004 extends SpringScenarioTest<GivenIAmCPSAdministrator,
-        WhenISetMaximumNumberOfSupervisor,
-ThenICanDetermineTheLimitOfSupervisor> {
+public class US_IN_PLC_3004 extends SpringScenarioTest<GivenIAmMGSEBAdministrator,
+        													WhenISetMaximumNumberOfSupervisor,
+        														ThenICanDetermineTheLimitOfSupervisor> {
 
 	private static final Logger LOG = LoggerFactory.getLogger(US_IN_PLC_3004.class);
 
@@ -34,7 +34,7 @@ ThenICanDetermineTheLimitOfSupervisor> {
     @Rollback(false)
     @Issue("PAMI-17")
     public void testScenario1() {
-        given().I_am_a_CPS_administrator_in_current_intake_session();
+        given().I_am_a_MGSEB_administrator_in_current_intake_session();
         when().I_set_maximum_number_of_supervisor();
         then().the_applicant_supervisor_maximum_number_is_set();
     }
