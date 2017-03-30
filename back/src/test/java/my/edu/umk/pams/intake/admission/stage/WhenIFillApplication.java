ab023ea23cd1c0e10a83cbe39a2149b76d5e208b
Msyahrul <msyahrul@umk.edu.my>
2017-03-30 12:30:22 +0800

    When key in Data PAMI52

---
+++ back/src/test/java/my/edu/umk/pams/intake/admission/stage/WhenIFillApplication.java
@@ -0,0 +1,70 @@
+package my.edu.umk.pams.intake.admission.stage;
+
+import com.tngtech.jgiven.Stage;
+
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.intake.application.model.InBidStatus;
+import my.edu.umk.pams.intake.application.model.InIntakeApplication;
+import my.edu.umk.pams.intake.application.model.InIntakeApplicationImpl;
+import my.edu.umk.pams.intake.application.service.ApplicationService;
+import my.edu.umk.pams.intake.identity.model.InApplicant;
+import my.edu.umk.pams.intake.policy.model.InIntake;
+import my.edu.umk.pams.intake.policy.model.InIntakeSession;
+import my.edu.umk.pams.intake.system.service.SystemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static my.edu.umk.pams.intake.IntakeConstants.INTAKE_APPLICATION_REFERENCE_NO;
+
+@JGivenStage
+public class WhenIFillApplication extends Stage<WhenIFillApplication> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WhenIFillApplication.class);
+
+    @Autowired
+    private ApplicationService applicationService;
+
+    @Autowired
+    private SystemService systemService;
+
+    @ExpectedScenarioState
+    private InIntake intake;
+
+    @ExpectedScenarioState
+    private InIntakeSession intakeSession;
+
+    @ExpectedScenarioState
+    private InApplicant applicant;
+
+    @ProvidedScenarioState
+    private InIntakeApplication intakeApplication;
+
+    public WhenIFillApplication I_fill_in_application() {
+        // generate intake reference no
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("intakeSession", intakeSession);
+        map.put("programLevel", intake.getProgramLevel());
+        String referenceNo = systemService.generateFormattedReferenceNo(INTAKE_APPLICATION_REFERENCE_NO, map);
+
+        // start an intakeApplication
+        intakeApplication = new InIntakeApplicationImpl();
+        intakeApplication.setIntake(this.intake);
+        intakeApplication.setReferenceNo(referenceNo);
+        intakeApplication.setName("dummy john bin john doe");
+        intakeApplication.setEmail("dummyjohn@gmail.com");
+        intakeApplication.setPhone("0111020202");
+        intakeApplication.setOkuNo("S12223214");
+        intakeApplication.setSchoolName("SMKZA");
+        intakeApplication.setBidStatus(InBidStatus.APPEAL);
+        applicationService.draftIntakeApplication(intake, intakeApplication);
+
+        return self();
+    }
+}
