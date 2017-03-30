fd3f9d018adca8f92116d384d9af8e81b6a9041f
Msyahrul <msyahrul@umk.edu.my>
2017-03-30 12:28:54 +0800

    UMS_1005 PAMI52

--- back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1005.java
+++ back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1005.java
@@ -12,7 +12,9 @@ import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.intake.admission.stage.ThenICanSubmitTheApplication;
+import my.edu.umk.pams.intake.admission.stage.WhenIFillApplication;
 import my.edu.umk.pams.intake.admission.stage.WhenIWantToReviewApplicationsFlaggedAsAppeal;
+import my.edu.umk.pams.intake.application.stage.WhenIWantToFillinMultipleInformationOnMyExtraCurricularExperience;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 
 /**
@@ -28,16 +30,17 @@ import my.edu.umk.pams.intake.config.TestAppConfiguration;
 @ContextConfiguration(classes = TestAppConfiguration.class)
 public class US_IN_AMS_1005 extends
         SpringScenarioTest<GivenIAmCPSAdministrator,
-        						WhenIWantToReviewApplicationsFlaggedAsAppeal,
+        						WhenIFillApplication,
         							ThenICanSubmitTheApplication> {
 
 	@Issue("PAMI-52")
 	@Test
-    @Rollback
+    @Rollback(false)
     public void scenario1() {
         given().I_am_a_CPS_administrator_in_current_intake_session()
         .and().I_pick_an_intake_$("201720181/MASTER");
-        when().I_want_to_review_applications_flagged_as_appeal();
+        when().I_fill_in_application();
+        addStage(WhenIWantToReviewApplicationsFlaggedAsAppeal.class).and().I_want_to_review_applications_flagged_as_appeal();
         then().I_can_submit_the_application();
 	}
 }
