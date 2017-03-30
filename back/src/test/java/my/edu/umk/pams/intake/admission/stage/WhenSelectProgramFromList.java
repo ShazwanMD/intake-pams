da524694c62ba36a069ddd4d7a370f58c2a5aa08
farah <farah@10.3.32.200>
2017-03-30 15:38:16 +0800

    commit US_IN_AMS_2100 (pending)

---
+++ back/src/test/java/my/edu/umk/pams/intake/admission/stage/WhenSelectProgramFromList.java
@@ -0,0 +1,53 @@
+package my.edu.umk.pams.intake.admission.stage;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.Pending;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.intake.common.model.InProgramCode;
+import my.edu.umk.pams.intake.common.service.CommonService;
+import my.edu.umk.pams.intake.policy.model.InIntake;
+import my.edu.umk.pams.intake.policy.model.InProgramOffering;
+import my.edu.umk.pams.intake.policy.service.PolicyService;
+
+@Pending
+@JGivenStage
+public class WhenSelectProgramFromList extends Stage<WhenSelectProgramFromList> {
+	
+@Autowired
+private PolicyService policyService;	
+
+@Autowired
+private CommonService commonService;
+
+@ExpectedScenarioState
+private List<InProgramOffering> programOfferings;
+
+@ExpectedScenarioState
+private InIntake intake;
+
+@ExpectedScenarioState
+private InProgramCode programCode;
+	
+	 public WhenSelectProgramFromList Select_Program_From_List() {
+		 
+		 intake = policyService.findIntakeByReferenceNo("201720181/MASTER");
+		 List<InProgramOffering> programOfferings = policyService.findProgramOfferings(intake);
+		 Assert.notNull(commonService.findProgramCodeByCode("MCA"),"program is null");
+		// Assert.notNull(policyService.findProgramOfferingByIntakeAndProgramCode(intake, programCode);
+				
+		 
+		 
+		 
+		 
+		 
+		 
+	 return self();
+	 }
+}
