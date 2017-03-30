f6e396b14df7835b806faa10c9999a73baa77dfb
samiya <samiya@10.3.32.157>
2017-03-30 15:32:34 +0800

    add additional stage when in PLC3002

--- back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenIfillInApplicationResult.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenIfillInApplicationResult.java
@@ -3,6 +3,8 @@ package my.edu.umk.pams.intake.policy.stage;
 import my.edu.umk.pams.intake.application.model.InIntakeApplicationImpl;
 import my.edu.umk.pams.intake.application.model.InResult;
 import my.edu.umk.pams.intake.application.model.InResultImpl;
+import my.edu.umk.pams.intake.application.model.InResultItem;
+import my.edu.umk.pams.intake.application.model.InResultItemImpl;
 import my.edu.umk.pams.intake.application.model.InResultType;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
@@ -11,6 +13,11 @@ import com.tngtech.jgiven.integration.spring.JGivenStage;
 import my.edu.umk.pams.intake.admission.selection.SelectionStrategyHelper;
 import my.edu.umk.pams.intake.application.model.InIntakeApplication;
 import my.edu.umk.pams.intake.application.service.ApplicationService;
+import my.edu.umk.pams.intake.common.model.InGradeCode;
+import my.edu.umk.pams.intake.common.model.InGradeCodeImpl;
+import my.edu.umk.pams.intake.common.model.InSubjectCode;
+import my.edu.umk.pams.intake.common.model.InSubjectCodeImpl;
+import my.edu.umk.pams.intake.common.service.CommonService;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.model.InIntakeSession;
 import my.edu.umk.pams.intake.policy.service.PolicyService;
@@ -25,6 +32,9 @@ public class WhenIfillInApplicationResult extends Stage<WhenIfillInApplicationRe
 
     @Autowired
     private PolicyService policyService;
+    
+    @Autowired
+    private CommonService commonService;
 
     @Autowired
     private ApplicationService applicationService;
@@ -58,11 +68,38 @@ public class WhenIfillInApplicationResult extends Stage<WhenIfillInApplicationRe
         intakeApplication.setSchoolName("SMKZA");
         applicationService.draftIntakeApplication(intake, intakeApplication);
         
-       InResult result = new InResultImpl();
+        
+      //adding result
+        InResult result = new InResultImpl();
         result.setResultType(resultType.DIPLOMA);
         result.setApplication(intakeApplication);
         applicationService.addResult(intakeApplication, result);
         LOG.debug("results", result);
+        
+        
+        //adding grade code
+        InGradeCode grade = new InGradeCodeImpl();
+        grade.setCode("grade123");
+        grade.setDescription("test grade desc");
+        grade.setOrdinal(1);
+        commonService.saveGradeCode(grade);
+
+        
+        //adding subject code
+        InSubjectCode subject = new InSubjectCodeImpl();
+        subject.setCode("subject123");
+        subject.setDescription("test subject description");
+        
+        commonService.saveSubjectCode(subject);
+        
+        
+        //adding result item
+        InResultItem item = new InResultItemImpl();
+        item.setResult(result);
+        item.setGradeCode(grade);
+        item.setSubjectCode(subject);
+        
+        applicationService.addResultItem(intakeApplication, result, item);
           //    Assert.notEmpty(programOfferings, "program offering cannot be empty");
 
 //        helper.select(intake);
