3f93f5dcdc706a856010adb005ab3fe235b10209
Msyahrul <msyahrul@umk.edu.my>
2017-04-02 17:34:20 +0800

    commit PAMI 75

--- back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_3002.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_3002.java
@@ -3,6 +3,7 @@ package my.edu.umk.pams.intake.registration;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
+import my.edu.umk.pams.intake.admission.stage.WhenIFillApplication;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import my.edu.umk.pams.intake.registration.service.RegistrationService;
 import my.edu.umk.pams.intake.registration.stage.ThenICanProceedToProcessTheirApplication;
@@ -26,7 +27,9 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
-public class US_IN_RGN_3002 extends SpringScenarioTest<GivenIAmCPSAdministrator, WhenISelectTopApplicants, ThenICanProceedToProcessTheirApplication> {
+public class US_IN_RGN_3002 extends SpringScenarioTest<GivenIAmCPSAdministrator, 
+															WhenIFillApplication, 
+																ThenICanProceedToProcessTheirApplication> {
 
     private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_3002.class);
 
@@ -35,10 +38,12 @@ public class US_IN_RGN_3002 extends SpringScenarioTest<GivenIAmCPSAdministrator,
 
     @Test
     @Rollback
-    @Issue("PAMI-5")
+    @Issue("PAMI-75")
     public void scenario1() {
-        given().I_am_a_CPS_administrator_in_current_intake_session();
-        when().I_select_top_applicants();
+        given().I_am_a_CPS_administrator_in_current_intake_session()
+        .and().I_pick_an_intake_$("201720181/MASTER");
+        when().I_fill_in_application();
+        addStage(WhenISelectTopApplicants.class).and().I_select_top_applicants();
         then().I_can_proceed_to_process_their_application();
     }
 }
