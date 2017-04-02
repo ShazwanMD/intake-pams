ba3c947d3bfb6cc0d1e74ecc6b6c5b449af60e46
Msyahrul <msyahrul@umk.edu.my>
2017-04-02 16:01:45 +0800

    commit PAMI51

--- back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1004.java
+++ back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1004.java
@@ -4,6 +4,8 @@ import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.intake.admission.stage.ThenICanInformTheSelectedApplicants;
+import my.edu.umk.pams.intake.admission.stage.WhenIFillApplication;
+import my.edu.umk.pams.intake.admission.stage.WhenIWantToReviewApplicationsFlaggedAsAppeal;
 import my.edu.umk.pams.intake.admission.stage.WhenIWantToSelectSuitableAppealedApplicants;
 import my.edu.umk.pams.intake.application.US_IN_APN_1001;
 import my.edu.umk.pams.intake.application.US_IN_APN_1007;
@@ -30,7 +32,7 @@ import org.springframework.transaction.annotation.Transactional;
 @ContextConfiguration(classes = TestAppConfiguration.class)
 public class US_IN_AMS_1004 extends
         SpringScenarioTest<GivenIAmCPSAdministrator,
-                				WhenIWantToSelectSuitableAppealedApplicants,
+        						WhenIFillApplication,
                 					ThenICanInformTheSelectedApplicants> {
 	
 	@Issue ("PAMI-51")
@@ -39,7 +41,9 @@ public class US_IN_AMS_1004 extends
     public void scenario1() {
         given().I_am_a_CPS_administrator_in_current_intake_session()
         .and().I_pick_an_intake_$("201720181/MASTER");
-        when().I_want_to_select_suitable_appealed_applicants();
+        when().I_fill_in_application();
+        addStage(WhenIWantToSelectSuitableAppealedApplicants.class).and().I_want_to_select_suitable_appealed_applicants();
+        //when().I_want_to_select_suitable_appealed_applicants();
         then().I_can_inform_the_selected_applicants();
     }
 
