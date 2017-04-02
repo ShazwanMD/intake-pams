b6a6d2c854d6e3906ae707a54eb8dd8ac7fdf10e
samiya <samiya@10.3.32.157>
2017-04-02 16:56:05 +0800

    commit todo

--- back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenISelectApplicant.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenISelectApplicant.java
@@ -12,8 +12,13 @@ import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 
+import my.edu.umk.pams.intake.admission.dao.InCandidateDao;
+import my.edu.umk.pams.intake.admission.model.InCandidate;
+import my.edu.umk.pams.intake.admission.model.InCandidateImpl;
+import my.edu.umk.pams.intake.admission.model.InCandidateStatus;
 import my.edu.umk.pams.intake.admission.selection.SelectionStrategyHelper;
 import my.edu.umk.pams.intake.admission.selection.StandardSelectionStrategy;
+import my.edu.umk.pams.intake.admission.service.AdmissionService;
 import my.edu.umk.pams.intake.application.model.InIntakeApplication;
 import my.edu.umk.pams.intake.application.model.InResultType;
 import my.edu.umk.pams.intake.application.service.ApplicationService;
@@ -23,6 +28,7 @@ import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.model.InIntakeSession;
 import my.edu.umk.pams.intake.policy.model.InProgramOffering;
 import my.edu.umk.pams.intake.policy.service.PolicyService;
+import my.edu.umk.pams.intake.security.service.SecurityService;
 @Pending
 @JGivenStage
 public class WhenISelectApplicant extends Stage <WhenISelectApplicant>{
@@ -36,29 +42,69 @@ public class WhenISelectApplicant extends Stage <WhenISelectApplicant>{
 
     @Autowired
     private ApplicationService applicationService;
+    
+    @Autowired
+    private SecurityService securityService;
+    
+    @Autowired
+    AdmissionService admissionService;
+    
+    @Autowired
+    private InCandidateDao candidateDao;
 
     @Autowired
-    private StandardSelectionStrategy stdSelStrategy;
+    private StandardSelectionStrategy standardSelectionStrategy;
 
+    @Autowired
+    private SelectionStrategyHelper selectionStrategyHelper;
+    
     @ExpectedScenarioState
     private InIntake intake;
 
     @ProvidedScenarioState
-    private InIntakeApplication intakeApplication;
+    private InIntakeApplication application;
     
     @ProvidedScenarioState
     private InResultType resultType;
 
     @ExpectedScenarioState
     private InIntakeSession intakeSession;
+    
+    @ExpectedScenarioState
+    private InApplicant applicant;
 	
 	public WhenISelectApplicant I_select_applicant(){
 		//todo: (uda/max) tlg fix problem
 		
-		stdSelStrategy.select(intake);
+	//	stdSelStrategy.select(intake);
 	
+		
+		//List<InApplicant> applicant = applicationService.findApplicants(intake);
+		//List<InIntakeApplication> applications = applicationService.findIntakeApplicationsOrderedByRank(intake);
+		//standardSelectionStrategy.select(intake);
+		
+		
+		selectionStrategyHelper.select(intake);
 		LOG.debug("testing {} :", intake);
-	//	List<InApplicant> applicants = applicationService.findApplicants(intake);
+		admissionService.preselectIntakeApplication(application);
+		
+		LOG.debug("testing {} :", intake);
+	//	List<InIntakeApplication> application = applicationService.findIntakeApplications(intake);
+		
+		InCandidate candidate = new InCandidateImpl();
+        candidate.setName(application.getName());
+        candidate.setIdentityNo(application.getCredentialNo());
+        candidate.setEmail(application.getEmail());
+        candidate.setStudyMode(application.getStudyMode());
+        candidate.setStatus(InCandidateStatus.SELECTED);
+        candidate.setApplicant(application.getApplicant());
+        candidate.setOffering(application.getSelection());
+        
+        
+        candidateDao.save(candidate, securityService.getCurrentUser());
+/*        for (InIntakeApplication application : applications) {
+            preselectIntakeApplication(application);
+        }*/
 		
 		return self();
 	}
