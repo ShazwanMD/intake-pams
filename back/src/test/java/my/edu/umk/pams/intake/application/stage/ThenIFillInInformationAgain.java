4c872cac4ffa242e61acc2225e194ccfa33a7f97
samiya <samiya@MK02PC10>
2017-04-04 16:45:53 +0800

    commit updated complete then stage in US_IN_APN_1013

---
+++ back/src/test/java/my/edu/umk/pams/intake/application/stage/ThenIFillInInformationAgain.java
@@ -0,0 +1,54 @@
+package my.edu.umk.pams.intake.application.stage;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+import my.edu.umk.pams.intake.application.model.InBidStatus;
+import my.edu.umk.pams.intake.application.model.InIntakeApplication;
+import my.edu.umk.pams.intake.application.service.ApplicationService;
+import my.edu.umk.pams.intake.identity.model.InApplicant;
+import my.edu.umk.pams.intake.policy.model.InIntake;
+import my.edu.umk.pams.intake.policy.model.InIntakeSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+@JGivenStage
+public class ThenIFillInInformationAgain extends Stage<ThenIFillInInformationAgain>{
+	private static final Logger LOG = LoggerFactory.getLogger(ThenIFillInInformationAgain.class);
+
+	@Autowired
+    private ApplicationService applicationService;
+
+    @ExpectedScenarioState
+    private InIntake intake;
+
+    @ExpectedScenarioState
+    private InIntakeSession intakeSession;
+
+    @ExpectedScenarioState
+    private InApplicant applicant;
+
+    @ProvidedScenarioState
+    private InIntakeApplication intakeApplication;   
+
+    
+	public ThenIFillInInformationAgain I_can_fill_in_information_in_my_application_again() {
+
+        intakeApplication.setName("dummy john bin john doe");
+        intakeApplication.setEmail("dummyjohn@gmail.com");
+        intakeApplication.setPhone("0111020202");
+        intakeApplication.setOkuNo("S12223214");
+        intakeApplication.setSchoolName("SMKZA");
+        intakeApplication.setBidStatus(InBidStatus.DRAFTED);
+        applicationService.draftIntakeApplication(intake, intakeApplication);
+               
+		Assert.notNull(InBidStatus.DRAFTED, "application is not drafted");
+		LOG.debug("intake application status is: {}", intakeApplication.getBidStatus());
+
+        return self();
+	        
+	    }
+}
