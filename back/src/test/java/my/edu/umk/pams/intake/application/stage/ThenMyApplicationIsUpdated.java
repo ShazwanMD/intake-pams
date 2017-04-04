1a95bf1a1f6d73b103d348e40b9bec753bd9f167
samiya <samiya@MK02PC10>
2017-04-04 12:51:01 +0800

    commit complete then stage for US_IN_APN_2000

--- back/src/test/java/my/edu/umk/pams/intake/application/stage/ThenMyApplicationIsUpdated.java
+++ back/src/test/java/my/edu/umk/pams/intake/application/stage/ThenMyApplicationIsUpdated.java
@@ -1,11 +1,44 @@
 package my.edu.umk.pams.intake.application.stage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+import my.edu.umk.pams.intake.application.model.InBidStatus;
+import my.edu.umk.pams.intake.application.model.InIntakeApplication;
+import my.edu.umk.pams.intake.application.service.ApplicationService;
+import my.edu.umk.pams.intake.identity.model.InApplicant;
+import my.edu.umk.pams.intake.policy.model.InIntake;
+import my.edu.umk.pams.intake.policy.model.InIntakeSession;
 
 @JGivenStage
 public class ThenMyApplicationIsUpdated extends Stage<ThenMyApplicationIsUpdated>{
+	private static final Logger LOG = LoggerFactory.getLogger(ThenMyApplicationIsUpdated.class);
+	
+	 @Autowired
+	 private ApplicationService applicationService;
+
+	 @ExpectedScenarioState
+	 private InIntake intake;
+
+	 @ExpectedScenarioState
+	 private InIntakeSession intakeSession;
+
+	 @ExpectedScenarioState
+	 private InApplicant applicant;
+
+	 @ExpectedScenarioState
+	 private InIntakeApplication intakeApplication;
+	 
+	 
 	 public ThenMyApplicationIsUpdated my_application_is_updated(){
+		 applicationService.updateIntakeApplication(intakeApplication);
+		 Assert.notNull(InBidStatus.PROCESSING, "application is not updated");
+		 LOG.debug("intake status {}", intakeApplication.getBidStatus());
+
 		 return self();
 		 
 	 }
