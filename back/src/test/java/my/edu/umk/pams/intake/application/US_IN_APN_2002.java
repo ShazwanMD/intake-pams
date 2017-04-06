3744072fa3b86401b96610603b7a8832c1a1c542
samiya <samiya@10.3.32.157>
2017-04-06 15:47:19 +0800

    commit updated APN_2002

--- back/src/test/java/my/edu/umk/pams/intake/application/US_IN_APN_2002.java
+++ back/src/test/java/my/edu/umk/pams/intake/application/US_IN_APN_2002.java
@@ -14,6 +14,7 @@ import my.edu.umk.pams.bdd.stage.GivenIAmApplicant;
 import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.intake.application.stage.ThenTheApplicationIsWithdrawn;
 import my.edu.umk.pams.intake.application.stage.WhenIAmSelectedApplicant;
+import my.edu.umk.pams.intake.application.stage.WhenSelectedApplicantWithdrawApplication;
 import my.edu.umk.pams.intake.application.stage.WhenWithdrawApplication;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import my.edu.umk.pams.intake.policy.stage.WhenISetupCompetenciesMatrix;
@@ -40,8 +41,8 @@ public class US_IN_APN_2002 extends SpringScenarioTest<GivenIAmApplicant, WhenIA
     public void testScenario1() {
     	given().I_am_an_applicant_in_current_intake_session()
     	.and().I_am_applying_for_intake_$(INTAKE_REFERENCE_NO);
-    	when().I_am_selected_applicant();    	
-    	addStage(WhenWithdrawApplication.class).and().Withdraw_Application();
+    	when().I_am_selected_applicant_in_current_intake_session_$(INTAKE_REFERENCE_NO);    	
+    	addStage(WhenSelectedApplicantWithdrawApplication.class).and().Withdraw_Application_in_current_intake_session_$(INTAKE_REFERENCE_NO);
     	then().The_Application_Is_Withdrawn();
     	
     }
