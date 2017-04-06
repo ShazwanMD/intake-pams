b68ed42111b05cefa1e30df887424ce19da52783
samiya <samiya@10.3.32.157>
2017-04-06 15:47:48 +0800

    commit updated when stage APN_2002

--- back/src/test/java/my/edu/umk/pams/intake/application/stage/WhenIAmSelectedApplicant.java
+++ back/src/test/java/my/edu/umk/pams/intake/application/stage/WhenIAmSelectedApplicant.java
@@ -1,5 +1,7 @@
 package my.edu.umk.pams.intake.application.stage;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ import my.edu.umk.pams.intake.application.service.ApplicationService;
 import my.edu.umk.pams.intake.identity.model.InApplicant;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.model.InIntakeSession;
+import my.edu.umk.pams.intake.policy.service.PolicyService;
 
 @JGivenStage
 public class WhenIAmSelectedApplicant extends Stage<WhenIAmSelectedApplicant>{
@@ -26,6 +29,9 @@ public class WhenIAmSelectedApplicant extends Stage<WhenIAmSelectedApplicant>{
 	
 	@Autowired
     private ApplicationService applicationService;
+	
+	@Autowired
+	private PolicyService policyService;
 
 	@ExpectedScenarioState
     private InIntake intake;
@@ -52,13 +58,24 @@ public class WhenIAmSelectedApplicant extends Stage<WhenIAmSelectedApplicant>{
     @ProvidedScenarioState
     private InResultType resultType;
 	
-	public WhenIAmSelectedApplicant I_am_selected_applicant(){
+	public WhenIAmSelectedApplicant I_am_selected_applicant_in_current_intake_session_$(String intakeSession){
+		
+		
+		
+		InIntake intake = policyService.findIntakeByReferenceNo(intakeSession);
+		LOG.debug("intake {}", intake);
+		List<InIntakeApplication> applicants = applicationService.findIntakeApplications(intake,InBidStatus.SELECTED);
+		LOG.debug("applicants {}", applicants);
+		for (InIntakeApplication inIntakeApplication : applicants) {
+			Assert.notNull(inIntakeApplication, "list is null");
+			//inIntakeApplication.setBidStatus(InBidStatus.SELECTED);
+		}
 		
 		 
-	//  intakeApplication selection
+/*	//  intakeApplication selection
 		standardSelectionStrategy.select(intake);
 		admissionService.preselectIntakeApplication(intakeApplication);
-		 
+	*/	 
 	//	intakeApplication status SELECTED
 //		Assert.notNull(InBidStatus.SELECTED, "intake application is not selected");
 //		LOG.debug("intake application status: {}", intakeApplication.getBidStatus());
