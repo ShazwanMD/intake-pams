7a13d22afc8acb35ee23e5771c0f466d961481dc
farah <farah@10.3.32.200>
2017-04-10 17:02:26 +0800

    commit US_IN_RGN_5001

---
+++ back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_5001.java
@@ -0,0 +1,47 @@
+package my.edu.umk.pams.intake.registration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+import my.edu.umk.pams.bdd.stage.GivenIAmCandidate;
+import my.edu.umk.pams.bdd.tags.Issue;
+import my.edu.umk.pams.intake.application.stage.WhenISubmitApplication;
+import my.edu.umk.pams.intake.application.stage.WhenIWantToFillAllRequiredInformation;
+import my.edu.umk.pams.intake.config.TestAppConfiguration;
+import my.edu.umk.pams.intake.registration.stage.ThenAlertAnyChanges;
+import my.edu.umk.pams.intake.registration.stage.WhenReceiveConfirmationEmailForRegistration;
+import my.edu.umk.pams.intake.registration.stage.WhenReceiveNotificationOnRegistrationDate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+@As("As a selected applicant, I want to receive notification on registration date and itineraries so that I can be alert about my registration")
+public class US_IN_RGN_5001 extends SpringScenarioTest<GivenIAmCandidate,
+											WhenIWantToFillAllRequiredInformation,
+														ThenAlertAnyChanges> {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_5000.class);
+
+	public static final String INTAKE_REFERENCE_NO = "201720181/MASTER";
+	
+	 @Test
+	 @Issue("PAMI-96")
+	 @Rollback
+	 public void scenario1()  {
+	
+		 given().I_am_candidate_in_current_intake_session()
+		 .and().I_am_applying_for_intake_$(INTAKE_REFERENCE_NO);
+		 when().I_fill_in_all_the_required_information_in_my_application();
+		 addStage(WhenISubmitApplication.class).and().I_submit_application();
+		 addStage(WhenReceiveNotificationOnRegistrationDate.class).and().I_receive_notification_on_registration_date();
+		 then().alert_any_changes();
+	 }
+}
+
---
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenReceiveNotificationOnRegistrationDate.java
@@ -0,0 +1,49 @@
+package my.edu.umk.pams.intake.registration.stage;
+
+import java.util.List;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.intake.application.model.InBidStatus;
+import my.edu.umk.pams.intake.application.model.InIntakeApplication;
+import my.edu.umk.pams.intake.application.service.ApplicationService;
+import my.edu.umk.pams.intake.policy.model.InIntake;
+import my.edu.umk.pams.intake.policy.service.PolicyService;
+
+@JGivenStage
+public class WhenReceiveNotificationOnRegistrationDate  extends Stage<WhenReceiveNotificationOnRegistrationDate> {
+
+	private static final Logger LOG = LoggerFactory.getLogger (WhenReceiveNotificationOnRegistrationDate.class);
+	
+	 @Autowired
+	 private ApplicationService applicationService;
+	 
+	 @Autowired
+	 private PolicyService policyService;
+	
+	 @ExpectedScenarioState
+	 private InIntake intake;
+	 
+	
+	 public WhenReceiveNotificationOnRegistrationDate I_receive_notification_on_registration_date() {
+		 
+		  intake = policyService.findIntakeByReferenceNo(intake.getReferenceNo());
+		 
+		 LOG.debug("intake is : {}", intake);
+	     	List<InIntakeApplication> applications = applicationService.findIntakeApplications(intake, InBidStatus.SUBMITTED); 
+	      
+	     	intake.getEndDate();
+	     	LOG.debug("intake end Date : {}", intake.getEndDate());
+	     
+	        
+		 return self();
+ 	}
+
+}
