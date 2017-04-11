5b6718fb775f4452dacf76045d83df2a10c09628
Msyahrul <msyahrul@umk.edu.my>
2017-04-11 15:25:54 +0800

    AMS_1001

--- back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1001.java
+++ back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1001.java
@@ -17,7 +17,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
