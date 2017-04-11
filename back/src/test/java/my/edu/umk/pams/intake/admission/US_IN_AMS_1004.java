0389331ebee00e6a1800ff78fe0fb765a15d6d3f
Msyahrul <msyahrul@umk.edu.my>
2017-04-11 14:33:19 +0800

    commit AMS_1004

--- back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1004.java
+++ back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1004.java
@@ -5,12 +5,8 @@ import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.intake.admission.stage.ThenICanInformTheSelectedApplicants;
-import my.edu.umk.pams.intake.admission.stage.WhenIFillApplication;
-import my.edu.umk.pams.intake.admission.stage.WhenIWantToReviewApplicationsFlaggedAsAppeal;
+import my.edu.umk.pams.intake.admission.stage.WhenFillAllInformation;
 import my.edu.umk.pams.intake.admission.stage.WhenIWantToSelectSuitableAppealedApplicants;
-import my.edu.umk.pams.intake.application.US_IN_APN_1001;
-import my.edu.umk.pams.intake.application.US_IN_APN_1007;
-import my.edu.umk.pams.intake.application.stage.WhenIWantToFillinMultipleInformationOnMyWorkingExperience;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,18 +24,19 @@ import org.springframework.transaction.annotation.Transactional;
 @As("As CPS administrator, I want to select suitable appealed applicants, so that i can inform the selected applicants")
 public class US_IN_AMS_1004 extends
         SpringScenarioTest<GivenIAmCPSAdministrator,
-        						WhenIFillApplication,
+        						WhenFillAllInformation,
                 					ThenICanInformTheSelectedApplicants> {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(US_IN_AMS_1005.class);
+	public static final String INTAKE_REFERENCE_NO = "201720181/MASTER";
 	
 	@Issue ("PAMI-51")
     @Test
     @Rollback
     public void scenario1() {
-        given().I_am_a_CPS_administrator_in_current_intake_session()
-        .and().I_pick_an_intake_$("201720181/MASTER");
-        when().I_fill_in_application();
+		given().I_am_a_CPS_administrator_in_current_intake_session().and().I_pick_an_intake_$(INTAKE_REFERENCE_NO);
+        when().Applicant_fill_all_required_information_$(INTAKE_REFERENCE_NO); 
         addStage(WhenIWantToSelectSuitableAppealedApplicants.class).and().I_want_to_select_suitable_appealed_applicants();
-        //when().I_want_to_select_suitable_appealed_applicants();
         then().I_can_inform_the_selected_applicants();
     }
 
