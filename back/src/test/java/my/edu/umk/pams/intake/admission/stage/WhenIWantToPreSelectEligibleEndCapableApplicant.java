3770dd658c32ff51ccf4a70a763c20b064bf15bd
Msyahrul <msyahrul@umk.edu.my>
2017-04-11 10:32:48 +0800

    Commit When PAMI49

---
+++ back/src/test/java/my/edu/umk/pams/intake/admission/stage/WhenIWantToPreSelectEligibleEndCapableApplicant.java
@@ -0,0 +1,51 @@
+package my.edu.umk.pams.intake.admission.stage;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+import my.edu.umk.pams.intake.admission.model.InCandidate;
+import my.edu.umk.pams.intake.application.model.InBidStatus;
+import my.edu.umk.pams.intake.application.model.InIntakeApplication;
+import my.edu.umk.pams.intake.application.service.ApplicationService;
+import my.edu.umk.pams.intake.policy.model.InIntake;
+import my.edu.umk.pams.intake.policy.model.InIntakeSession;
+
+@JGivenStage
+public class WhenIWantToPreSelectEligibleEndCapableApplicant extends Stage<WhenIWantToPreSelectEligibleEndCapableApplicant> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WhenIWantToPreSelectEligibleEndCapableApplicant.class);
+	
+	 @ProvidedScenarioState
+	    private InCandidate candidate;
+	    
+	    @ExpectedScenarioState
+	    InIntakeSession intakeSession;
+	    
+	    @ExpectedScenarioState
+	    private InIntake intake;
+	    
+	    @Autowired
+	    private ApplicationService applicationService;
+	    
+	    @ProvidedScenarioState
+	    private InIntakeApplication preselectApplication;
+ 
+    //@Pending
+    public WhenIWantToPreSelectEligibleEndCapableApplicant I_want_to_pre_select_eligible_and_capable_applicants() {
+		
+    	List<InIntakeApplication> applications  =  applicationService.findIntakeApplications(intake,InBidStatus.PROCESSING);
+		
+		 for (InIntakeApplication application : applications) {
+         	
+         	//Fromm the list, we selected one application and set the data to selectedApplication
+			 preselectApplication = application;
+         }
+	
+       return self();
+    }
+}
