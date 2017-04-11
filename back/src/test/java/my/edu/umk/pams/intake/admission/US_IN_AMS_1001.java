0a3efae0a052b58ae55ab4eb1f56b89686330273
Msyahrul <msyahrul@umk.edu.my>
2017-04-11 11:48:12 +0800

    commit by Syah n Azah PAMI48

--- back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1001.java
+++ back/src/test/java/my/edu/umk/pams/intake/admission/US_IN_AMS_1001.java
@@ -3,11 +3,15 @@ package my.edu.umk.pams.intake.admission;
 import com.tngtech.jgiven.annotation.As;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
+import my.edu.umk.pams.bdd.tags.Issue;
 import my.edu.umk.pams.intake.admission.stage.ThenICanFilterTheTopApplicant;
+import my.edu.umk.pams.intake.admission.stage.WhenFillAllInformation;
 import my.edu.umk.pams.intake.admission.stage.WhenISliceAndDiceTopApplicant;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -20,15 +24,19 @@ import org.springframework.transaction.annotation.Transactional;
 @As("As CPS administrator, I want to slide and dice top applicant applications for an intake so that I can filter the top applicants")
 public class US_IN_AMS_1001 extends
         SpringScenarioTest<GivenIAmCPSAdministrator,
-                WhenISliceAndDiceTopApplicant,
-                ThenICanFilterTheTopApplicant> {
+        						WhenFillAllInformation,
+                					ThenICanFilterTheTopApplicant> {
 
+	private static final Logger LOG = LoggerFactory.getLogger(US_IN_AMS_1001.class);
+	public static final String INTAKE_REFERENCE_NO = "201720181/MASTER";
+	
     @Test
     @Rollback
+    @Issue("PAMI-48")
     public void scenario1() {
-        given().I_am_a_CPS_administrator_in_current_intake_session()
-        .and().I_pick_an_intake_$("201720181/MASTER");
-        when().I_slide_and_dice_top_applicant_for_intake();
+    	given().I_am_a_CPS_administrator_in_current_intake_session().and().I_pick_an_intake_$(INTAKE_REFERENCE_NO);
+        when().Applicant_fill_all_required_information_$(INTAKE_REFERENCE_NO); 
+        addStage(WhenISliceAndDiceTopApplicant.class).I_slide_and_dice_top_applicant_for_intake();
         then().I_can_filter_the_top_applicant();
     }
 }
