430e9ff64158362f7ea8fc5cd7a2e6bde5779a19
Ashraf <Ashraf@10.3.2.67>
2017-04-11 16:17:57 +0800

    Updated AdmissionTestSuite

--- back/src/test/java/my/edu/umk/pams/intake/admission/AdmissionModuleTestSuite.java
+++ back/src/test/java/my/edu/umk/pams/intake/admission/AdmissionModuleTestSuite.java
@@ -13,6 +13,23 @@ import org.junit.runners.Suite;
         US_IN_AMS_1002.class,
         US_IN_AMS_1003.class,
         US_IN_AMS_1004.class,
+        US_IN_AMS_1005.class,
+        US_IN_AMS_1006.class,
+        US_IN_AMS_1007.class,
+        US_IN_AMS_1009.class,
+        US_IN_AMS_1100.class,
+        US_IN_AMS_2000.class,
+        US_IN_AMS_2001.class,
+        US_IN_AMS_2002.class,
+        US_IN_AMS_2003.class,
+        US_IN_AMS_2004.class,
+        US_IN_AMS_2005.class,
+        US_IN_AMS_2006.class,
+        US_IN_AMS_2007.class,
+        US_IN_AMS_2009.class,
+        US_IN_AMS_2100.class,
+        US_IN_AMS_2200.class,
+        
 })
 public class AdmissionModuleTestSuite {
 }
