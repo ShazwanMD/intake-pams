0630718a7c8b4253ca2556b0b13cf86850a46306
Ashraf <Ashraf@10.3.2.67>
2017-04-11 12:31:18 +0800

    updated "WhenFillAllInformation"

--- back/src/test/java/my/edu/umk/pams/intake/admission/stage/WhenFillAllInformation.java
+++ back/src/test/java/my/edu/umk/pams/intake/admission/stage/WhenFillAllInformation.java
@@ -4,6 +4,8 @@ import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import io.jsonwebtoken.lang.Assert;
 import my.edu.umk.pams.intake.application.model.InBidStatus;
 import my.edu.umk.pams.intake.application.model.InIntakeApplication;
 import my.edu.umk.pams.intake.application.model.InIntakeApplicationImpl;
@@ -61,7 +63,8 @@ public class WhenFillAllInformation extends Stage<WhenFillAllInformation> {
         intakeApplication.setSchoolName("SMKZ1");
         intakeApplication.setBidStatus(InBidStatus.DRAFTED);
         applicationService.draftIntakeApplication(intake, intakeApplication);
-
+        Assert.notNull(intakeApplication, "intake application is empty");
+        LOG.debug("intake appliaction {} :" , intakeApplication);
         return self();
     }
 }
