1c26d8c552f5cb53e8707f21d76d664b740b8987
samiya <samiya@10.3.32.157>
2017-04-12 16:36:51 +0800

    update RGN test suite

--- back/src/test/java/my/edu/umk/pams/intake/registration/RegistrationModuleTestSuite.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/RegistrationModuleTestSuite.java
@@ -13,12 +13,15 @@ import org.junit.runners.Suite;
         US_IN_RGN_2001.class,
         US_IN_RGN_3000.class,
         US_IN_RGN_3001.class, //pending
-        US_IN_RGN_3002.class, //pending
+        US_IN_RGN_3002.class, 
         US_IN_RGN_3003.class,
         US_IN_RGN_3007.class,
         US_IN_RGN_4000.class,
         US_IN_RGN_4001.class, //pending
-        US_IN_RGN_4002.class, //pending
+        US_IN_RGN_4002.class,
+        US_IN_RGN_4003.class, //pending
+        US_IN_RGN_4004.class, //pending
+        US_IN_RGN_4007.class,
         US_IN_RGN_4011.class,
         US_IN_RGN_5000.class,
         US_IN_RGN_5001.class,
