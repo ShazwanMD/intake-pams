d7b1ee81faa8fc110c6ce17c17117c76209c7e1f
samiya <samiya@10.3.32.157>
2017-04-12 16:04:29 +0800

    fix error

--- back/src/test/java/my/edu/umk/pams/intake/application/stage/WhenIFillProcessingApplication.java
+++ back/src/test/java/my/edu/umk/pams/intake/application/stage/WhenIFillProcessingApplication.java
@@ -10,7 +10,6 @@ import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
-import my.edu.umk.pams.intake.admission.stage.WhenIFillApplication;
 import my.edu.umk.pams.intake.application.model.InBidStatus;
 import my.edu.umk.pams.intake.application.model.InIntakeApplication;
 import my.edu.umk.pams.intake.application.model.InIntakeApplicationImpl;
