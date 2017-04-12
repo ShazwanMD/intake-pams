b4a60b611c554a0926756de83561464430a21981
samiya <samiya@10.3.32.157>
2017-04-12 09:54:57 +0800

    add completed US_IN_RGN_4007

---
+++ back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_4007.java
@@ -0,0 +1,40 @@
+package my.edu.umk.pams.intake.registration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+import my.edu.umk.pams.bdd.stage.GivenIAmMGSEBAdministrator;
+import my.edu.umk.pams.bdd.tags.Issue;
+import my.edu.umk.pams.intake.config.TestAppConfiguration;
+import my.edu.umk.pams.intake.registration.stage.ThenICanCompleteStudentRegistration;
+import my.edu.umk.pams.intake.registration.stage.WhenIWantToScanTheApplicantOfferLetterBarcode;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+@As("As a MGSEB academic administrator, I want to scan the applicant's offer letter barcode for matriculation so that I can complete the student's registration")
+
+public class US_IN_RGN_4007 extends SpringScenarioTest <GivenIAmMGSEBAdministrator,
+WhenIWantToScanTheApplicantOfferLetterBarcode, ThenICanCompleteStudentRegistration> {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_4007.class);
+	
+    private String intakeReferenceNo = "201720181/MASTER";
+
+    @Test
+    @Rollback
+    @Issue("PAMI-91")
+    public void scenario1() {
+        given().I_am_a_MGSEB_administrator_in_current_intake_session()
+        .and().I_pick_an_intake_$(intakeReferenceNo);
+        when().I_want_to_scan_the_applicant_offer_letter_barcode();
+        then().I_can_complete_student_registration();
+    }
+}
