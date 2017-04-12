60ee1868003084b3029505b7399b09084aae57bf
samiya <samiya@10.3.32.157>
2017-04-12 16:00:41 +0800

    commit US_IN_RGN_4000

--- back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_4000.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_4000.java
@@ -13,29 +13,32 @@ import com.tngtech.jgiven.annotation.As;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmMGSEBAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
-import my.edu.umk.pams.intake.admission.stage.WhenIFillInIntakeApplication;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import my.edu.umk.pams.intake.registration.service.RegistrationService;
 import my.edu.umk.pams.intake.registration.stage.ThenICanProceedRegistration;
+import my.edu.umk.pams.intake.registration.stage.WhenApplicantFillAndSubmitApplication;
 import my.edu.umk.pams.intake.registration.stage.WhenIVerifySponsorshipStatus;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
 @As("As a MGSEB academic Administrator, I want to view applicant that have sponsorship or not so that i can proceed the applicant's registration")
-public class US_IN_RGN_4000 extends SpringScenarioTest<GivenIAmMGSEBAdministrator, WhenIFillInIntakeApplication, ThenICanProceedRegistration> {
+public class US_IN_RGN_4000 extends SpringScenarioTest<GivenIAmMGSEBAdministrator, WhenApplicantFillAndSubmitApplication, ThenICanProceedRegistration> {
 
     private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_4000.class);
 
     @Autowired
     private RegistrationService registrationService;
+    
+    private String intakeReferenceNo = "201720181/MASTER";
 
     @Test
     @Rollback
     @Issue("PAMI-84")
     public void scenario1() {
-        given().I_am_a_MGSEB_administrator_in_current_intake_session();
-        when().I_fill_in_intake_applicaton();
+        given().I_am_a_MGSEB_administrator_in_current_intake_session()
+        .and().I_pick_an_intake_$(intakeReferenceNo);
+        when().I_fill_in_application().and().applicant_submit_application();
         addStage(WhenIVerifySponsorshipStatus.class).and().I_verify_applicant_has_valid_sponsorship_status();
         then().I_can_process_applicant_registration();
     }
