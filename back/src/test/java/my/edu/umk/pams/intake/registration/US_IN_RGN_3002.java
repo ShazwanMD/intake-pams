6d01608d5ce21ebb9caca0e4f0d5fe35b2c45591
Ashraf <Ashraf@10.3.2.67>
2017-04-12 14:38:44 +0800

    updated US_IN_RGN_3002

--- back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_3002.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_3002.java
@@ -4,10 +4,10 @@ import com.tngtech.jgiven.annotation.As;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
-import my.edu.umk.pams.intake.admission.stage.WhenIFillApplication;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import my.edu.umk.pams.intake.registration.service.RegistrationService;
 import my.edu.umk.pams.intake.registration.stage.ThenICanProceedToProcessTheirApplication;
+import my.edu.umk.pams.intake.registration.stage.WhenApplicantFillAndSubmitApplication;
 import my.edu.umk.pams.intake.registration.stage.WhenISelectTopApplicants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,7 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 @ContextConfiguration(classes = TestAppConfiguration.class)
 @As("As a CPS academic Administrator, I want to view all the top applicant applications for an intake so that I can proceed to process their application")
 public class US_IN_RGN_3002 extends SpringScenarioTest<GivenIAmCPSAdministrator,
-															WhenIFillApplication,
+															WhenApplicantFillAndSubmitApplication,
 																ThenICanProceedToProcessTheirApplication> {
 
     private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_3002.class);
@@ -40,7 +40,7 @@ public class US_IN_RGN_3002 extends SpringScenarioTest<GivenIAmCPSAdministrator,
     public void scenario1() {
         given().I_am_a_CPS_administrator_in_current_intake_session()
         .and().I_pick_an_intake_$(intakeReferenceNo);
-        when().I_fill_in_application();
+        when().I_fill_in_application().and().applicant_submit_application();
         addStage(WhenISelectTopApplicants.class).and().I_select_top_applicants();
         then().I_can_proceed_to_process_their_application();
     }
