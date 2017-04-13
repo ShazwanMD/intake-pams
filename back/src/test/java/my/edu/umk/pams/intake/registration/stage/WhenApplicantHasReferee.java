591ba870599e9ca28d48d6731f3df98cd3950d56
Ashraf <Ashraf@10.3.2.67>
2017-04-13 16:31:12 +0800

    updated "WhenApplicantHasReferee"

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenApplicantHasReferee.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenApplicantHasReferee.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+
+import my.edu.umk.pams.intake.application.model.InReferee;
+import my.edu.umk.pams.intake.application.model.InRefereeImpl;
+import my.edu.umk.pams.intake.application.model.InRefereeType;
+import my.edu.umk.pams.intake.application.service.ApplicationService;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.service.PolicyService;
 
@@ -16,14 +21,30 @@ public class WhenApplicantHasReferee extends Stage<WhenApplicantHasReferee>{
 	@Autowired
 	private PolicyService policyService;
 	
+	@Autowired
+	private ApplicationService applicationService;
+	
 	@ProvidedScenarioState
 	private InIntake intake;
 	
 	 public WhenApplicantHasReferee an_applicant_has_referee(){
-		 Assert.notNull(intake, "intake cannot be null");
+		
 		 intake = policyService.findIntakeByReferenceNo("201720181/MASTER");
+		 Assert.notNull(intake, "intake cannot be null");
 		 
-		 //todo : need referee's setup
+		 InReferee referee = new InRefereeImpl();
+		 // referee.setApplication(application);
+		 
+		 //todo mia : need to add a method for drafting an application
+		 
+		 referee.setName("Mr. Chan");
+		 referee.setOccupation("Lecturer");
+		 referee.setOfficeAddrs("UMK Pengkalan Chepa, Kota Bharu");
+		 referee.setPhoneNo("012343234");
+		 referee.setType(InRefereeType.PRIMARY);
+		
+		 //todo mia : need to add a method to save referee
+		 
 		 return self();
 	 }
 		
