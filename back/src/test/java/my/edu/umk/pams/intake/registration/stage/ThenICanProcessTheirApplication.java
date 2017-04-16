76c77302aaa5e152cccfe158f2d1cac237fc6849
samiya <samiya@10.3.32.157>
2017-04-16 12:57:40 +0800

    update complete then stage for US_IN_RGN_4003

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/ThenICanProcessTheirApplication.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/ThenICanProcessTheirApplication.java
@@ -2,24 +2,52 @@ package my.edu.umk.pams.intake.registration.stage;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.intake.admission.model.InCandidate;
+import my.edu.umk.pams.intake.application.model.InIntakeApplication;
+import my.edu.umk.pams.intake.application.service.ApplicationService;
 import my.edu.umk.pams.intake.identity.model.InUser;
+import my.edu.umk.pams.intake.policy.model.InIntake;
+import my.edu.umk.pams.intake.policy.model.InIntakeSession;
 import my.edu.umk.pams.intake.registration.service.RegistrationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
+@JGivenStage
 public class ThenICanProcessTheirApplication extends Stage<ThenICanProcessTheirApplication> {
 
     public static final Logger LOG = LoggerFactory.getLogger(ThenICanProcessTheirApplication.class);
 
     @Autowired
     private RegistrationService registrationService;
+    
+    @Autowired
+    private ApplicationService applicationService;
 
     @ExpectedScenarioState
     InUser user;
+    
+    @ExpectedScenarioState
+    InIntakeSession intakeSession;
 
-
-    public ThenICanProcessTheirApplication I_can_process_their_application() {
+    @ProvidedScenarioState
+    private InCandidate candidate;
+    
+    @ExpectedScenarioState
+    private InIntake intake;
+    
+    @ExpectedScenarioState
+    private InIntakeApplication application;
+        
+    public ThenICanProcessTheirApplication I_can_process_the_application() {
+    	
+    	   	
+    	//prosses application 
+    
+    	applicationService.processIntakeApplication(intake, application);
+        LOG.debug("intake status {} :", application.getBidStatus());
         return self();
     }
 
