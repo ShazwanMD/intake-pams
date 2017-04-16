37e7d7f92a6777e4bb65b1eab59d4c216dbcbb48
samiya <samiya@10.3.32.157>
2017-04-16 15:23:56 +0800

    RGN_4004

--- back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_4004.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_4004.java
@@ -13,15 +13,19 @@ import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 import my.edu.umk.pams.bdd.stage.GivenIAmMGSEBAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
+import my.edu.umk.pams.intake.application.stage.ThenICanSubmitMyApplication;
+import my.edu.umk.pams.intake.application.stage.WhenIWantToFillAllRequiredInformation;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
 import my.edu.umk.pams.intake.registration.stage.ThenICanProceedToProcessTheirApplication;
+import my.edu.umk.pams.intake.registration.stage.ThenICanProcessTheirApplication;
 import my.edu.umk.pams.intake.registration.stage.WhenApplicantHasReferee;
+import my.edu.umk.pams.intake.registration.stage.WhenISelectTopApplicants;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
 @As("As a MGSEB academic Administrator, I want to know applicant's referee's educational background, so that i can process the application")
-public class US_IN_RGN_4004 extends SpringScenarioTest<GivenIAmMGSEBAdministrator, WhenApplicantHasReferee, ThenICanProceedToProcessTheirApplication>{
+public class US_IN_RGN_4004 extends SpringScenarioTest<GivenIAmMGSEBAdministrator, WhenIWantToFillAllRequiredInformation, ThenICanSubmitMyApplication>{
 
 private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_3007.class);
 	
@@ -30,11 +34,14 @@ private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_3007.class);
     @Test
     @Rollback
     @Issue("PAMI-88")
-    @Pending
+   // @Pending
     public void scenario1() {
-    	given().I_am_a_MGSEB_administrator_in_current_intake_session();
-    	when().an_applicant_has_referee();
-    	then().I_can_proceed_to_process_their_application();
+    	given().I_am_a_MGSEB_administrator_in_current_intake_session()
+    		.and().I_pick_an_intake_$(intakeReferenceNo);
+    	when().I_fill_in_all_the_required_information_in_my_application();
+    	addStage(WhenApplicantHasReferee.class).and().an_applicant_has_referee();    	
+    	then().I_can_submit_my_application();
+    	addStage(ThenICanProcessTheirApplication.class).and().I_can_process_the_application();
     }
 	
 }
