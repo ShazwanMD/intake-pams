fb757597d48905be96ae2cdc69ed74f68d169716
samiya <samiya@10.3.32.157>
2017-04-16 15:40:24 +0800

    commit US_IN_RGN_3003

--- back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_3003.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_3003.java
@@ -3,12 +3,13 @@ package my.edu.umk.pams.intake.registration;
 import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.As;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
-import my.edu.umk.pams.bdd.stage.GivenIAmCPSKeraniAdministrator;
+import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
 import my.edu.umk.pams.bdd.tags.Issue;
+import my.edu.umk.pams.intake.application.stage.ThenICanSubmitMyApplication;
+import my.edu.umk.pams.intake.application.stage.WhenIWantToFillAllRequiredInformation;
 import my.edu.umk.pams.intake.config.TestAppConfiguration;
-import my.edu.umk.pams.intake.registration.service.RegistrationService;
 import my.edu.umk.pams.intake.registration.stage.ThenICanProcessTheirApplication;
-import my.edu.umk.pams.intake.registration.stage.WhenIVerifyRefereeStatus;
+import my.edu.umk.pams.intake.registration.stage.WhenApplicantHasReferee;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -23,20 +24,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @ContextConfiguration(classes = TestAppConfiguration.class)
-@As("As an academic administrator, I want to know who is applicant's referee so that i can process their application")
-public class US_IN_RGN_3003 extends SpringScenarioTest<GivenIAmCPSKeraniAdministrator, WhenIVerifyRefereeStatus, ThenICanProcessTheirApplication> {
-    private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_3003.class);
-
-
+@As("As a CPS academic Administrator, I want to know who is applicant's referee, so that i can process their application")
+public class US_IN_RGN_3003 extends SpringScenarioTest<GivenIAmCPSAdministrator,  WhenIWantToFillAllRequiredInformation, ThenICanSubmitMyApplication>{	
+  private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_3003.class);
+    
+    private String intakeReferenceNo = "201720181/MASTER";
 
     @Test
     @Rollback
-    @Issue("")
+    @Issue("76")
     public void scenario1() {
-        given().I_am_a_CPS_administrator_in_current_intake_session();
-        when().I_verify_applicant_has_referee();
-        then().I_can_process_the_application();
-    }
+        given().I_am_a_CPS_administrator_in_current_intake_session()
+		.and().I_pick_an_intake_$(intakeReferenceNo);
+		when().I_fill_in_all_the_required_information_in_my_application();
+		addStage(WhenApplicantHasReferee.class).and().an_applicant_has_referee();    	
+		then().I_can_submit_my_application();
+		addStage(ThenICanProcessTheirApplication.class).and().I_can_process_the_application();
+}
 
     // todo(samiya + ashraf)
     @Pending
