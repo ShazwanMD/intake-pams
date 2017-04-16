a6f253f5a8680d2d38fe63e72093adb18c429511
samiya <samiya@10.3.32.157>
2017-04-16 12:56:55 +0800

    update complete when stage for US_IN_RGN_4003

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenApplicantHasReferee.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenApplicantHasReferee.java
@@ -5,8 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
 
+import my.edu.umk.pams.intake.application.model.InIntakeApplication;
 import my.edu.umk.pams.intake.application.model.InReferee;
 import my.edu.umk.pams.intake.application.model.InRefereeImpl;
 import my.edu.umk.pams.intake.application.model.InRefereeType;
@@ -14,7 +17,7 @@ import my.edu.umk.pams.intake.application.service.ApplicationService;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.service.PolicyService;
 
-
+@JGivenStage
 public class WhenApplicantHasReferee extends Stage<WhenApplicantHasReferee>{
 	private static final Logger LOG = LoggerFactory.getLogger(WhenApplicantHasReferee.class);
 	
@@ -27,13 +30,18 @@ public class WhenApplicantHasReferee extends Stage<WhenApplicantHasReferee>{
 	@ProvidedScenarioState
 	private InIntake intake;
 	
+	@ExpectedScenarioState
+    private InIntakeApplication application;
+	
+	
 	 public WhenApplicantHasReferee an_applicant_has_referee(){
 		
 		 intake = policyService.findIntakeByReferenceNo("201720181/MASTER");
 		 Assert.notNull(intake, "intake cannot be null");
+		 LOG.debug("intake status {}", application.getBidStatus());
 		 
 		 InReferee referee = new InRefereeImpl();
-		 // referee.setApplication(application);
+		 // referee.setApplication(application);		 
 		 
 		 //todo mia : need to add a method for drafting an application
 		 
@@ -42,7 +50,22 @@ public class WhenApplicantHasReferee extends Stage<WhenApplicantHasReferee>{
 		 referee.setOfficeAddrs("UMK Pengkalan Chepa, Kota Bharu");
 		 referee.setPhoneNo("012343234");
 		 referee.setType(InRefereeType.PRIMARY);
+		 applicationService.addReferee(application, referee);
+		 
+		 
+		 Assert.notNull(referee, "referee cannot be null");
+		 LOG.debug("referee status : {} ", referee.getType());
+		 
+		 
+		 referee.setName("Mr. Lim");
+		 referee.setOccupation("Lecturer");
+		 referee.setOfficeAddrs("UMK Pengkalan Chepa, Kota Bharu");
+		 referee.setPhoneNo("0123433303");
+		 referee.setType(InRefereeType.SECONDARY);
+		 applicationService.addReferee(application, referee);
 		
+		 Assert.notNull(referee, "referee cannot be null");
+		 LOG.debug("referee status : {} ", referee.getType());
 		 //todo mia : need to add a method to save referee
 		 
 		 return self();
