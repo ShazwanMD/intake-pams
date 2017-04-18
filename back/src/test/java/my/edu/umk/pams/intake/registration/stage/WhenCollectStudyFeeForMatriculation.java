46d862e7a417b242be221c61f1b97274e63a2706
farah <farah@10.3.32.200>
2017-04-18 10:08:58 +0800

    updated code for study fee

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenCollectStudyFeeForMatriculation.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenCollectStudyFeeForMatriculation.java
@@ -1,5 +1,7 @@
 package my.edu.umk.pams.intake.registration.stage;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,11 @@ import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.intake.admission.model.InCandidate;
+import my.edu.umk.pams.intake.admission.service.AdmissionService;
 import my.edu.umk.pams.intake.application.model.InBidStatus;
+import my.edu.umk.pams.intake.application.model.InIntakeApplication;
 import my.edu.umk.pams.intake.application.service.ApplicationService;
 import my.edu.umk.pams.intake.identity.model.InUser;
 import my.edu.umk.pams.intake.policy.model.InIntake;
@@ -24,20 +30,28 @@ private PolicyService policyService;
 @Autowired
 private ApplicationService applicationService;
 
+@Autowired
+private AdmissionService admissionService;
+
 @ProvidedScenarioState
 private InUser user;
 
 @ExpectedScenarioState
 private InIntake intake;
 
+@ExpectedScenarioState
+private InIntakeApplication intakeApplication;
+
+@ProvidedScenarioState
+private List<InCandidate> candidates;
+
 //TODO all study fee pending
 
-	 public WhenCollectStudyFeeForMatriculation I_want_to_collect_study_fee_for_matriculation() {
+	 public WhenCollectStudyFeeForMatriculation I_want_to_collect_study_fee_for_matriculation_$(String referenceNo){
 		 
 		 intake = policyService.findIntakeByReferenceNo(intake.getReferenceNo());
 		 
-		 LOG.debug("intake is : {}", intake);
-	     applicationService.findIntakeApplications(intake, InBidStatus.SUBMITTED); 
+		 
 
 	   
 	     return self();
