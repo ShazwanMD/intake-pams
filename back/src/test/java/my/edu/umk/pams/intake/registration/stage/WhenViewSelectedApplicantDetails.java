9596dc8b5560a1c4302f7b37fdb6f9317874ecf7
Ashraf <Ashraf@MK02PC16>
2017-04-18 08:27:33 +0800

    updated "WhenViewSelectedApplicantDetails"

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenViewSelectedApplicantDetails.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenViewSelectedApplicantDetails.java
@@ -4,64 +4,48 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
 
-import io.jsonwebtoken.lang.Assert;
 import my.edu.umk.pams.intake.admission.model.InCandidate;
-import my.edu.umk.pams.intake.admission.service.AdmissionService;
-import my.edu.umk.pams.intake.application.model.InBidStatus;
-import my.edu.umk.pams.intake.application.model.InIntakeApplication;
-import my.edu.umk.pams.intake.application.service.ApplicationService;
 import my.edu.umk.pams.intake.identity.model.InApplicant;
-import my.edu.umk.pams.intake.identity.service.IdentityService;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.model.InIntakeSession;
-import my.edu.umk.pams.intake.policy.service.PolicyService;
-import my.edu.umk.pams.intake.system.service.SystemService;
 
+@JGivenStage
 public class WhenViewSelectedApplicantDetails extends Stage<WhenViewSelectedApplicantDetails> {
 	
 	 private static final Logger LOG = LoggerFactory.getLogger(WhenViewSelectedApplicantDetails.class);
 	 
-	@Autowired
-    private PolicyService policyService;
-	
-    @Autowired
-    private ApplicationService applicationService;
-
-    @Autowired
-    private SystemService systemService;
-
-    @Autowired
-    private IdentityService identityService;
-    
-    @Autowired
-    private AdmissionService admissionService;
-
-    @ExpectedScenarioState
+	@ExpectedScenarioState
     InIntakeSession intakeSession;
 
     @ExpectedScenarioState
     private InApplicant applicant;
 
-    @ExpectedScenarioState
-    private InIntakeApplication intakeApplication;
-	
 	@ExpectedScenarioState
 	private InIntake intake;
+	
+    @ProvidedScenarioState
+    private List<InCandidate> candidates;
 
 	public WhenViewSelectedApplicantDetails View_selected_applicant_details() {
 		
-		admissionService.preselectIntakeApplication(intakeApplication);
 
-		List<InCandidate> candidates = admissionService.findCandidates(intake);
-		LOG.debug(" test  : {}", candidates);
+		for (InCandidate candidate : candidates) {
+			
+			LOG.debug("");
+			LOG.debug("Candidates Name : {}", candidate.getName());
+			LOG.debug("Candidates Name : {}", candidate.getEmail());
+			LOG.debug("Candidates Name : {}", candidate.getIdentityNo());
+			LOG.debug("Candidates Name : {}", candidate.getStudyMode().getDescriptionEn());
+			LOG.debug("Candidates Name : {}", candidate.getStudyMode().getDescriptionMs());
+			LOG.debug("");
+		}
 		
-		Assert.notEmpty(candidates, "is empty");
+		
 		
 
 		
