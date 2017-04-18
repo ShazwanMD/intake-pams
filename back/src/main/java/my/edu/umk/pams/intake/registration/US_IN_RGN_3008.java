25f1c9e01661b60d89a05517926a8281717cd2d6
Msyahrul <msyahrul@umk.edu.my>
2017-04-18 11:39:05 +0800

    commit PAMI-81 RGN_3008

---
+++ back/src/main/java/my/edu/umk/pams/intake/registration/US_IN_RGN_3008.java
@@ -0,0 +1,50 @@
+package my.edu.umk.pams.intake.registration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+import my.edu.umk.pams.bdd.stage.GivenIAmCPSAdministrator;
+import my.edu.umk.pams.bdd.tags.Issue;
+import my.edu.umk.pams.intake.config.TestAppConfiguration;
+import my.edu.umk.pams.intake.registration.stage.ThenCompleteApplicantRegistration;
+import my.edu.umk.pams.intake.registration.stage.WhenApplicantFillAndSubmitApplication;
+import my.edu.umk.pams.intake.registration.stage.WhenCollectStudyFeeForMatriculation;
+import my.edu.umk.pams.intake.registration.stage.WhenIPreselectApplicant;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+@As("As a CPS academic administrator,  "
+		+ "I want to collect study fees for matriculation, "
+		+ "so that i can complete the applicant's registration")
+
+public class US_IN_RGN_3008 extends SpringScenarioTest <GivenIAmCPSAdministrator,
+WhenApplicantFillAndSubmitApplication, ThenCompleteApplicantRegistration> {
+	
+
+	private static final Logger LOG = LoggerFactory.getLogger(US_IN_RGN_4008.class);
+	
+	private static final String INTAKE_REFERENCE_NO = "201720181/MASTER";
+	private static final String IDENTITY_NO = "248674";
+	
+	@Test
+    @Rollback
+    @Issue("PAMI-81")
+    public void scenario1() {
+    	
+        given().I_am_a_CPS_administrator_in_current_intake_session()
+        .and().I_pick_an_intake_$(INTAKE_REFERENCE_NO);
+        when().I_fill_in_application().and().applicant_submit_application();
+	    addStage(WhenIPreselectApplicant.class).and().I_preselect_applicant_in_intake_session_$(IDENTITY_NO, INTAKE_REFERENCE_NO);
+	    addStage(WhenCollectStudyFeeForMatriculation.class).and().I_want_to_collect_study_fee_for_matriculation_$(INTAKE_REFERENCE_NO);
+	  
+        then().I_can_complete_applicant_registration();
+    }
+}
