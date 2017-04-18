d5f1f41a54b5125475d9c99ff58200327b5cdb43
Ashraf <Ashraf@MK02PC16>
2017-04-18 15:59:44 +0800

    updated "WhenViewListOfSelectedApplicant"

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenViewListOfSelectedApplicant.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/WhenViewListOfSelectedApplicant.java
@@ -5,23 +5,17 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
-
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
-import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 import my.edu.umk.pams.intake.admission.model.InCandidate;
-import my.edu.umk.pams.intake.admission.stage.WhenIWantToSelectSuitableAppealedApplicants;
-import my.edu.umk.pams.intake.application.model.InBidStatus;
+import my.edu.umk.pams.intake.admission.service.AdmissionService;
 import my.edu.umk.pams.intake.application.model.InIntakeApplication;
 import my.edu.umk.pams.intake.application.service.ApplicationService;
-import my.edu.umk.pams.intake.common.model.InProgramCode;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.model.InIntakeSession;
 import my.edu.umk.pams.intake.policy.service.PolicyService;
-import my.edu.umk.pams.intake.policy.stage.WhenIListProgrammes;
 
 @JGivenStage
 public class WhenViewListOfSelectedApplicant extends Stage<WhenViewListOfSelectedApplicant> {
@@ -32,7 +26,7 @@ public class WhenViewListOfSelectedApplicant extends Stage<WhenViewListOfSelecte
     InIntakeSession intakeSession;
 
     @ProvidedScenarioState
-    private InCandidate candidate;
+    private List<InCandidate> candidates;
 
     @ExpectedScenarioState
     private InIntake intake;
@@ -41,20 +35,19 @@ public class WhenViewListOfSelectedApplicant extends Stage<WhenViewListOfSelecte
     private InIntakeApplication intakeApplication;
     
     @Autowired
-    private ApplicationService applicationService;
-    
-    @Autowired
-    private PolicyService policyService;
+    private AdmissionService admissionService;
     
     public WhenViewListOfSelectedApplicant View_List_Of_Selected_Applicant() {
 
-    	intake = policyService.findIntakeByReferenceNo("201720181/MASTER");
-	   	List<InIntakeApplication> intakeApplications  =  applicationService.findIntakeApplications(intake,InBidStatus.SELECTED);
-	   	
-	    for (InIntakeApplication application : intakeApplications) {
-	    	LOG.debug("application : {} ", application.getBidStatus());
-        }
-	    Assert.notNull(intakeApplication, "intakeApplication is empty");
+
+		 List<InCandidate> candidates = admissionService.findCandidates(intake);
+		 
+			for (InCandidate candidate : candidates) {
+				
+				
+			     LOG.debug("this applicant is selected : {}", candidate.getName());
+				
+			}
 	    return self(); 
 	}
 }
