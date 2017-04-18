fc71ed8563f27147f11a9974a309f714fcd68431
Ashraf <Ashraf@MK02PC16>
2017-04-18 16:21:39 +0800

    tested and updated latest "RegistrationModuleTestSuit"

--- back/src/test/java/my/edu/umk/pams/intake/registration/RegistrationModuleTestSuite.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/RegistrationModuleTestSuite.java
@@ -15,20 +15,31 @@ import org.junit.runners.Suite;
         US_IN_RGN_3001.class, //pending
         US_IN_RGN_3002.class, 
         US_IN_RGN_3003.class, //pending
-        US_IN_RGN_3007.class, //incomplete
-        US_IN_RGN_3009.class, //pending
+        US_IN_RGN_3004.class, //pending
+        US_IN_RGN_3006.class, //pending
+        US_IN_RGN_3007.class, 
+        US_IN_RGN_3008.class,
+        US_IN_RGN_3009.class, 
+        US_IN_RGN_3010.class, //pending
         US_IN_RGN_4000.class, //pending
         US_IN_RGN_4001.class, //incomplete
         US_IN_RGN_4002.class,
         US_IN_RGN_4003.class, 
         US_IN_RGN_4004.class, //pending
-        US_IN_RGN_4007.class, //incomplete
+        US_IN_RGN_4006.class, //pending
+        US_IN_RGN_4007.class, 
+        US_IN_RGN_4008.class,
+        US_IN_RGN_4009.class,
+        US_IN_RGN_4010.class, //pending
         US_IN_RGN_4011.class,
         US_IN_RGN_5000.class,
         US_IN_RGN_5001.class,
         US_IN_RGN_5003.class,
+        US_IN_RGN_5004.class, //pending
         US_IN_RGN_6000.class,
         US_IN_RGN_6001.class,
+        US_IN_RGN_6003.class,
+        US_IN_RGN_6005.class,
 })
 
 public class RegistrationModuleTestSuite {
