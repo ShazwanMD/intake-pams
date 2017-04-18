4add58edabef1e6d456f6f1e7da35744757356e3
Ashraf <Ashraf@MK02PC16>
2017-04-18 08:26:53 +0800

    updated "ThenProceedRegistrationForSelectedApplicants" to complete US_IN_RGN_6001

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/ThenProceedRegistrationForSelectedApplicants.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/ThenProceedRegistrationForSelectedApplicants.java
@@ -1,5 +1,7 @@
 package my.edu.umk.pams.intake.registration.stage;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +9,10 @@ import org.springframework.util.Assert;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
-import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 
 import my.edu.umk.pams.intake.admission.model.InCandidate;
-import my.edu.umk.pams.intake.admission.model.InCandidateStatus;
 import my.edu.umk.pams.intake.admission.service.AdmissionService;
 import my.edu.umk.pams.intake.application.model.InBidStatus;
 import my.edu.umk.pams.intake.application.model.InIntakeApplication;
@@ -44,14 +44,22 @@ public class ThenProceedRegistrationForSelectedApplicants extends Stage<ThenProc
 	
     @ProvidedScenarioState
     private InIntakeApplication application;
+    
+    @ExpectedScenarioState
+    private List<InCandidate> candidates;
 	 
 	 public ThenProceedRegistrationForSelectedApplicants Proceed_Registration_For_Selected_Applicants() {
 		 
-		 //Assert.notNull(applicant, "applicant is required");
-		// application.setBidStatus(InBidStatus.SELECTED);
+
+			for (InCandidate candidate : candidates) {
+				
+				 Assert.notNull(InBidStatus.SELECTED, "candidate is not selected");
+			     LOG.debug("intake application status is {}", candidate.getStatus());
+				
+			}
+			
 		
-		 Assert.notNull(InBidStatus.SELECTED, "withdraw application is null");
-	     LOG.debug("intake application status is {}", application.getBidStatus());
+
 		 
 		
 		 return self();
