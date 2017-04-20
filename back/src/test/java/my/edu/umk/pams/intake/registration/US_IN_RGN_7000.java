eb94e1e815b4ae07dd8745c8c99b04fd40daa081
farah <farah@10.3.32.200>
2017-04-20 11:56:26 +0800

    add US_IN_RGN_7000 (in progress)

---
+++ back/src/test/java/my/edu/umk/pams/intake/registration/US_IN_RGN_7000.java
@@ -0,0 +1,45 @@
+package my.edu.umk.pams.intake.registration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.bdd.stage.GivenIAmInternationalOfficer;
+import my.edu.umk.pams.bdd.tags.Issue;
+import my.edu.umk.pams.intake.config.TestAppConfiguration;
+import my.edu.umk.pams.intake.registration.stage.ThenProceedRegistrationForSelectedApplicants;
+import my.edu.umk.pams.intake.registration.stage.ThenValidateTheApplication;
+import my.edu.umk.pams.intake.registration.stage.WhenPrepareApplicationSubmission;
+import my.edu.umk.pams.intake.registration.stage.WhenViewApplicationWithInternationalAcademicQualification;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+@As("As an International Officer, "
+		+ "I want to view applications with an international academic qualification, "
+		+ "so that I can validate the application")
+
+public class US_IN_RGN_7000 extends
+SpringScenarioTest<GivenIAmInternationalOfficer, WhenViewApplicationWithInternationalAcademicQualification, ThenValidateTheApplication> {
+	
+	 public static final String INTAKE_REFERENCE_NO = "201720181/MASTER";
+	 
+	 @Issue ("PAMI-107")
+	 @Test
+	 @Rollback
+	 public void scenario1() {
+		 
+		 given().I_am_a_International_Officer_in_current_intake_session()   
+		 .and().I_pick_an_intake_$(INTAKE_REFERENCE_NO);
+		 when().I_view_application_with_international_academic_qualification();
+		 then().I_can_validate_the_application();
+	 }
+	
+
+}
