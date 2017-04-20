8c1969e7e1c7367ea6ce989ae515f6f84fc60b15
farah <farah@10.3.32.200>
2017-04-20 11:22:39 +0800

    commit US_IN_RGN_3005

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/ThenSubmitResultOfSelection.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/ThenSubmitResultOfSelection.java
@@ -5,27 +5,30 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+
 import my.edu.umk.pams.intake.admission.model.InCandidate;
 import my.edu.umk.pams.intake.admission.service.AdmissionService;
+import my.edu.umk.pams.intake.application.model.InBidStatus;
 import my.edu.umk.pams.intake.application.model.InIntakeApplication;
-import my.edu.umk.pams.intake.application.service.ApplicationService;
+import my.edu.umk.pams.intake.application.stage.ThenICanSubmitMyApplication;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.model.InIntakeSession;
-import my.edu.umk.pams.intake.policy.service.PolicyService;
 
 @JGivenStage
-public class WhenViewListOfSelectedApplicant extends Stage<WhenViewListOfSelectedApplicant> {
+public class ThenSubmitResultOfSelection extends Stage<ThenSubmitResultOfSelection> {
 	
-	private static final Logger LOG = LoggerFactory.getLogger(WhenViewListOfSelectedApplicant.class);
-	
+	private static final Logger LOG = LoggerFactory.getLogger(ThenSubmitResultOfSelection.class);
+
 	@ExpectedScenarioState
     InIntakeSession intakeSession;
 
-    @ProvidedScenarioState
+	@ExpectedScenarioState
     private List<InCandidate> candidates;
 
     @ExpectedScenarioState
@@ -36,34 +39,22 @@ public class WhenViewListOfSelectedApplicant extends Stage<WhenViewListOfSelecte
     
     @Autowired
     private AdmissionService admissionService;
-    
-    public WhenViewListOfSelectedApplicant View_List_Of_Selected_Applicant() {
-
-
-		 List<InCandidate> candidates = admissionService.findCandidates(intake);
-		 
-			for (InCandidate candidate : candidates) {
-				
-				
-			     LOG.debug("this applicant is selected : {}", candidate.getName());
-				
-			}
-	    return self(); 
-	}
-    
-    public WhenViewListOfSelectedApplicant broadcast_result_of_Selected_Applicant() {
     	
-    	 List<InCandidate> candidates = admissionService.findCandidates(intake);
+	 public ThenSubmitResultOfSelection I_can_submit_the_result() {
 		 
+
 			for (InCandidate candidate : candidates) {
 				
-				
-			     LOG.debug("the selected applicant is : {} {} ", candidate.getName(), candidate.getIdentityNo());
+				Assert.notNull(InBidStatus.SELECTED, "candidate is not selected");
+			 
 				
 			}
-	    return self(); 
-    }
+			
+		
+
+		 
+		
+		 return self();
+	 }
 
 }
-
-	
