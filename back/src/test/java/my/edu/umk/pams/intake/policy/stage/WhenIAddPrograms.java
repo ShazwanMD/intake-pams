2550090bda2bccf08183b869074c03e5c8a40f48
ashraf <ashraf@umk.edu.my>
2017-04-21 11:22:58 +0800

    fixed merge conflicts

--- back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenIAddPrograms.java
+++ back/src/test/java/my/edu/umk/pams/intake/policy/stage/WhenIAddPrograms.java
@@ -5,7 +5,6 @@ import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 
-import my.edu.umk.pams.intake.common.model.InFacultyCode;
 import my.edu.umk.pams.intake.common.model.InProgramCode;
 import my.edu.umk.pams.intake.common.model.InProgramCodeImpl;
 import my.edu.umk.pams.intake.common.service.CommonService;
