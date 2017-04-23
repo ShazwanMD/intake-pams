3a208d095a5673510a4c4bb320da69833703a4a4
samiya <samiya@10.3.32.157>
2017-04-23 15:42:11 +0800

    update then stage to completed US_IN_RGN_4007 and RGN_3007

--- back/src/test/java/my/edu/umk/pams/intake/registration/stage/ThenICanCompleteStudentRegistration.java
+++ back/src/test/java/my/edu/umk/pams/intake/registration/stage/ThenICanCompleteStudentRegistration.java
@@ -1,5 +1,7 @@
 package my.edu.umk.pams.intake.registration.stage;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +9,15 @@ import org.springframework.util.Assert;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
-import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 
-import my.edu.umk.pams.intake.common.model.InStudyCenterCode;
-import my.edu.umk.pams.intake.identity.model.InUser;
+import my.edu.umk.pams.intake.admission.model.InCandidate;
+import my.edu.umk.pams.intake.admission.service.AdmissionService;
+import my.edu.umk.pams.intake.application.model.InIntakeApplication;
+import my.edu.umk.pams.intake.application.service.ApplicationService;
 import my.edu.umk.pams.intake.policy.model.InIntake;
 import my.edu.umk.pams.intake.policy.service.PolicyService;
-import my.edu.umk.pams.intake.registration.service.RegistrationService;
 
 @JGivenStage
 public class ThenICanCompleteStudentRegistration extends Stage <ThenICanCompleteStudentRegistration> {
@@ -23,17 +25,34 @@ public class ThenICanCompleteStudentRegistration extends Stage <ThenICanComplete
 	public static final Logger LOG = LoggerFactory.getLogger(ThenICanCompleteStudentRegistration.class);
 
 	@Autowired
-	private PolicyService policyService;
- 
+	private AdmissionService admissionService;
+
+    @ExpectedScenarioState
+    List<InIntakeApplication>  applications;
+    
+    @Autowired
+    ApplicationService applicationService;
+
 	@ExpectedScenarioState
-	private InIntake intake;
+	private InIntakeApplication intakeApplication;
+	
+	@ExpectedScenarioState
+    private InIntake intake; 
+    
+	@ExpectedScenarioState
+    private List<InCandidate> candidates;
+	
+	@ExpectedScenarioState
+	private InCandidate candidate;
     
     public ThenICanCompleteStudentRegistration I_can_complete_student_registration() {
-		
-    	intake.getReferenceNo();
     	
     	Assert.notNull(intake, "intake cannot be empty");
     	
-		return self();
+    	InCandidate candidate1 = admissionService.findCandidateByIdentityNo("910607145581");
+    	LOG.debug("candidate's status of registeration : {} ", candidate1.getRegistration());
+    	
+    	Assert.isTrue(true, "Candidate is not registered");
+    	return self();
 		}
 }
