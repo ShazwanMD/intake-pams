0a1d8d3defd3a90ac3b0e52d525abc0704ae6a22
Max Zain <max.zain@canang.com.my>
2017-04-30 22:30:50 +0800

    Let Spring Boot autoconfigures - remove @EnableWebMvc Send NOT_FOUND requests to /

--- back/src/main/java/my/edu/umk/pams/intake/config/WebConfiguration.java
+++ back/src/main/java/my/edu/umk/pams/intake/config/WebConfiguration.java
@@ -1,7 +1,11 @@
 package my.edu.umk.pams.intake.config;
 
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,8 +15,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurerAdapter  {
+
+    @Bean
+    public EmbeddedServletContainerCustomizer notFoundCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+            }
+        };
+    }
 
     @Bean
     public ViewResolver internalResourceViewResolver() {
