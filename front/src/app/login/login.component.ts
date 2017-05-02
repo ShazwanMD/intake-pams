82f5fb9c5172b8bc56c66f1689d99df786309093
farah <farah>
2017-05-02 17:01:20 +0800

    add forget_password in Login page

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -24,4 +24,14 @@ export class LoginComponent {
       this._loadingService.resolve();
     }, 2000);
   }
+  
+   forgetPassword() : void {
+    this._loadingService.register();
+    alert('reset new password' + this.password);
+    setTimeout(() => {
+      this._router.navigate(['/']);
+      this._loadingService.resolve();
+    }, 2000);
+  }
+
 }
