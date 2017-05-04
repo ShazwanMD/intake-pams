01d727792bf63aec22481d84f9b97160fa76da37
Msyahrul <msyahrul@umk.edu.my>
2017-05-04 11:13:17 +0800

    GenderCodes

---
+++ back/src/main/java/my/edu/umk/pams/intake/web/module/common/vo/GenderCode.java
@@ -0,0 +1,53 @@
+package my.edu.umk.pams.intake.web.module.common.vo;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import my.edu.umk.pams.intake.web.module.core.vo.MetaObject;
+
+import java.io.IOException;
+
+/**
+ * @author PAMS
+ */
+public class GenderCode extends MetaObject {
+    private String code;
+    private String descriptionMs;
+    private String descriptionEn;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescriptionMs() {
+        return descriptionMs;
+    }
+
+    public void setDescriptionMs(String descriptionMs) {
+        this.descriptionMs = descriptionMs;
+    }
+
+    public String getDescriptionEn() {
+        return descriptionEn;
+    }
+
+    public void setDescriptionEn(String descriptionEn) {
+        this.descriptionEn = descriptionEn;
+    }
+
+
+    @JsonCreator
+    public static GenderCode create(String jsonString) {
+        GenderCode o = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            o = mapper.readValue(jsonString, GenderCode.class);
+        } catch (IOException e) {
+            // handle
+        }
+        return o;
+    }
+}
