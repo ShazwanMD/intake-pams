7cdf52f5d620226c5e592fedba4d0820491f7a66
Ashraf <Ashraf@10.3.32.43>
2017-05-07 17:40:46 +0800

    updated InReligionCodeDao.Impl

--- back/src/main/java/my/edu/umk/pams/intake/common/dao/InReligionCodeDaoImpl.java
+++ back/src/main/java/my/edu/umk/pams/intake/common/dao/InReligionCodeDaoImpl.java
@@ -37,13 +37,15 @@ public class InReligionCodeDaoImpl extends GenericDaoSupport<Long, InReligionCod
         Session session = sessionFactory.getCurrentSession();
         Query query = session.createQuery("select s from InReligionCode s where " +
                 "(upper(s.code) like upper(:filter) " +
-                "or upper(s.description) like upper(:filter)) " +
+                "or upper(s.descriptionMs) like upper(:filter)) " +
+                "or upper(s.descriptionEn) like upper(:filter)) " +
                 "and s.metadata.state = :state ");
         query.setString("filter", WILDCARD + filter + WILDCARD);
         query.setInteger("state", InMetaState.ACTIVE.ordinal());
         query.setFirstResult(offset);
         query.setMaxResults(limit);
         query.setCacheable(true);
+        
         return (List<InReligionCode>) query.list();
 
     }
