c001e569773543ff6f7e94825f1ee5aa79449107
samiya <samiya@10.3.32.157>
2017-05-07 17:22:58 +0800

    commit index in intake-sessions

--- front/src/app/policy/intake-sessions/index.ts
+++ front/src/app/policy/intake-sessions/index.ts
@@ -5,7 +5,6 @@ import {appRoutes, appRoutingProviders} from '../../app.routes';
 import {environment} from '../../../environments/environment';
 
 import {CovalentCoreModule} from '@covalent/core';
-
 import {EffectsModule} from "@ngrx/effects";
 import {CommonService} from '../../../services';
 import {IdentityService} from '../../../services';
@@ -14,6 +13,7 @@ import {CommonModule} from "../../common/index";
 import {CommonActions} from "../../common/common.action";
 import {IntakeSessionActions} from "../intake-sessions/intake-session.action";
 import {IntakeSessionEffects} from "./intake-session.effect";
+import {IntakeSessionCenterPage} from "./intake-session-center.page";
 import {IntakeSessionSelectComponent} from "./component/intake-session-select.component";
 
 
@@ -29,6 +29,7 @@ import {IntakeSessionSelectComponent} from "./component/intake-session-select.co
   declarations: [
     IntakeSessionSelectComponent,
     // page
+    IntakeSessionCenterPage,
     // components
     // panels
     // dialogs
