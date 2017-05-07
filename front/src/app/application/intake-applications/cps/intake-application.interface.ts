79d576f9e906c4a41d16276b6534fae1c229946c
'hanifnoordin' <hanifnoordin@umk.edu.my>
2017-05-07 13:04:29 +0800

    draft cps application form

--- front/src/app/application/intake-applications/cps/intake-application.interface.ts
+++ front/src/app/application/intake-applications/cps/intake-application.interface.ts
@@ -1,7 +1,4 @@
 import {ProgramLevel} from "../../../policy/program-levels/program-level.interface";
 export interface IntakeApplicationPersonal{
-  name:string;
-  identityNo:string;
-  birthdate:Date;
-  programLevel:ProgramLevel;
+
 }
