013a5b28ebd88dc42bccee008df677d90a70b309
samiya <samiya>
2017-05-07 17:26:58 +0800

    update

--- front/src/app/policy/intake-sessions/intake-session.action.ts
+++ front/src/app/policy/intake-sessions/intake-session.action.ts
@@ -21,4 +21,36 @@ export class IntakeSessionActions {
       payload: sessions
     };
   }
+
+  static FIND_ASSIGNED_INTAKE_SESSIONS = '[Common] Find Assigned Intake Sessions';
+
+  findAssignedIntakeSessions(sessions): Action {
+    console.log("findAssignedIntakeSessions");
+    return {
+      type: IntakeSessionActions.FIND_ASSIGNED_INTAKE_SESSIONS,
+      payload: sessions
+    };
+  }
+
+  static FIND_ASSIGNED_INTAKE_SESSION_TASKS = '[Common] Find Assigned Intake Session Tasks';
+
+  findAssignedIntakeSessionTasks(sessions): Action {
+    console.log("findAssignedIntakeSessionTasks");
+    return {
+      type: IntakeSessionActions.FIND_ASSIGNED_INTAKE_SESSION_TASKS,
+      payload: sessions
+    };
+  }
+
+  //findIntakeSessionTaskByTaskId
+   static FIND_INTAKE_SESSIONS_TASK_BY_TASK_ID = '[Common] Find Intake Sessions Task';
+
+  findIntakeSessionTaskByTaskId(taskId): Action {
+    console.log("findIntakeSessionTaskByTaskId");
+    return {
+      type: IntakeSessionActions.FIND_INTAKE_SESSIONS_TASK_BY_TASK_ID,
+      payload: taskId
+    };
+  }
+
 }
