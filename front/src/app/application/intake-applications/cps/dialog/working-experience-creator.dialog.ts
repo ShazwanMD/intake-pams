1515da967bae5fc4b134bf5b396947922ebce74a
'hanifnoordin' <hanifnoordin@umk.edu.my>
2017-05-07 12:56:16 +0800

    commit conflict

--- front/src/app/application/intake-applications/cps/dialog/working-experience-creator.dialog.ts
+++ front/src/app/application/intake-applications/cps/dialog/working-experience-creator.dialog.ts
@@ -5,7 +5,8 @@ import {Router, ActivatedRoute} from '@angular/router';
 import {Store} from "@ngrx/store";
 import {ApplicationModuleState} from "../../../index";
 import {IntakeApplication} from "../../intake-application.interface";
-import {MdDialogRef} from "@angular/material";
+import { MdDialogRef } from "@angular/material";
+import { IntakeApplicationPersonal } from "../intake-application.interface";
 
 
 @Component({
@@ -26,6 +27,28 @@ export class WorkingExperienceCreatorDialog implements OnInit {
   }
 
   ngOnInit(): void {
+    this.createForm = this.formBuilder.group(<IntakeApplicationPersonal>{
+      id: null,
+      //referenceNo: '',
+     // sourceNo: '',
+     // intakeNo: '',
+     // description: '',
+     // projection: 0,
+     // startDate: null,
+     // endDate: null,
+     // programLevel: <ProgramLevel>{},
+      //intakeSession: <IntakeSession>{},
+      //graduateCentre: <GraduateCentre>{}
+    });
+  }
+
+  save(intake: Intake, isValid: boolean) {
+    console.log("intake: " + intake.description);
+    console.log("program level: " + intake.programLevel.code);
+    console.log("session " + intake.intakeSession.code);
+    console.log("graduate centre " + intake.graduateCentre.code);
+    this.store.dispatch(this.actions.startIntakeTask(intake));
+    this.dialog.close();
   }
 
   expandedEvent(): void {
