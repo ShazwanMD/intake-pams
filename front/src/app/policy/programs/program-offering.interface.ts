11dc1ee86ee750c51bbdf6d9e8b75a521ad403c5
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-05-08 11:50:18 +0800

    Remove unused sub module

--- front/src/app/policy/programs/program-offering.interface.ts
+++
@@ -1,8 +0,0 @@
-import {MetaObject} from "../../core/meta-object.interface";
-import {ProgramCode} from "../../common/program-codes/program-code.interface";
-
-export interface ProgramOffering extends MetaObject {
-  projection:number;
-  interview:boolean;
-  programCode:ProgramCode;
-}
