9ac562a6794dcc62c01aaf89afde25aef22d1ef6
Max Zain <max.zain@canang.com.my>
2017-05-11 21:18:42 +0800

    Move customizer bean to a more appropriate location

--- back/src/main/java/my/edu/umk/pams/intake/Application.java
+++ back/src/main/java/my/edu/umk/pams/intake/Application.java
@@ -2,11 +2,26 @@ package my.edu.umk.pams.intake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication()
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer notFoundCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+            }
+        };
     }
 }
--- back/src/main/java/my/edu/umk/pams/intake/config/WebConfiguration.java
+++ back/src/main/java/my/edu/umk/pams/intake/config/WebConfiguration.java
@@ -1,31 +1,16 @@
 package my.edu.umk.pams.intake.config;
 
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class WebConfiguration extends WebMvcConfigurerAdapter  {
-
-    @Bean
-    public EmbeddedServletContainerCustomizer notFoundCustomizer() {
-        return new EmbeddedServletContainerCustomizer() {
-            @Override
-            public void customize(ConfigurableEmbeddedServletContainer container) {
-                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
-            }
-        };
-    }
+public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
     public ViewResolver internalResourceViewResolver() {
@@ -36,7 +21,7 @@ public class WebConfiguration extends WebMvcConfigurerAdapter  {
     }
 
     @Bean
-    public CommonsMultipartResolver multipartResolver(){
+    public CommonsMultipartResolver multipartResolver() {
         CommonsMultipartResolver resolver = new CommonsMultipartResolver();
         return resolver;
     }
