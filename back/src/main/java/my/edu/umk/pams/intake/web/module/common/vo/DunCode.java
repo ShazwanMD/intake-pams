77f4a93496ea668614a393a85220c61a20934ba1
Msyahrul <msyahrul@umk.edu.my>
2017-05-14 09:12:21 +0800

    Commit DUN_CODE vo

---
+++ back/src/main/java/my/edu/umk/pams/intake/web/module/common/vo/DunCode.java
@@ -0,0 +1,54 @@
+package my.edu.umk.pams.intake.web.module.common.vo;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import my.edu.umk.pams.intake.web.module.core.vo.MetaObject;
+
+import java.io.IOException;
+
+/**
+ * @author PAMS
+ */
+public class DunCode extends MetaObject {
+    private String code;
+    private String description;
+    //private String descriptionEn;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //public String getDescriptionEn() {
+    //    return descriptionEn;
+    //}
+
+    //public void setDescriptionEn(String descriptionEn) {
+    //    this.descriptionEn = descriptionEn;
+    //}
+    
+    
+
+    @JsonCreator
+    public static DunCode create(String jsonString) {
+        DunCode o = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            o = mapper.readValue(jsonString, DunCode.class);
+        } catch (IOException e) {
+            // handle
+        }
+        return o;
+    }
+}
