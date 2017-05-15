1b6b3bc01beddae472fb9638f1cf94fcc371c4a6
hanifnoordin <hanifnoordin@umk.edu.my>
2017-05-15 17:10:14 +0800

    draft cps application form

--- front/src/app/application/intake-applications/cps/intake-application.interface.ts
+++ front/src/app/application/intake-applications/cps/intake-application.interface.ts
@@ -1,10 +1,52 @@
+import { Intake } from './../../../policy/intakes/intake.interface';
+import { Applicant } from './../../../identity/applicant.interface';
+import { ProgramOffering } from './../../../policy/intakes/program-offering.interface';
+import { ReligionCode } from './../../../common/religion-codes/religion-code.interface';
+import { GenderCode } from './../../../common/gender-codes/gender-code.interface';
+import { EthnicityCode } from './../../../common/ethnicity-codes/ethnicity-code.interface';
 import {ProgramLevel} from "../../../policy/program-levels/program-level.interface";
-export interface IntakeApplication{
+import { StudyMode } from "../../../common/study-modes/study-mode.interface";
+import { DisabilityCode } from "../../../common/disability-codes/disability-code.interface";
+import { RaceCode } from "../../../common/race-codes/race-code.interface";
+export interface IntakeApplication {
   id: number;
-  startDate: Date;
-  endDate: Date;
-  employer:String;
-
+  rank: number;
+  merit: number;
+  referenceNo: String;
+  name: String;
+  email: String;
+  phone: String;
+  fax: String;
+  credentialNo: String;
+  okuNo: String;
+  paymentSourceNo: String;
+  age: number;
+  //schoolCode: SchoolCode;
+  schoolName: String;
+  schoolBatch: number;
+  reason: String;
+  passport: String;
+  passExpDate: Date;
+  visaType: number;
+  bidType: number;
+  bidStatus: number;
+  bidResponse: number;
+  studyMode: StudyMode;
+  disabilityCode: DisabilityCode;
+  raceCode: RaceCode;
+  ethnicityCode: EthnicityCode;
+  genderCode: GenderCode;
+  religionCode: ReligionCode;
+  programSelection: ProgramOffering;
+  intake: Intake;
+  applicant: Applicant;
+  //results: Results;
+  //educations: Educations;
+  //employments: Employments;
+  //addresses: Addresses;
+  //referees: Referees;
+  //attachments: Attachments;
   // transient
-  current?:boolean;
+  paid?:boolean;
+  verification?:boolean;
 }
