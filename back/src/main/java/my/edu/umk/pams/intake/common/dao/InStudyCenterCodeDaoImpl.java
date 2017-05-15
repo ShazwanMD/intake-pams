2cafe3738f3128efa0ad3f03a7c92dc9ab311fc0
Ashraf <Ashraf@10.3.32.91>
2017-05-15 16:29:44 +0800

    updated InStudyCenterDaoImpl to fix error in controller

--- back/src/main/java/my/edu/umk/pams/intake/common/dao/InStudyCenterCodeDaoImpl.java
+++ back/src/main/java/my/edu/umk/pams/intake/common/dao/InStudyCenterCodeDaoImpl.java
@@ -38,7 +38,8 @@ public class InStudyCenterCodeDaoImpl extends GenericDaoSupport<Long, InStudyCen
         Session session = sessionFactory.getCurrentSession();
         Query query = session.createQuery("select s from InStudyCenterCode s where " +
                 "(upper(s.code) like upper(:filter) " +
-                "or upper(s.description) like upper(:filter)) " +
+                "or upper(s.descriptionEn) like upper(:filter) " +
+                "or upper(s.descriptionMs) like upper(:filter)) " +
                 "and s.metadata.state = :state ");
         query.setString("filter", WILDCARD + filter + WILDCARD);
         query.setInteger("state", InMetaState.ACTIVE.ordinal());
