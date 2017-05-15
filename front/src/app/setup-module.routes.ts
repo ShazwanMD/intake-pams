099c5dc24fd13c86c901c8e360da27f625541743
ashraf <ashraf@umk.edu.my>
2017-05-15 20:34:20 +0800

    Added everything involving Study Center Code in setup interface (display data, and dialogues for add, remove and edit)

---
+++ front/src/app/setup-module.routes.ts
@@ -0,0 +1,59 @@
+<<<<<<< HEAD
+import { StudyCenterCodeListPage } from './study-center-codes/study-center-code-list.page';
+import { StudyCenterCode } from './../common/study-center-codes/study-center-code.interface';
+=======
+import { SchoolCodeListPage } from './school-codes/school-code-list-page';
+import { SchoolCode } from './../common/school-codes/school-code.interface';
+>>>>>>> ad022055cffdbc8455da9ca06fd0bdd5822a8b9b
+import { DisabilityCodeListPage } from './disability-codes/disability-code-list-page';
+import { DisabilityCode } from './../common/disability-codes/disability-code.interface';
+import {StudyModeListPage} from './study-modes/study-mode-list.page';
+import {FacultyCodeListPage} from './faculty-codes/faculty-code-list.page';
+import {NationalityCodeListPage} from './nationality-codes/nationality-code-list.page';
+import {EthnicityCodeListPage} from './ethnicity-codes/ethnicity-code-list.page';
+import {GenderCodeListPage} from './gender-codes/gender-code-list.page';
+import {RaceCodeListPage} from './race-codes/race-code-list.page';
+import {SupervisorCodeListPage} from './supervisor-codes/supervisor-code-list.page';
+import {ProgramCodeListPage} from './program-codes/program-code-list.page';
+import {StateCodeListPage} from './state-codes/state-code-list.page';
+import {MaritalCodeListPage} from './marital-codes/marital-code-list-page';
+import {ReligionCodeListPage} from './religion-codes/religion-code-list.page';
+import {SetupModule} from './index';
+import {Routes, RouterModule} from '@angular/router';
+import {SetupPage} from "./setup.page";
+import {BankCodeListPage} from "./bank-codes/bank-code-list.page";
+import {GraduateCentreListPage} from "./graduate-centres/graduate-centre-list.page";
+import {CountryCodeListPage} from './country-codes/country-code-list.page';
+
+
+
+
+
+export const SetupModuleRoutes: Routes = [
+  {
+    path: 'setup', component: SetupPage,
+    children: [
+      {path: 'bank-codes', component: BankCodeListPage},
+      {path: 'graduate-centres', component: GraduateCentreListPage},
+      {path: 'religion-codes', component: ReligionCodeListPage},
+      {path: 'marital-codes', component: MaritalCodeListPage},
+      {path: 'country-codes', component: CountryCodeListPage},
+      {path: 'state-codes', component: StateCodeListPage},
+      {path: 'program-codes', component: ProgramCodeListPage},
+      {path: 'supervisor-codes', component: SupervisorCodeListPage},
+      {path: 'race-codes', component: RaceCodeListPage},
+      {path: 'gender-codes', component: GenderCodeListPage},
+      {path: 'ethnicity-codes', component: EthnicityCodeListPage},
+      {path: 'nationality-codes', component: NationalityCodeListPage},
+      {path: 'faculty-codes', component: FacultyCodeListPage},
+      {path: 'study-modes', component: StudyModeListPage},
+      {path: 'disability-codes', component: DisabilityCodeListPage},
+<<<<<<< HEAD
+      {path: 'study-centre-codes', component: StudyCenterCodeListPage},
+=======
+      {path: 'school-codes', component: SchoolCodeListPage},
+>>>>>>> ad022055cffdbc8455da9ca06fd0bdd5822a8b9b
+    ]
+  },
+
+];
---
+++ front/src/app/study-center-codes/dialogs/study-center-code-editor.dialog.ts
@@ -0,0 +1,53 @@
+import { StudyCenterCode } from './../../../common/study-center-codes/study-center-code.interface';
+import {Component, ViewContainerRef, OnInit, AfterViewInit} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {Store} from "@ngrx/store";
+import {MdDialogRef} from "@angular/material";
+import {SetupModuleState} from "../../index";
+import {SetupActions} from "../../setup.action";
+
+
+@Component({
+  selector: 'pams-study-center-code-editor',
+  templateUrl: './study-center-code-editor.dialog.html',
+})
+
+export class StudyCenterCodeEditorDialog implements OnInit {
+
+  private editorForm: FormGroup;
+  private edit: boolean = false;
+  private _studyCenterCode: StudyCenterCode;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private formBuilder: FormBuilder,
+              private viewContainerRef: ViewContainerRef,
+              private dialog: MdDialogRef<StudyCenterCodeEditorDialog>,
+              private store: Store<SetupModuleState>,
+              private actions: SetupActions) {
+  }
+
+  set studyCenterCode(value: StudyCenterCode) {
+    this._studyCenterCode = value;
+    this.edit = true;
+  }
+
+  ngOnInit(): void {
+    this.editorForm = this.formBuilder.group(<StudyCenterCode>{
+      id: null,
+      code: '',
+      descriptionEn: '',
+      descriptionMs: '',
+    });
+
+    if (this.edit) this.editorForm.patchValue(this._studyCenterCode);
+  }
+
+  submit(code: StudyCenterCode, isValid: boolean) {
+    if (!code.id) this.store.dispatch(this.actions.saveStudyCenterCode(code));
+    else  this.store.dispatch(this.actions.updateStudyCenterCode(code));
+    this.dialog.close();
+  }
+}
