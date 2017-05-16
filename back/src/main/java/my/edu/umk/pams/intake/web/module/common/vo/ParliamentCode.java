87a1592118544a860f4ba54589256fcd36713eb6
Msyahrul <msyahrul@umk.edu.my>
2017-05-16 15:04:39 +0800

    ParliamentCodes

---
+++ back/src/main/java/my/edu/umk/pams/intake/web/module/common/vo/ParliamentCode.java
@@ -0,0 +1,43 @@
+package my.edu.umk.pams.intake.web.module.common.vo;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import my.edu.umk.pams.intake.web.module.core.vo.MetaObject;
+
+import java.io.IOException;
+
+/**
+ * @author PAMS
+ */
+public class ParliamentCode extends MetaObject {
+    private String code;
+    private String description;
+        public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+    @JsonCreator
+    public static ParliamentCode create(String jsonString) {
+        ParliamentCode o = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            o = mapper.readValue(jsonString, ParliamentCode.class);
+        } catch (IOException e) {
+            // handle
+        }
+        return o;
+    }
+}
