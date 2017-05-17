7c291eed8c37462576a851b338f036abfc50c851
farah <farah>
2017-05-17 15:44:29 +0800

    add spm subject

--- back/src/main/java/my/edu/umk/pams/intake/application/model/InSpmResult.java
+++ back/src/main/java/my/edu/umk/pams/intake/application/model/InSpmResult.java
@@ -9,11 +9,48 @@ public interface InSpmResult extends InResult {
 
     void setYear(Integer year);
 
-    String getRegistrationNo();
-
-    void setRegistrationNo(String registrationNo);
-
     Integer getAggregate();
 
     void setAggregate(Integer aggregate);
+
+	void SetMalay(String malay);
+
+	String getMalay();
+
+	String getEnglish();
+
+	void SetEnglish(String english);
+
+	String getMath();
+
+	void setMath(String math);
+
+	void setAddmath(String addMath);
+
+	String getAddMath();
+
+	void setIslamEduc(String islamEduc);
+
+	String getIslamEduc();
+
+	void setChemist(String chemist);
+
+	String getChemist();
+
+	void setPhysic(String physic);
+
+	String getPhysic();
+
+	void setBiology(String bio);
+
+	String getBiology();
+
+	void setHistory(String history);
+
+	String getHistory();
+
+	
+
+
+	
 }
