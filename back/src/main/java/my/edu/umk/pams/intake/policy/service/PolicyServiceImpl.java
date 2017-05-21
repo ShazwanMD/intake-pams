9be86e1c54c5925209bec6d3de5ae7907a6287e4
'hanifnoordin' <hanifnoordin@umk.edu.my>
2017-05-21 15:37:18 +0800

    commit merge

--- back/src/main/java/my/edu/umk/pams/intake/policy/service/PolicyServiceImpl.java
+++ back/src/main/java/my/edu/umk/pams/intake/policy/service/PolicyServiceImpl.java
@@ -287,8 +287,8 @@ public class PolicyServiceImpl implements PolicyService {
         Validate.notNull(intake, "Intake cannot be null");
         Validate.notNull(programOffering, "Offering cannot be null");
         Validate.isTrue(DRAFTED.equals(intake.getFlowdata().getState()), "Intake can only be configured in DRAFTED state");
-        programOffering.setGeneralCriteria(generalCriteria());
-        programOffering.setSpecificCriteria(specificCriteria());
+        programOffering.setGeneralCriteria(programOffering.getGeneralCriteria());
+        programOffering.setSpecificCriteria(programOffering.getSpecificCriteria());
         intakeDao.updateProgramOfferings(intake, programOffering, securityService.getCurrentUser());
     }
 
