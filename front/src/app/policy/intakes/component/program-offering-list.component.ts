85a6993055e23ca12a137c60248df553f6861623
samiya <samiya@10.3.32.157>
2017-05-22 11:59:52 +0800

    update to show editor dialog box

--- front/src/app/policy/intakes/component/program-offering-list.component.ts
+++ front/src/app/policy/intakes/component/program-offering-list.component.ts
@@ -7,6 +7,8 @@ import {PolicyModuleState} from "../../index";
 import {Store} from "@ngrx/store";
 import {MdDialogConfig, MdDialog, MdDialogRef} from "@angular/material";
 import {ProgramOfferingEditorDialog} from "../dialog/program-offering-editor.dialog";
+import {ProgramOfferingListEditorDialog} from "./program-offering-list-editor.dialog";
+
 import { Observable } from "rxjs/Rx";
 
 @Component({
@@ -15,25 +17,52 @@ import { Observable } from "rxjs/Rx";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProgramOfferingListComponent {
+  
 
   @Input() intake: Intake;
   @Input() programOfferings: ProgramOffering[];
 
+  programOfferings$: Observable<ProgramOffering[]>;
+
   private editorDialogRef: MdDialogRef<ProgramOfferingEditorDialog>;
+  private editorDialogRef2: MdDialogRef<ProgramOfferingListEditorDialog>;
   private INTAKE = "policyModuleState.intake".split(".");
+  private PROGRAM_OFFERINGS = "policyModuleState.programOfferings".split(".");
   private intake$:Observable<Intake>;
+ 
 
   constructor(private store: Store<PolicyModuleState>,
               private actions: IntakeActions,
               private vcf: ViewContainerRef,
+              private viewContainerRef: ViewContainerRef,
               private dialog: MdDialog) {
           this.intake$ = this.store.select(...this.INTAKE);
+          this.programOfferings$ = this.store.select(...this.PROGRAM_OFFERINGS);
 
   }
 
- 
+  ngOnInit(): void {
+    //this.store.dispatch(this.actions.findProgramOfferings(intake));
+      this.programOfferings$.subscribe(intake => this.store.dispatch(this.actions.findProgramOfferings(intake)));
+  }
+  
 
-  showDialog(): void {
+   createDialog2(): void {
+    this.showDialog();
+  }
+
+  editDialog2(intake:Intake,id:ProgramOffering): void {
+  //  this.showDialog(intake,id);
+  }
+
+  delete(intake:Intake, id:ProgramOffering): void {
+    this.store.dispatch(this.actions.deleteProgramOffering(intake,id))
+  }
+
+  filter(): void {
+  }
+
+   showDialog(): void {
     console.log("showDialog");
     let config = new MdDialogConfig();
     config.viewContainerRef = this.vcf;
@@ -45,6 +74,24 @@ export class ProgramOfferingListComponent {
     this.editorDialogRef.componentInstance.intake = this.intake;
 
     this.editorDialogRef.afterClosed().subscribe(res => {
+      console.log("closeDialog");
+      // reload program offerings
+      this.store.dispatch(this.actions.findProgramOfferings(this.intake));
+    });
+  }
+  
+    showDialog2(): void {
+    console.log("showDialog");
+    let config = new MdDialogConfig();
+    config.viewContainerRef = this.vcf;
+    config.role = 'dialog';
+    config.width = '50%';
+    config.height = '60%';
+    config.position = {top: '0px'};
+    this.editorDialogRef2 = this.dialog.open(ProgramOfferingListEditorDialog, config);
+   // this.editorDialogRef2.componentInstance.programOffering = (this.intake);
+
+    this.editorDialogRef2.afterClosed().subscribe(res => {
       console.log("closeDialog");
       // reload program offerings
       this.store.dispatch(this.actions.findProgramOfferings(this.intake));
