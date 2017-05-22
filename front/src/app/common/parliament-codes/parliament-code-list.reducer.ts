34c2d99a77d34467c3b9dda7f6c007f9520aade5
Msyahrul <msyahrul@umk.edu.my>
2017-05-22 10:36:51 +0800

    ParliamentCodes

---
+++ front/src/app/common/parliament-codes/parliament-code-list.reducer.ts
@@ -0,0 +1,20 @@
+import { SetupActions } from './../../setup/setup.action';
+import {Action} from '@ngrx/store';
+
+import { ParliamentCode } from "./parliament-code.interface";
+
+
+export type ParliamentCodeListState = ParliamentCode[];
+
+const initialState: ParliamentCodeListState = <ParliamentCode[]>[];
+
+export function parliamentCodeListReducer(state = initialState, action: Action): ParliamentCodeListState {
+  switch (action.type) {
+    case SetupActions.FIND_PARLIAMENT_CODES_SUCCESS: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
