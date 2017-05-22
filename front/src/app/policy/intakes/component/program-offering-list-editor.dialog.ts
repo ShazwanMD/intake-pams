ec7d180ce5bab3d7226f6d1e5449d1aa2bb4e7c2
samiya <samiya@10.3.32.157>
2017-05-22 17:06:40 +0800

    add implements and fix error

--- front/src/app/policy/intakes/component/program-offering-list-editor.dialog.ts
+++ front/src/app/policy/intakes/component/program-offering-list-editor.dialog.ts
@@ -8,7 +8,7 @@ import {PolicyModuleState} from "../../index";
 import { ProgramOffering } from "../program-offering.interface";
 import { IntakeActions } from "../intake.action";
 import { Intake } from "../intake.interface";
-
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'pams-program-offering-list-editor',
@@ -20,6 +20,8 @@ export class ProgramOfferingListEditorDialog implements OnInit {
   private editorForm: FormGroup;
   private edit: boolean = false;
   private _programOffering: ProgramOffering;
+  private _intake: Intake;
+  private intake$:Observable<Intake>;
 
   constructor(private router: Router,
               private route: ActivatedRoute,
@@ -35,20 +37,26 @@ export class ProgramOfferingListEditorDialog implements OnInit {
     this.edit = true;
   }
 
+  set intake(intake: Intake) {
+    this._intake = intake;
+  }
+
   ngOnInit(): void {
     this.editorForm = this.formBuilder.group(<ProgramOffering>{
-      projection:0,
-      interview:false,
+      id:null,
+     // projection:0,
+     // interview:false,
       generalCriteria:'',
       specificCriteria:'',   
     });
-
-    if (this.edit) this.editorForm.patchValue(this._programOffering);
+    console.log(this._programOffering);
+    //if (this.edit) this.editorForm.patchValue(this._programOffering);
   }
 
-  submit(intake:Intake, id: ProgramOffering, isValid: boolean) {
-    if (!intake.id) intake =>this.store.dispatch(this.actions.addProgramOffering(intake,id));
-    else  this.store.dispatch(this.actions.updateProgramOffering(intake,id));
+  submit(id: ProgramOffering, isValid: boolean) {
+    // if (!intake.id) intake =>this.store.dispatch(this.actions.addProgramOffering(intake,id));
+    // else  
+    this.store.dispatch(this.actions.updateProgramOffering(this._intake, id));
     this.dialog.close();
   }
 }
