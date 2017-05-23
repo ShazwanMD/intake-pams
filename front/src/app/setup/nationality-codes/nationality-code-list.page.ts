98160c09006cdbb89d8e2e037238b8263736eb0e
Msyahrul <msyahrul@umk.edu.my>
2017-05-23 11:49:48 +0800

    Error Dialog

--- front/src/app/setup/nationality-codes/nationality-code-list.page.ts
+++ front/src/app/setup/nationality-codes/nationality-code-list.page.ts
@@ -1,12 +1,11 @@
 import { NationalityCode } from './../../common/nationality-codes/nationality-code.interface';
-import { MdDialog } from '@angular/material';
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewContainerRef} from "@angular/core";
 import {Store} from "@ngrx/store";
 import {SetupActions} from "../setup.action";
 import {SetupModuleState} from "../index";
 import {Observable} from "rxjs/Observable";
-
-
+import {NationalityCodeCreatorDialog} from "./dialog/nationality-code-creator.dialog";
+import {MdDialog, MdDialogConfig, MdDialogRef} from "@angular/material";
 
 @Component({
   selector: 'pams-nationality-list-page',
@@ -14,8 +13,9 @@ import {Observable} from "rxjs/Observable";
 })
 export class NationalityCodeListPage implements OnInit {
 
-  private NATIONALITY_CODES = "setupModuleState.nationalityCodes".split(".");
-  private nationalityCodes$:Observable<NationalityCode>;
+  private ETHNICITY_CODES = "setupModuleState.nationalityCodes".split(".");
+  private nationalityCodes$: Observable<NationalityCode>;
+  private creatorDialogRef: MdDialogRef<NationalityCodeCreatorDialog>;
   private columns: any[] = [
     {name: 'code', label: 'Code'},
     {name: 'descriptionMs', label: 'DescriptionMs'},
@@ -25,15 +25,44 @@ export class NationalityCodeListPage implements OnInit {
 
   constructor(private actions: SetupActions,
               private store: Store<SetupModuleState>,
-              private dialog: MdDialog){
-    this.nationalityCodes$ = this.store.select(...this.NATIONALITY_CODES);
+              private vcf: ViewContainerRef,
+              private dialog: MdDialog) {
+    this.nationalityCodes$ = this.store.select(...this.ETHNICITY_CODES);
   }
 
   ngOnInit(): void {
-    this.store.dispatch(this.actions.findNationalityCodes())
+    this.store.dispatch(this.actions.findNationalityCodes());
     this.store.dispatch(this.actions.changeTitle("Nationality Codes"))
   }
 
-  filter():void{}
+  createDialog(): void {
+    this.showDialog(null);
+  }
+
+  editDialog(code:NationalityCode): void {
+    this.showDialog(code);
+  }
+
+  delete(code: NationalityCode): void {
+    this.store.dispatch(this.actions.removeNationalityCode(code))
+  }
+
+  filter(): void {
+  }
+
+  private showDialog(code:NationalityCode): void {
+    console.log("create");
+    let config = new MdDialogConfig();
+    config.viewContainerRef = this.vcf;
+    config.role = 'dialog';
+    config.width = '70%';
+    config.height = '65%';
+    config.position = {top: '0px'};
+    this.creatorDialogRef = this.dialog.open(NationalityCodeCreatorDialog, config);
+    if(code) this.creatorDialogRef.componentInstance.nationalityCode = code; // set
+    this.creatorDialogRef.afterClosed().subscribe(res => {
+      console.log("close dialog");
+    });
+  }
 
 }
