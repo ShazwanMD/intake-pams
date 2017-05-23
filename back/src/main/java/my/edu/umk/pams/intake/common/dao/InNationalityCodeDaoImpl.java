b7f7887019be2a667ab301b38fb76dc85397dfc2
Ashraf <Ashraf@10.3.32.140>
2017-05-23 10:28:30 +0800

    updated InNationalityCodeDaoImpl to fix controller error

--- back/src/main/java/my/edu/umk/pams/intake/common/dao/InNationalityCodeDaoImpl.java
+++ back/src/main/java/my/edu/umk/pams/intake/common/dao/InNationalityCodeDaoImpl.java
@@ -37,7 +37,8 @@ public class InNationalityCodeDaoImpl extends GenericDaoSupport<Long, InNational
         Session session = sessionFactory.getCurrentSession();
         Query query = session.createQuery("select s from InNationalityCode s where " +
                 "(upper(s.code) like upper(:filter) " +
-                "or upper(s.description) like upper(:filter)) " +
+                "or upper(s.descriptionEn) like upper(:filter) " +
+                "or upper(s.descriptionMs) like upper(:filter)) " +
                 "and s.metadata.state = :state ");
         query.setString("filter", WILDCARD + filter + WILDCARD);
         query.setInteger("state", InMetaState.ACTIVE.ordinal());
@@ -53,7 +54,8 @@ public class InNationalityCodeDaoImpl extends GenericDaoSupport<Long, InNational
         Session session = sessionFactory.getCurrentSession();
         Query query = session.createQuery("select count(s) from InNationalityCode s where " +
                 "(upper(s.code) like upper(:filter) " +
-                "or upper(s.description) like upper(:filter)) " +
+                "or upper(s.descriptionEn) like upper(:filter) " +
+                "or upper(s.descriptionMs) like upper(:filter)) " +
                 "and s.metadata.state = :state ");
         query.setString("filter", WILDCARD + filter + WILDCARD);
         query.setInteger("state", InMetaState.ACTIVE.ordinal());
