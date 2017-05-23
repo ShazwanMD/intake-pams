fc502df459afee6487013b4f4f2792bdd3d859b8
Msyahrul <msyahrul@umk.edu.my>
2017-05-23 10:31:42 +0800

    NationalityCodes

---
+++ front/src/app/common/nationality-codes/component/nationality-code-select.component.ts
@@ -0,0 +1,33 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {Store} from "@ngrx/store";
+import {FormControl} from "@angular/forms";
+import {CommonActions} from "../../common.action";
+import { CommonModuleState } from "../../index";
+import { NationalityCode } from "../nationality-code.interface";
+
+@Component({
+  selector: 'pams-nationality-code-select',
+  templateUrl: './nationality-code-select.component.html',
+})
+export class NationalityCodeSelectComponent implements OnInit {
+
+  private NATIONALITY_CODE = "commonModuleState.nationalityCodes".split(".");
+  @Input() placeholder: string;
+  @Input() innerFormControl: FormControl;
+  nationalityCodes$: Observable<NationalityCode[]>;
+
+  constructor(private store: Store<CommonModuleState>,
+              private actions: CommonActions) {
+    this.nationalityCodes$ = this.store.select(...this.NATIONALITY_CODE);
+  }
+
+  ngOnInit() {
+    this.store.dispatch(this.actions.findNationalityCodes());
+  }
+
+  selectChangeEvent(event: NationalityCode) {
+    this.innerFormControl.setValue(event, {emitEvent: false});
+  }
+}
+
