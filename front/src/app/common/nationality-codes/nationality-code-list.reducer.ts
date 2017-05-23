369c3aeabc66a6229f21280dfc6c68da66a81a88
Msyahrul <msyahrul@umk.edu.my>
2017-05-23 10:31:28 +0800

    NationalityCodes

---
+++ front/src/app/common/nationality-codes/nationality-code-list.reducer.ts
@@ -0,0 +1,18 @@
+import {Action} from '@ngrx/store';
+import {CommonActions} from "../common.action";
+import { NationalityCode } from "./nationality-code.interface";
+
+export type NationalityCodeListState = NationalityCode[];
+
+const initialState: NationalityCodeListState = <NationalityCode[]>[];
+
+export function nationalityCodeListReducer(state = initialState, action: Action): NationalityCodeListState {
+  switch (action.type) {
+    case CommonActions.FIND_NATIONALITY_CODES_SUCCESS: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
