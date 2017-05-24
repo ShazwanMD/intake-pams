3f1e6d3b6ef04512e4d21328779bdb4921b596bd
samiya <samiya@10.3.32.157>
2017-05-24 11:34:32 +0800

    fix interface

--- front/src/app/policy/intakes/program-offering.interface.ts
+++ front/src/app/policy/intakes/program-offering.interface.ts
@@ -1,6 +1,7 @@
 import {MetaObject} from "../../core/meta-object.interface";
 import {ProgramCode} from "../../common/program-codes/program-code.interface";
 export interface ProgramOffering extends MetaObject {
+  id:number;
   projection:number;
   interview:boolean;
   generalCriteria:string;
