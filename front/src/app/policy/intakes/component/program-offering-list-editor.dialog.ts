ad068a3c6a368638c8153d2be96fcc930bf398cc
samiya <samiya@10.3.32.157>
2017-05-25 15:51:13 +0800

    update to fix function update & delete prog offrg but still not functioning

--- front/src/app/policy/intakes/component/program-offering-list-editor.dialog.ts
+++ front/src/app/policy/intakes/component/program-offering-list-editor.dialog.ts
@@ -1,4 +1,4 @@
-import {Component, ViewContainerRef, OnInit, AfterViewInit} from '@angular/core';
+import { Component, ViewContainerRef, OnInit, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms';
 import {FormBuilder} from '@angular/forms';
 import {Router, ActivatedRoute} from '@angular/router';
@@ -9,19 +9,22 @@ import { ProgramOffering } from "../program-offering.interface";
 import { IntakeActions } from "../intake.action";
 import { Intake } from "../intake.interface";
 import {Observable} from "rxjs/Observable";
+import { ProgramCode } from "../../../common/program-codes/program-code.interface";
 
 @Component({
   selector: 'pams-program-offering-list-editor',
   templateUrl: './program-offering-list-editor.dialog.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class ProgramOfferingListEditorDialog implements OnInit {
+export class ProgramOfferingListEditorDialog {
+//implements OnInit {
 
   private editorForm: FormGroup;
   private edit: boolean = false;
   private _programOffering: ProgramOffering;
   private _intake: Intake;
-  private intake$:Observable<Intake>;
+ // private intake$:Observable<Intake>;
 
   constructor(private router: Router,
               private route: ActivatedRoute,
@@ -33,27 +36,31 @@ export class ProgramOfferingListEditorDialog implements OnInit {
   }
 
   set programOffering(value: ProgramOffering) {
+    console.log("value.id :" + value.id);
     this._programOffering = value;
     this.edit = true;
   }
 
   set intake(intake: Intake) {
     this._intake = intake;
+    this.edit = true;
+
   }
 
   ngOnInit(): void {
     this.editorForm = this.formBuilder.group(<ProgramOffering>{
       id:null,
-     // projection:0,
-     // interview:false,
+      projection:0,
+      interview:false,
       generalCriteria:'',
       specificCriteria:'',   
+      programCode: <ProgramCode>{},
     });
-    console.log(this._programOffering);
-    //if (this.edit) this.editorForm.patchValue(this._programOffering);
+    console.log("this._programOffering :"+this._programOffering.programCode);
+    if (this.edit) this.editorForm.patchValue(this._programOffering);
   }
 
-  submit(id: ProgramOffering, isValid: boolean) {
+  submit(intake :Intake,id: ProgramOffering, isValid: boolean) {
     // if (!intake.id) intake =>this.store.dispatch(this.actions.addProgramOffering(intake,id));
     // else  
     this.store.dispatch(this.actions.updateProgramOffering(this._intake, id));
