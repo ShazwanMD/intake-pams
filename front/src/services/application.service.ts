ff7ec16f62f69d9b68a8e3a94843348f7d2012d2
'hanifnoordin' <hanifnoordin@umk.edu.my>
2017-05-29 08:17:32 +0800

    commit

--- front/src/services/application.service.ts
+++ front/src/services/application.service.ts
@@ -43,9 +43,8 @@ export class ApplicationService {
       .map((res: Response) => <Intake>res.json());
   }
 
-  findProgramOfferingsByIntake(referenceNo: string): Observable<ProgramOffering[]> {
-      console.log("intake.referenceNo : "+referenceNo);
-    return this.http.get(environment.endpoint + '/api/application/intakes/' + referenceNo + '/programOfferings')
+  findProgramOfferingsByIntake(intake: Intake): Observable<ProgramOffering[]> {
+    return this.http.get(environment.endpoint + '/api/application/intakes/' + intake.referenceNo + '/programOfferings')
       .map((res: Response) => <ProgramOffering[]>res.json());
   }
 
