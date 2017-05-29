ab1bc968e60823d15db253614502541e718de2a8
PAMS10 <PAMS10@PAMS10>
2017-05-30 00:30:41 +0800

    Add InLanguageCode/Impl

---
+++ back/src/main/java/my/edu/umk/pams/intake/common/model/InLanguageCode.java
@@ -0,0 +1,20 @@
+package my.edu.umk.pams.intake.common.model;
+
+import my.edu.umk.pams.intake.core.InMetaObject;
+
+public interface InLanguageCode extends InMetaObject {
+
+    String getCode();
+
+    void setCode(String code);
+
+    String getDescriptionMs();
+
+    void setDescriptionMs(String descriptionMs);
+
+    String getDescriptionEn();
+
+    void setDescriptionEn(String descriptionEn);
+    
+
+}
