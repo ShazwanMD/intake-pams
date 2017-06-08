099054a139590313afa26108cd391c1527c81b1a
Msyahrul <msyahrul@umk.edu.my>
2017-06-08 10:51:16 +0800

    SubjectCodeDescription

--- front/src/app/common/subject-codes/subject-code.interface.ts
+++ front/src/app/common/subject-codes/subject-code.interface.ts
@@ -1,6 +1,6 @@
 import {MetaObject} from "../../core/meta-object.interface";
 export interface SubjectCode extends MetaObject{
   code:string;
-  description:string;
-  
+  descriptionEn:string;
+  descriptionMs:string;
 }
