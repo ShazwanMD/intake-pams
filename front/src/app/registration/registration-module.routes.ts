e76b59ae3c962a70ee50a942c5042c004c65caa5
samiya <samiya@10.3.32.157>
2017-06-08 15:05:27 +0800

    update registration routes

--- front/src/app/registration/registration-module.routes.ts
+++ front/src/app/registration/registration-module.routes.ts
@@ -1,9 +1,11 @@
 import {Routes, RouterModule} from '@angular/router';
 import {RegistrationPage} from "./registration.page";
 import { ValidationPage } from "./validation.page";
+import { ValidationInformationPage } from "./validation-information.page";
 
 
 export const RegistrationModuleRoutes: Routes = [
   {path: 'registration', component: RegistrationPage},
+  {path: 'validationInfo', component: ValidationInformationPage},
   {path: 'validation', component: ValidationPage},
 ];
