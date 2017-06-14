820d2f20ea400171d31c2f794754f2ce7d906b79
Rafizan Baharum <rafizan.baharum@canang.com.my>
2017-06-15 01:49:23 +0800

    Cosmetic

--- front/src/app/common/country-codes/component/country-code-select.component.ts
+++ front/src/app/common/country-codes/component/country-code-select.component.ts
@@ -1,19 +1,19 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
-import {Store} from "@ngrx/store";
-import {FormControl} from "@angular/forms";
-import {CommonActions} from "../../common.action";
-import { CommonModuleState } from "../../index";
-import { CountryCode } from "../country-code.interface";
+import {Observable} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {FormControl} from '@angular/forms';
+import {CommonActions} from '../../common.action';
+import {CommonModuleState} from '../../index';
+import {CountryCode} from '../country-code.interface';
 
 @Component({
   selector: 'pams-country-code-select',
   templateUrl: './country-code-select.component.html',
- // styleUrls: ['./country-code.select.scss'],
+  styleUrls: ['./country-code-select.scss'],
 })
 export class CountryCodeSelectComponent implements OnInit {
 
-  private COUNTRY_CODE = "commonModuleState.countryCodes".split(".");
+  private COUNTRY_CODE: string[] = 'commonModuleState.countryCodes'.split('.');
   @Input() placeholder: string;
   @Input() innerFormControl: FormControl;
   countryCodes$: Observable<CountryCode[]>;
@@ -23,11 +23,11 @@ export class CountryCodeSelectComponent implements OnInit {
     this.countryCodes$ = this.store.select(...this.COUNTRY_CODE);
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.store.dispatch(this.actions.findCountryCodes());
   }
 
-  selectChangeEvent(event: CountryCode) {
+  selectChangeEvent(event: CountryCode): void {
     this.innerFormControl.setValue(event, {emitEvent: false});
   }
 }
--- front/src/app/common/state-codes/component/state-code-select.component.ts
+++ front/src/app/common/state-codes/component/state-code-select.component.ts
@@ -1,19 +1,19 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
-import {Store} from "@ngrx/store";
-import {FormControl} from "@angular/forms";
-import {CommonActions} from "../../common.action";
-import { CommonModuleState } from "../../index";
-import { StateCode } from "../state-code.interface";
-
+import {Observable} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {FormControl} from '@angular/forms';
+import {CommonActions} from '../../common.action';
+import {CommonModuleState} from '../../index';
+import {StateCode} from '../state-code.interface';
 
 @Component({
   selector: 'pams-state-code-select',
   templateUrl: './state-code-select.component.html',
+  styleUrls: ['./state-code-select.scss'],
 })
 export class StateCodeSelectComponent implements OnInit {
 
-  private STATE_CODE = "commonModuleState.stateCodes".split(".");
+  private STATE_CODE: string[] = 'commonModuleState.stateCodes'.split('.');
   @Input() placeholder: string;
   @Input() innerFormControl: FormControl;
   stateCodes$: Observable<StateCode[]>;
@@ -23,11 +23,11 @@ export class StateCodeSelectComponent implements OnInit {
     this.stateCodes$ = this.store.select(...this.STATE_CODE);
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.store.dispatch(this.actions.findStateCodes());
   }
 
-  selectChangeEvent(event: StateCode) {
+  selectChangeEvent(event: StateCode): void {
     this.innerFormControl.setValue(event, {emitEvent: false});
   }
 }
