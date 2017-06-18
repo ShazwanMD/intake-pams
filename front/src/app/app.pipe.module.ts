fd1d27ba9490989756b9acffe15441888bf6c7ba
Max Zain <max.zain@canang.com.my>
2017-06-18 17:13:01 +0800

    Add a pipe module to collect all pipes

---
+++ front/src/app/app.pipe.module.ts
@@ -0,0 +1,25 @@
+import { NgModule }      from '@angular/core';
+import {FlowStatePipe} from "./core/flow-state.pipe";
+import {BidStatusPipe} from "./application/intake-applications/bid-status.pipe";
+
+@NgModule({
+    imports:        [],
+    declarations:   [
+        FlowStatePipe,
+        BidStatusPipe
+    ],
+    exports:        [
+        FlowStatePipe,
+        BidStatusPipe
+    ],
+})
+
+export class PipeModule {
+
+    static forRoot() {
+        return {
+            ngModule: PipeModule,
+            providers: [],
+        };
+    }
+}
