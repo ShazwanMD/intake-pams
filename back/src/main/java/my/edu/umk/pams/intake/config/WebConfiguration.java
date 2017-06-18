014ce5992befa1137e8054f8fbeeea99527a8d00
Max Zain <max.zain@canang.com.my>
2017-06-18 18:17:34 +0800

    Remove  duplicate @ComponentScan

--- back/src/main/java/my/edu/umk/pams/intake/config/WebConfiguration.java
+++ back/src/main/java/my/edu/umk/pams/intake/config/WebConfiguration.java
@@ -1,19 +1,13 @@
 package my.edu.umk.pams.intake.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.multipart.support.MultipartFilter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@ComponentScan
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
