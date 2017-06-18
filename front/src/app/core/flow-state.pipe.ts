cb648f0258633a62f45fee4ed9978ef324c2524c
Max Zain <max.zain@canang.com.my>
2017-06-18 17:07:24 +0800

    Add a pipe for flow state enum

---
+++ front/src/app/core/flow-state.pipe.ts
@@ -0,0 +1,21 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {FlowState} from "./flow-state.enum";
+
+@Pipe({name: 'flowStatePipe'})
+export class FlowStatePipe implements PipeTransform {
+
+    transform(value: FlowState): any {
+        if (!value) return value;
+        switch (FlowState[value.toString()]) {
+            case FlowState.DRAFTED : {
+                return 'New';
+            }
+            case FlowState.COMPLETED : {
+                return 'Final';
+            }
+            default: {
+                return value;
+            }
+        }
+    }
+}
