d551d87e848c906a5b1a9b60b3808e5c486e571b
hanifnoordin <hanifnoordin@umk.edu.my>
2017-06-20 12:48:34 +0800

    change bid status according to back

--- front/src/app/application/intake-applications/bid-status.enum.ts
+++ front/src/app/application/intake-applications/bid-status.enum.ts
@@ -1,22 +1,10 @@
 export enum BidStatus{
- /* NEW,
-  DRAFTED,
-  SUBMITTED,
-  EVALUATE,
-  PROCESSING,
-  PRE_SELECT,
-  OFFER,
-  REJECT,
-  WITHDRAWN,
-  APPEALE,
-  APPROVE*/
-    
     NEW,
-    DRAFTED, // new, incomplete
-    SUBMITTED, // after submit
-    SELECTED,  // selected in intake evaluation flow
-    REJECTED,  // rejected in intake evaluation flow
-    WITHDRAW,  // ?? we have cancidate
-    APPEALED, // ?? we have candidate
-    APPROVED // ?? we have candidate
+    DRAFTED,
+    SUBMITTED,
+    SELECTED,
+    REJECTED,
+    WITHDRAW,
+    APPEALED,
+    APPROVED
 }
