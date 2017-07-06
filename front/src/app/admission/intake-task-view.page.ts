396b48dd766196fa77bceca3e783c4f48f7176b9
ashraf <ashraf@umk.edu.my>
2017-07-06 14:46:28 +0800

    Updated intake task view page.ts/html

--- front/src/app/admission/intake-task-view.page.ts
+++ front/src/app/admission/intake-task-view.page.ts
@@ -1,14 +1,13 @@
 import {
-  Component, OnInit
+  Component, OnInit,
 } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
-import {Observable} from "rxjs";
-import {Store} from "@ngrx/store";
-import {IntakeTask} from "../policy/intakes/intake-task.interface";
-import {AdmissionActions} from "./admission.action";
-import {AdmissionModuleState} from "./index";
-import {Candidate} from "./candidate.interface";
-
+import {Observable} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {IntakeTask} from '../policy/intakes/intake-task.interface';
+import {AdmissionActions} from './admission.action';
+import {AdmissionModuleState} from './index';
+import {Candidate} from './candidate.interface';
 
 @Component({
   selector: 'pams-intake-task-view',
@@ -16,36 +15,39 @@ import {Candidate} from "./candidate.interface";
 })
 export class IntakeTaskViewPage implements OnInit {
 
-  private INTAKE_TASK = "admissionModuleState.intakeTask".split(".");
-  private CANDIDATES = "admissionModuleState.candidates".split(".");
+  private INTAKE_TASK: string[] = 'admissionModuleState.intakeTask'.split('.');
+  private CANDIDATES: string[] = 'admissionModuleState.candidates'.split('.');
+  private SELECTED_CANDIDATES: string[] = 'admissionModuleState.selectedCandidates'.split('.');
+  private REJECTED_CANDIDATES: string[] = 'admissionModuleState.rejectedCandidates'.split('.');
   private intakeTask$: Observable<IntakeTask>;
   private candidates$: Observable<Candidate[]>;
+  private selectedCandidates$: Observable<Candidate[]>;
+  private rejectedCandidates$: Observable<Candidate[]>;
 
   constructor(private router: Router,
               private route: ActivatedRoute,
               private store: Store<AdmissionModuleState>,
               private actions: AdmissionActions) {
-    this.intakeTask$ = this.store.select(...this.INTAKE_TASK)
-    this.candidates$ = this.store.select(...this.CANDIDATES)
+    this.intakeTask$ = this.store.select(...this.INTAKE_TASK);
+    this.candidates$ = this.store.select(...this.CANDIDATES);
+    this.selectedCandidates$ = this.store.select(...this.SELECTED_CANDIDATES);
+    this.rejectedCandidates$ = this.store.select(...this.REJECTED_CANDIDATES);
   }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: { taskId: string }) => {
       let taskId: string = params.taskId;
+      console.log('intake: ' + taskId);
       this.store.dispatch(this.actions.findIntakeTaskByTaskId(taskId));
-      this.intakeTask$.subscribe(task => {
-        if (null != task)
-          this.store.dispatch(this.actions.findCandidates(task.intake))
-      })
     });
   }
 
   broadcast(): void {
-    this.store.dispatch(this.actions.broadcastIntakeResult(null)); // todo: payload
+    // this.store.dispatch(this.actions.broadcastIntakeResult(null));
   }
 
-  register(): void {
-
+  approve(): void {
+    // no op
   }
 
   goBack(): void {
