2693f8e25f667c18a5bbc71ad5a38cdce18c5e87
farahanie.mh <farahanie.mh@umk.edu.my>
2017-07-13 16:02:22 +0800

    update change password in dashboard

--- front/src/app/secure/applicant/applicant-dashboard.page.ts
+++ front/src/app/secure/applicant/applicant-dashboard.page.ts
@@ -1,5 +1,5 @@
 import { Applicant } from '../administrator/identity/applicant.interface';
-import { Component, Output, OnInit, Input } from '@angular/core';
+import { Component, Output, OnInit, Input, ViewContainerRef } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {AuthorizationService} from '../../../services/authorization.service';
 import {ApplicationModuleState} from './application/index';
@@ -9,6 +9,9 @@ import {IntakeApplicationActions} from './application/intake-applications/intake
 import {IntakeApplication} from '../../shared/model/application/intake-application.interface';
 import {AccountActions} from "./account/account.action";
 import {Intake} from "../../shared/model/policy/intake.interface";
+import { ChangePasswordDialog } from "./account/dialog/change-password.dialog";
+import { MdDialogConfig, MdDialogRef, MdDialog } from "@angular/material";
+
 
 @Component({
   selector: 'pams-applicant-dashboard',
@@ -17,6 +20,7 @@ import {Intake} from "../../shared/model/policy/intake.interface";
 })
 
 export class ApplicantDashboardPage implements OnInit {
+  [x: string]: any;
 
   private PUBLISHED_INTAKES: string[] = 'accountModuleState.publishedIntakes'.split('.');
   private INTAKE_APPLICATIONS: string[] = 'accountModuleState.intakeApplications'.split('.');
@@ -34,9 +38,13 @@ export class ApplicantDashboardPage implements OnInit {
     {name: 'action', label: ''},
   ];
 
+   private editorDialogRef: MdDialogRef<ChangePasswordDialog>;
+
   constructor(private router: Router,
               private  route: ActivatedRoute,
               private authz: AuthorizationService,
+              private vcf: ViewContainerRef,
+              private dialog: MdDialog,
               private store: Store<ApplicationModuleState>,
               private actions: AccountActions) {
     this.publishedIntakes$ = this.store.select(...this.PUBLISHED_INTAKES);
@@ -48,5 +56,20 @@ export class ApplicantDashboardPage implements OnInit {
     this.store.dispatch(this.actions.findIntakeApplications());
     this.store.dispatch(this.actions.findPublishedIntakes());
     this.store.dispatch(this.actions.findApplicant());
+  }
+  
+showDialog(): void {
+    console.log("showDialog");
+    let config = new MdDialogConfig();
+    config.viewContainerRef = this.vcf;
+    config.role = 'dialog';
+    config.width = '70%';
+    config.height = '65%';
+    config.position = {top: '0px'};
+    this.editorDialogRef= this.dialog.open(ChangePasswordDialog, config);
+    this.editorDialogRef.afterClosed().subscribe(res => {
+      console.log("close dialog");
+      // load something here
+    });
   }
 }
