03ebddfebd3ae2c8cb9ef2959ddaa98e30f06a55
samiya <samiya@10.3.32.157>
2017-07-16 17:00:26 +0800

    update routes

--- front/src/app/registration/registration-module.routes.ts
+++ front/src/app/registration/registration-module.routes.ts
@@ -2,10 +2,13 @@ import {Routes} from '@angular/router';
 import {RegistrationPage} from "./registration.page";
 import {VerificationPage} from "./verification.page";
 import {RegistrationInformationPage} from "./registration-information.page";
+import {ForgetPasswordInformationPage} from "../login/forget-password-information.page";
 
 
 export const registrationModuleRoutes: Routes = [
   {path: 'registration', component: RegistrationPage},
   {path: 'registrationInfo', component: RegistrationInformationPage},
   {path: 'verification/:token', component: VerificationPage},
+  {path: 'forgetPasswordInfo', component: ForgetPasswordInformationPage},
+
 ];
