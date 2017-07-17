dbd13b8507999977aad06e4b29db708d2e52bc8e
ashraf <ashraf@umk.edu.my>
2017-07-17 12:52:35 +0800

    Updated applicant.cmponent, user.component, password-changer dialog.

--- front/src/app/secure/account/dialog/password-changer.dialog.ts
+++ front/src/app/secure/account/dialog/password-changer.dialog.ts
@@ -1,3 +1,4 @@
+import { AuthenticatedUser } from './../../../shared/model/identity/authenticated-user.interface';
 import {PasswordChange} from '../../../shared/model/identity/password-change.interface';
 import {User} from '../../identity/user.interface';
 import {AccountActions} from '../account.action';
@@ -7,6 +8,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {Store} from '@ngrx/store';
 import {MdDialogRef} from '@angular/material';
 import {AccountModuleState} from '../index';
+import { AuthenticationService } from "../../../../services/authentication.service";
 
 @Component({
   selector: 'pams-password-changer',
@@ -17,10 +19,13 @@ export class PasswordChangerDialog implements OnInit {
 
   private changePasswordForm: FormGroup;
   private _user: User;
+    private authenticatedUser: AuthenticatedUser;
 
   constructor(private formBuilder: FormBuilder,
               private dialog: MdDialogRef<PasswordChangerDialog>,
               private store: Store<AccountModuleState>,
+              private authnService: AuthenticationService,
+              private router: Router,
               private actions: AccountActions) {
   }
 
@@ -36,9 +41,15 @@ export class PasswordChangerDialog implements OnInit {
     });
   }
 
+    logout(): void {
+    this.authnService.logout();
+    this.router.navigate(['/login']);
+  }
+
   submit(change: PasswordChange, valid: boolean): void {
     console.log('submit password change: ', change);
     this.store.dispatch(this.actions.changeUserPassword(change));
     this.dialog.close();
+    this.logout();
   }
 }
