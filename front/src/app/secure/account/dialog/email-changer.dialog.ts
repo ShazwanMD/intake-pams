92691ec65ffdbe3e817e2153be8d73feac08aed9
farahanie.mh <farahanie.mh@umk.edu.my>
2017-07-17 17:09:49 +0800

    add email changer dialog

--- front/src/app/secure/account/dialog/email-changer.dialog.ts
+++ front/src/app/secure/account/dialog/email-changer.dialog.ts
@@ -1,5 +1,4 @@
 import { AuthenticatedUser } from './../../../shared/model/identity/authenticated-user.interface';
-import {PasswordChange} from '../../../shared/model/identity/password-change.interface';
 import {User} from '../../identity/user.interface';
 import {AccountActions} from '../account.action';
 import {Component, OnInit, ViewContainerRef} from '@angular/core';
@@ -11,18 +10,18 @@ import {AccountModuleState} from '../index';
 import { AuthenticationService } from "../../../../services/authentication.service";
 
 @Component({
-  selector: 'pams-password-changer',
-  templateUrl: './password-changer.dialog.html',
+  selector: 'pams-email-changer',
+  templateUrl: './email-changer.dialog.html',
 })
 
-export class PasswordChangerDialog implements OnInit {
+export class EmailChangerDialog implements OnInit {
 
-  private changePasswordForm: FormGroup;
+  private emailChangerForm: FormGroup;
   private _user: User;
-    private authenticatedUser: AuthenticatedUser;
+  private authenticatedUser: AuthenticatedUser;
 
   constructor(private formBuilder: FormBuilder,
-              private dialog: MdDialogRef<PasswordChangerDialog>,
+              private dialog: MdDialogRef<EmailChangerDialog>,
               private store: Store<AccountModuleState>,
               private authnService: AuthenticationService,
               private router: Router,
@@ -34,10 +33,9 @@ export class PasswordChangerDialog implements OnInit {
   }
 
   ngOnInit(): void {
-    this.changePasswordForm = this.formBuilder.group({
-      currentPassword: ['', Validators.required],
-      newPassword: ['', Validators.required],
-      newPasswordAgain: ['', Validators.required],
+    this.emailChangerForm = this.formBuilder.group({
+      currentEmail: ['', Validators.required],
+      newEmail: ['', Validators.required],
     });
   }
 
@@ -46,10 +44,10 @@ export class PasswordChangerDialog implements OnInit {
     this.router.navigate(['/login']);
   }
 
-  submit(change: PasswordChange, valid: boolean) {
-    console.log('submit password change: ', change);
-    this.store.dispatch(this.actions.changeUserPassword(change));
-    this.dialog.close();
-    this.logout();   
-} 
+//   submit(change: PasswordChange, valid: boolean) {
+//     console.log('submit password change: ', change);
+//     this.store.dispatch(this.actions.changeUserPassword(change));
+//     this.dialog.close();
+//     this.logout();   
+// } 
 }
