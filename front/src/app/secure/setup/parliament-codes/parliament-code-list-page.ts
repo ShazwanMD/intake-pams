a8ea61662ab0c2aef7214e63a53ccab0993fe09d
Msyahrul <msyahrul@umk.edu.my>
2017-07-25 09:13:46 +0800

    Parliament Code Pagination

--- front/src/app/secure/setup/parliament-codes/parliament-code-list-page.ts
+++ front/src/app/secure/setup/parliament-codes/parliament-code-list-page.ts
@@ -1,60 +1,96 @@
-import {Component, OnInit, ViewContainerRef} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {SetupActions} from '../setup.action';
-import {SetupModuleState} from '../index';
-import {Observable} from 'rxjs/Observable';
+import { ParliamentCode } from './../../../shared/model/common/parliament-code.interface';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  ChangeDetectionStrategy,
+  OnInit,
+  AfterViewInit,
+  ViewContainerRef,
+} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {SetupActions} from "../setup.action";
+import {SetupModuleState} from "../index";
+import {Observable} from "rxjs/Observable";
+import {MdDialog, MdDialogConfig, MdDialogRef} from "@angular/material";
 import {ParliamentCodeEditorDialog} from './dialog/parliament-code-editor.dialog';
-import { MdDialog, MdDialogConfig, MdDialogRef, MdSnackBar } from '@angular/material';
-import {ParliamentCode} from '../../../shared/model/common/parliament-code.interface';
-
+import {
+  TdDataTableService,
+  TdDataTableSortingOrder,
+  ITdDataTableSortChangeEvent,
+  IPageChangeEvent,
+} from '@covalent/core';
 @Component({
-  selector: 'pams-parliament-list-page',
+  selector: 'pams-parliament-list.page',
   templateUrl: './parliament-code-list.page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ParliamentCodeListPage implements OnInit {
-
-  private PARLIAMENT_CODES: string[] = 'setupModuleState.parliamentCodes'.split('.');
-  private parliamentCodes$: Observable<ParliamentCode>;
+export class ParliamentCodeListPage implements OnInit{
+  private PARLIAMENT_CODES = "setupModuleState.parliamentCodes".split(".");
+  private parliamentCodes$: Observable<ParliamentCode[]>;
   private creatorDialogRef: MdDialogRef<ParliamentCodeEditorDialog>;
   private columns: any[] = [
     {name: 'code', label: 'Code'},
     {name: 'description', label: 'Description'},
-    {name: 'action', label: ''},
+    {name: 'action', label: ''}
   ];
-
+    private parliamentCodes: ParliamentCode[];
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  sortBy: string = 'code';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
   constructor(private actions: SetupActions,
               private store: Store<SetupModuleState>,
               private vcf: ViewContainerRef,
               private dialog: MdDialog,
-              private snackBar: MdSnackBar) {
+              private _dataTableService: TdDataTableService) {
     this.parliamentCodes$ = this.store.select(...this.PARLIAMENT_CODES);
+    this.parliamentCodes$.subscribe(ParliamentCodes=>this.parliamentCodes = ParliamentCodes)
   }
-
   ngOnInit(): void {
     this.store.dispatch(this.actions.findParliamentCodes());
-    this.store.dispatch(this.actions.changeTitle('Parliament Codes'));
+    this.store.dispatch(this.actions.changeTitle("Parliament Codes"));
   }
-
   createDialog(): void {
     this.showDialog(null);
   }
-
-  editDialog(code: ParliamentCode): void {
+  editDialog(code:ParliamentCode): void {
     this.showDialog(code);
   }
-
   delete(code: ParliamentCode): void {
-    let snackBarRef = this.snackBar.open('Delete this parliament code?', 'Ok');
-    snackBarRef.afterDismissed().subscribe(() => {
-    this.store.dispatch(this.actions.removeParliamentCode(code));
-    });
+    this.store.dispatch(this.actions.removeParliamentCode(code))
   }
-
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+   search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+    page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
   filter(): void {
+    console.log('filter');
+    let newData: any[] = this.parliamentCodes;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
   }
-
-  private showDialog(code: ParliamentCode): void {
-    console.log('create');
+  private showDialog(code:ParliamentCode): void {
+    console.log("create");
     let config = new MdDialogConfig();
     config.viewContainerRef = this.vcf;
     config.role = 'dialog';
@@ -62,10 +98,9 @@ export class ParliamentCodeListPage implements OnInit {
     config.height = '65%';
     config.position = {top: '0px'};
     this.creatorDialogRef = this.dialog.open(ParliamentCodeEditorDialog, config);
-    if (code) this.creatorDialogRef.componentInstance.parliamentCode = code; // set
-    this.creatorDialogRef.afterClosed().subscribe((res) => {
-      console.log('close dialog');
+    if(code) this.creatorDialogRef.componentInstance.parliamentCode = code; // set
+    this.creatorDialogRef.afterClosed().subscribe(res => {
+      console.log("close dialog");
     });
   }
-
 }
