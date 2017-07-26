fc82f7bf6bfdda63f6b9ff79b6cd3e06ae2f2713
Msyahrul <msyahrul@umk.edu.my>
2017-07-26 08:57:57 +0800

    State-Code Pagination

--- front/src/app/secure/setup/state-codes/state-code-list.page.ts
+++ front/src/app/secure/setup/state-codes/state-code-list.page.ts
@@ -1,57 +1,98 @@
+import { StateCode } from './../../../shared/model/common/state-code.interface';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  ChangeDetectionStrategy,
+  OnInit,
+  AfterViewInit,
+  ViewContainerRef,
+} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {SetupActions} from "../setup.action";
+import {SetupModuleState} from "../index";
+import {Observable} from "rxjs/Observable";
+import {MdDialog, MdDialogConfig, MdDialogRef} from "@angular/material";
 import {StateCodeEditorDialog} from './dialog/state-code-editor.dialog';
-import {Component, OnInit, ViewContainerRef} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {SetupActions} from '../setup.action';
-import {SetupModuleState} from '../index';
-import {Observable} from 'rxjs/Observable';
-import {MdDialog, MdDialogConfig, MdDialogRef} from '@angular/material';
-import {StateCode} from '../../../shared/model/common/state-code.interface';
+import {
+  TdDataTableService,
+  TdDataTableSortingOrder,
+  ITdDataTableSortChangeEvent,
+  IPageChangeEvent,
+} from '@covalent/core';
 
 @Component({
   selector: 'pams-state-list-page',
   templateUrl: './state-code-list.page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StateCodeListPage implements OnInit {
-
-  private STATE_CODES: string[] = 'setupModuleState.stateCodes'.split('.');
-  private stateCodes$: Observable<StateCode>;
+export class StateCodeListPage implements OnInit{
+  private STATE_CODES = "setupModuleState.stateCodes".split(".");
+  private stateCodes$: Observable<StateCode[]>;
   private creatorDialogRef: MdDialogRef<StateCodeEditorDialog>;
   private columns: any[] = [
     {name: 'code', label: 'Code'},
     {name: 'descriptionMs', label: 'DescriptionMs'},
     {name: 'descriptionEn', label: 'DescriptionEn'},
-    {name: 'action', label: ''},
+    {name: 'action', label: ''}
   ];
-
+    private stateCodes: StateCode[];
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  sortBy: string = 'code';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
   constructor(private actions: SetupActions,
               private store: Store<SetupModuleState>,
               private vcf: ViewContainerRef,
-              private dialog: MdDialog) {
+              private dialog: MdDialog,
+              private _dataTableService: TdDataTableService) {
     this.stateCodes$ = this.store.select(...this.STATE_CODES);
+    this.stateCodes$.subscribe(StateCodes=>this.stateCodes = StateCodes)
   }
-
   ngOnInit(): void {
     this.store.dispatch(this.actions.findStateCodes());
-    this.store.dispatch(this.actions.changeTitle('State Codes'));
+    this.store.dispatch(this.actions.changeTitle("State Codes"));
   }
-
   createDialog(): void {
     this.showDialog(null);
   }
-
-  editDialog(code: StateCode): void {
+  editDialog(code:StateCode): void {
     this.showDialog(code);
   }
-
   delete(code: StateCode): void {
-    this.store.dispatch(this.actions.removeStateCode(code));
+    this.store.dispatch(this.actions.removeStateCode(code))
   }
-
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+   search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+    page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
   filter(): void {
+    console.log('filter');
+    let newData: any[] = this.stateCodes;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
   }
-
-  private showDialog(code: StateCode): void {
-    console.log('create');
+  private showDialog(code:StateCode): void {
+    console.log("create");
     let config = new MdDialogConfig();
     config.viewContainerRef = this.vcf;
     config.role = 'dialog';
@@ -59,9 +100,9 @@ export class StateCodeListPage implements OnInit {
     config.height = '65%';
     config.position = {top: '0px'};
     this.creatorDialogRef = this.dialog.open(StateCodeEditorDialog, config);
-    if (code) this.creatorDialogRef.componentInstance.stateCode = code; // set
-    this.creatorDialogRef.afterClosed().subscribe((res) => {
-      console.log('close dialog');
+    if(code) this.creatorDialogRef.componentInstance.stateCode = code; // set
+    this.creatorDialogRef.afterClosed().subscribe(res => {
+      console.log("close dialog");
     });
   }
 }
