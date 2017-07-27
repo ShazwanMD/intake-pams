423f0110ae7d3c28f0f2349b93803c97c9e93f30
Msyahrul <msyahrul@umk.edu.my>
2017-07-27 14:50:53 +0800

    Disability Code refresh onchange

--- front/src/app/secure/setup/disability-codes/disability-code-list-page.ts
+++ front/src/app/secure/setup/disability-codes/disability-code-list-page.ts
@@ -15,15 +15,9 @@ import {SetupModuleState} from "../index";
 import {Observable} from "rxjs/Observable";
 import {MdDialog, MdDialogConfig, MdDialogRef} from "@angular/material";
 import {DisabilityCodeEditorDialog} from './dialog/disability-code-editor.dialog';
-import {
-  TdDataTableService,
-  TdDataTableSortingOrder,
-  ITdDataTableSortChangeEvent,
-  IPageChangeEvent,
-} from '@covalent/core';
 
 @Component({
-  selector: 'pams-disability-list-page',
+  selector: 'pams-disability-list.page',
   templateUrl: './disability-code-list.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -31,28 +25,14 @@ export class DisabilityCodeListPage implements OnInit{
   private DISABILITY_CODES = "setupModuleState.disabilityCodes".split(".");
   private disabilityCodes$: Observable<DisabilityCode[]>;
   private creatorDialogRef: MdDialogRef<DisabilityCodeEditorDialog>;
-  private columns: any[] = [
-    {name: 'code', label: 'Code'},
-    {name: 'descriptionMs', label: 'DescriptionMs'},
-    {name: 'descriptionEn', label: 'DescriptionEn'},
-    {name: 'action', label: ''}
-  ];
-    private disabilityCodes: DisabilityCode[];
-  filteredData: any[];
-  filteredTotal: number;
-  searchTerm: string = '';
-  fromRow: number = 1;
-  currentPage: number = 1;
-  pageSize: number = 10;
-  sortBy: string = 'code';
-  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+  
   constructor(private actions: SetupActions,
               private store: Store<SetupModuleState>,
               private vcf: ViewContainerRef,
-              private dialog: MdDialog,
-              private _dataTableService: TdDataTableService) {
+              private dialog: MdDialog) {
+              
+              
     this.disabilityCodes$ = this.store.select(...this.DISABILITY_CODES);
-    this.disabilityCodes$.subscribe(DisabilityCodes=>this.disabilityCodes = DisabilityCodes)
   }
   ngOnInit(): void {
     this.store.dispatch(this.actions.findDisabilityCodes());
@@ -67,30 +47,7 @@ export class DisabilityCodeListPage implements OnInit{
   delete(code: DisabilityCode): void {
     this.store.dispatch(this.actions.removeDisabilityCode(code))
   }
-  sort(sortEvent: ITdDataTableSortChangeEvent): void {
-    this.sortBy = sortEvent.name;
-    this.sortOrder = sortEvent.order;
-    this.filter();
-  }
-   search(searchTerm: string): void {
-    this.searchTerm = searchTerm;
-    this.filter();
-  }
-    page(pagingEvent: IPageChangeEvent): void {
-    this.fromRow = pagingEvent.fromRow;
-    this.currentPage = pagingEvent.page;
-    this.pageSize = pagingEvent.pageSize;
-    this.filter();
-  }
-  filter(): void {
-    console.log('filter');
-    let newData: any[] = this.disabilityCodes;
-    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
-    this.filteredTotal = newData.length;
-    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
-    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
-    this.filteredData = newData;
-  }
+  
   private showDialog(code:DisabilityCode): void {
     console.log("create");
     let config = new MdDialogConfig();
