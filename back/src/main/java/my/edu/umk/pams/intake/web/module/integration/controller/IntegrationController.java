d5d62284e40b80d9a206d30e2511c7532a9e9a38
Rafizan Baharum <rafizan.baharum@canang.com.my>
2017-07-28 03:02:29 +0800

    Add integration config

--- back/src/main/java/my/edu/umk/pams/intake/web/module/integration/controller/IntegrationController.java
+++ back/src/main/java/my/edu/umk/pams/intake/web/module/integration/controller/IntegrationController.java
@@ -1,4 +1,4 @@
-package my.edu.umk.pams.intake.web.module.integration;
+package my.edu.umk.pams.intake.web.module.integration.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,14 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Policy;
-
-import my.edu.umk.pams.intake.admission.event.CandidatePayload;
 import my.edu.umk.pams.intake.common.service.CommonService;
 import my.edu.umk.pams.intake.identity.service.IdentityService;
 import my.edu.umk.pams.intake.policy.service.PolicyService;
