24ca644286fb62ee7daf425b8dfa67b74705e178
samiya <samiya>
2017-08-01 12:58:35 +0800

    fix effect

--- front/src/app/registration/registration.effect.ts
+++ front/src/app/registration/registration.effect.ts
@@ -21,7 +21,7 @@ export class RegistrationEffects {
     .switchMap(registration => this.registrationService.registerUser(registration))
     .map(message => this.registrationActions.registerUserSuccess(message))
     .catch((error) => this.notificationService.showError(error))
-    .do(action => this.router.navigate(['/registration', action.payload])).ignoreElements();
+    //.do(action => this.router.navigate(['/registration', action.payload])).ignoreElements();
 
   @Effect() verifyUser$ = this.actions$
     .ofType(RegistrationActions.VERIFY_USER)
@@ -33,5 +33,6 @@ export class RegistrationEffects {
     .ofType(RegistrationActions.FORGET_PASSWORD)
     .map(action => action.payload)
     .switchMap(email => this.registrationService.forgetPassword(email))
-    .map(message => this.registrationActions.forgetPasswordSuccess(message));  
+    .map(message => this.registrationActions.forgetPasswordSuccess(message))
+    .catch((error) => this.notificationService.showError(error));  
 }
