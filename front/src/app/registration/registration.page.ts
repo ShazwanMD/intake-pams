6098c837df8b75805adc17a574817d1545c33c07
samiya <samiya@10.3.32.157>
2017-08-17 15:24:19 +0800

    added snackbar in rgn page.ts

--- front/src/app/registration/registration.page.ts
+++ front/src/app/registration/registration.page.ts
@@ -5,6 +5,7 @@ import {Store} from '@ngrx/store';
 import {RegistrationModuleState} from './index';
 import {RegistrationActions} from './registration.action';
 import {UserRegistration} from '../shared/model/registration/user-registration.interface';
+import { MdSnackBar } from "@angular/material";
 
 @Component({
   selector: 'pams-registration-page',
@@ -20,6 +21,7 @@ export class RegistrationPage implements OnInit {
               private formBuilder: FormBuilder,
               private store: Store<RegistrationModuleState>,
               private registrationActions: RegistrationActions,
+              private snackBar: MdSnackBar,
           ) {
   }
 
@@ -35,5 +37,23 @@ export class RegistrationPage implements OnInit {
 
   register(registration: UserRegistration, isValid: boolean) {
     this.store.dispatch(this.registrationActions.registerUser(registration));
+    //if (this.registrationActions.registerUserSuccess){ 
+  //   if (true){ 
+  //   let snackBarRef = this.snackBar.open('A verification email has been sent to your registered email.', '',
+  //   {duration: 3000,});
+  //   snackBarRef.afterDismissed().subscribe((res) => { 
+  //     let snackBarRef2 = this.snackBar.open('Please click the verification link to complete your registration.', '',
+  //   {duration: 3000,});
+  //   snackBarRef2.afterDismissed().subscribe((res) => {     
+  //   });    
+  //   });
+  //   }
+
+  // // else if (!(this.registrationActions.registerUserSuccess))
+  // else
+  //   {
+
+  // }
+
   }
 }
