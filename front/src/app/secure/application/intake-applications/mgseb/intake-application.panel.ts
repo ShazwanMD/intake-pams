002a96d37dfe73ddab5c8b7cbe66ce96c4b11793
farahanie.mh <farahanie.mh@umk.edu.my>
2017-08-20 17:46:58 +0800

    disable copy address in mgseb form

--- front/src/app/secure/application/intake-applications/mgseb/intake-application.panel.ts
+++ front/src/app/secure/application/intake-applications/mgseb/intake-application.panel.ts
@@ -156,16 +156,16 @@ export class MgsebIntakeApplicationPanel implements OnInit {
     }
   }
 
-  copyAddress(application: IntakeApplication) {
-    this.store.dispatch(this.actions.updateIntakeApplication(this.applicationForm.value));
-    let snackBarRef = this.snackBar.open('Confirm to Copy this address?', 'Ok');
-    snackBarRef.afterDismissed().subscribe(() => {
-      if (!application.id) this.store.dispatch(this.actions.updateIntakeApplication(application));
-      else
-         this.store.dispatch(this.actions.copyAddressApplication(application));  
-      // this.goBack();
-    });
-  }
+  // copyAddress(application: IntakeApplication) {
+  //   this.store.dispatch(this.actions.updateIntakeApplication(this.applicationForm.value));
+  //   let snackBarRef = this.snackBar.open('Confirm to Copy this address?', 'Ok');
+  //   snackBarRef.afterDismissed().subscribe(() => {
+  //     if (!application.id) this.store.dispatch(this.actions.updateIntakeApplication(application));
+  //     else
+  //        this.store.dispatch(this.actions.copyAddressApplication(application));  
+  //     // this.goBack();
+  //   });
+  // }
 
   goBack(): void {
     this.router.navigate(['/secure']);
