21959377e61d64e5310449d0c438c34ea3c59367
farahanie.mh <farahanie.mh@umk.edu.my>
2017-08-21 15:47:45 +0800

    fix update intake task dialog

--- front/src/app/secure/policy/intakes/dialog/intake-updater.dialog.ts
+++ front/src/app/secure/policy/intakes/dialog/intake-updater.dialog.ts
@@ -1,6 +1,6 @@
 import {Component, ViewContainerRef, OnInit} from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms';
-import {FormBuilder} from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import {Store} from '@ngrx/store';
 import {MdDialogRef, MdSnackBar} from '@angular/material';
 import {IntakeActions} from '../intake.action';
@@ -9,6 +9,7 @@ import {GraduateCenter} from '../../../../shared/model/common/graduate-center.in
 import {Intake} from '../../../../shared/model/policy/intake.interface';
 import {IntakeSession} from '../../../../shared/model/policy/intake-session.interface';
 import {ProgramLevel} from '../../../../shared/model/policy/program-level.interface';
+import { DateValidation } from "../../../../shared/component/date-validation";
 
 @Component({
   selector: 'pams-intake-updater',
@@ -32,21 +33,22 @@ export class IntakeUpdaterDialog implements OnInit {
   }
 
   ngOnInit(): void {
-    this.updateForm = this.formBuilder.group(<Intake>{
+    this.updateForm = this.formBuilder.group({
       id: null,
       referenceNo: '',
       sourceNo: '',
       intakeNo: '',
       description: '',
       projection: 0,
-      startDate: null,
-      endDate: null,
-      programLevel: <ProgramLevel>{},
-      intakeSession: <IntakeSession>{},
-      graduateCenter: <GraduateCenter>{},
-    });
-    this.updateForm.patchValue(this._intake);
-  }
+      startDate:['', Validators.required],
+      endDate: ['', Validators.required],
+      programLevel:  [<ProgramLevel>{}],
+      intakeSession:  [<IntakeSession>{}], 
+      graduateCenter:[<GraduateCenter>{}],
+    },{
+      validator: DateValidation.CheckDate // your validation method
+   }) 
+ }
 
   submit(intake: Intake, isValid: boolean) {
     let snackBarRef = this.snackBar.open('Confirm to submit?', 'Ok');
