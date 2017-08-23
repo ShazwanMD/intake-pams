3d9277bcee07e51053b838542a82bd23fc832fd6
ashraf <ashraf@umk.edu.my>
2017-08-23 12:41:43 +0800

    Added seed table and domain for promo code

---
+++ back/src/main/java/my/edu/umk/pams/intake/common/model/InPromoCode.java
@@ -0,0 +1,11 @@
+package my.edu.umk.pams.intake.common.model;
+
+import my.edu.umk.pams.intake.core.InMetaObject;
+
+public interface InPromoCode extends InMetaObject {
+
+    String getCode();
+
+    void setCode(String promoCode);
+
+}
