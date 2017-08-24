804cc6d43126c1656826e05891071a4d6358e10b
hanifnoordin <hanifnoordin@umk.edu.my>
2017-08-24 16:11:05 +0800

    principle

--- back/src/main/java/my/edu/umk/pams/intake/identity/model/InRoleType.java
+++ back/src/main/java/my/edu/umk/pams/intake/identity/model/InRoleType.java
@@ -9,13 +9,8 @@ public enum InRoleType {
     ROLE_ADMINISTRATOR, // 0
     ROLE_USER,          // 1
     ROLE_GUEST,         // 2
-<<<<<<< HEAD
 	ROLE_PTJ,			// 3
 	ROLE_APCN,			// 4
 	ROLE_MGT,			// 5
 	ROLE_FCTY;			// 6
-	
-=======
-    ROLE_FACULTY;         // 3
->>>>>>> branch 'master' of http://119.110.101.9/pams/intake.git
 }
