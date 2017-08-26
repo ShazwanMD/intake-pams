e3ed7dc1d74710db437a0d3714fc8b362659355c
Rafizan Baharum <rafizan.baharum@canang.com.my>
2017-08-26 08:44:11 +0800

    Add many candidates

--- back/src/main/java/my/edu/umk/pams/intake/common/router/RouterStrategy.java
+++ back/src/main/java/my/edu/umk/pams/intake/common/router/RouterStrategy.java
@@ -1,6 +1,5 @@
 package my.edu.umk.pams.intake.common.router;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -12,56 +11,56 @@ public interface RouterStrategy {
 
     public List<String> findDrafterCandidates();
 
-    public String findDrafterCandidate(BigDecimal amount, Enum type);
-
-    public List<String> findDrafterCandidates(BigDecimal amount, Enum type);
-
     public String findRequesterCandidate();
 
-    public String findRequesterCandidate(BigDecimal amount, Enum type);
+    public List<String> findRequesterCandidates();
 
     public String findRegistererCandidate();
 
-    public String findRegistererCandidate(BigDecimal amount, Enum Type);
+    public List<String> findRegistererCandidates();
 
     public String findVerifierCandidate();
 
+    public List<String> findVerifierCandidates();
+
     public String findUpperVerifierCandidate();
 
-    public String findVerifierCandidate(BigDecimal amount);
+    public List<String> findUpperVerifierCandidates();
 
     public String findUpperCheckerCandidate();
 
-    public String findUpperCheckerCandidate(BigDecimal amount);
+    public List<String> findUpperCheckerCandidates();
 
     public String findCheckerCandidate();
 
-    public String findCheckerCandidate(BigDecimal amount);
+    public List<String> findCheckerCandidates();
 
     public String findQuerierCandidate();
 
+    public List<String> findQuerierCandidates();
+
     public String findUpperQuerierCandidate();
 
-    public String findUpperQuerierCandidate(BigDecimal amount);
+    public List<String> findUpperQuerierCandidates();
 
     public String findApproverCandidate();
 
-    public String findApproverCandidate(BigDecimal amount);
+    public List<String> findApproverCandidates();
 
     public String findPreparerCandidate();
 
     public List<String> findPreparerCandidates();
 
-    public String findPreparerCandidate(BigDecimal amount, Enum type);
-
-    public List<String> findPreparerCandidates(BigDecimal amount, Enum type);
-
     public String findEvaluatorCandidate();
 
-    public String findEvaluatorCandidate(BigDecimal amount, Enum type);
+    public List<String> findEvaluatorCandidates();
 
     public String findSelectorCandidate();
 
+    public List<String> findSelectorCandidates();
+
     public String findPublisherCandidate();
+
+    public List<String> findPublisherCandidates();
 
 }
