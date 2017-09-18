12bf38da1dea8fb58a628e48158bb1c0f47d0da3
ashraf <ashraf@umk.edu.my>
2017-09-18 17:11:28 +0800

    Add pagination in intake archive.

--- front/src/app/secure/policy/intakes/component/archived-intake-list.component.ts
+++ front/src/app/secure/policy/intakes/component/archived-intake-list.component.ts
@@ -2,6 +2,13 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 import {MdSnackBar, MdSnackBarRef, SimpleSnackBar} from '@angular/material';
 import {Intake} from '../../../../shared/model/policy/intake.interface';
+import {
+  IPageChangeEvent,
+  ITdDataTableSortChangeEvent,
+  TdDataTableService,
+  TdDataTableSortingOrder
+} from '@covalent/core';
+
 
 
 @Component({
@@ -23,8 +30,13 @@ export class ArchivedIntakeListComponent {
   @Input() intakes: Intake[];
   @Output() view: EventEmitter<Intake> = new EventEmitter<Intake>();
 
-  constructor(private snackBar: MdSnackBar) {
+  constructor(private snackBar: MdSnackBar,
+              private _dataTableService: TdDataTableService) {
   }
+
+  // constructor(private snackBar: MdSnackBar,
+  //             private _dataTableService: TdDataTableService) {
+  // }
 
   viewIntake(intake: Intake): void {
     console.log('Emitting task');
@@ -33,4 +45,48 @@ export class ArchivedIntakeListComponent {
       this.view.emit(intake);
     });
   }
+
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  sortBy: string = 'referenceNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
+  ngAfterViewInit(): void {
+    this.filteredData = this.intakes;
+    this.filteredTotal = this.intakes.length;
+    this.filter();
+  }
+
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+
+  search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+
+  page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
+
+  filter(): void {
+    let newData: any[] = this.intakes;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
+  }
+
 }
+
