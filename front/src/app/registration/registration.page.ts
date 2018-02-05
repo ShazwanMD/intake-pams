43ab607f3db611da6c74706a10921f9a67178192
farahanie.mh <farahanie.mh@umk.edu.my>
2018-02-05 17:07:59 +0800

    add verify password in registration

--- front/src/app/registration/registration.page.ts
+++ front/src/app/registration/registration.page.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
-import {FormBuilder, FormGroup,Validators} from '@angular/forms';
+import {FormBuilder, FormGroup,Validators, AbstractControl} from '@angular/forms';
 import {Store} from '@ngrx/store';
 import {RegistrationModuleState} from './index';
 import {RegistrationActions} from './registration.action';
 import {UserRegistration} from '../shared/model/registration/user-registration.interface';
 import { MdSnackBar } from "@angular/material";
+import { PasswordValidation } from '../shared/component/password-validation';
 
 @Component({
   selector: 'pams-registration-page',
@@ -27,12 +28,27 @@ export class RegistrationPage implements OnInit {
 
   ngOnInit(): void {
     let emailPattern: string = '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$';
+   // let password = 'password';
     this.registrationForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [(Validators.required), (Validators.pattern(emailPattern))]],
       identityNo: ['', Validators.required],
       password: ['', Validators.required],
-    });
+      confirmPassword :['', Validators.required],
+    },
+    { validator: this.matchingPasswords } 
+  )
+  } 
+
+  private matchingPasswords( AC: AbstractControl ) {
+    const password = AC.get( 'password' );
+    const confirmPassword = AC.get( 'confirmPassword' );
+
+    if ( !password || !confirmPassword ) {
+      return null;
+    }
+
+    return password.value === confirmPassword.value ? null : { nomatch: true };
   }
   
   register(registration: UserRegistration, isValid: boolean) {
